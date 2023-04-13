df28bd883006f5df2cb5b1255cfcf1f501571add
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-04-13 10:13:26 +0100

    Separate BasicBlockOp from ControlFlowOp: former in OpType, latter in FunctionOp

--- src/ops/controlflow.rs
+++ src/ops/controlflow.rs
@@ -4,26 +4,15 @@ use crate::types::{EdgeKind, Signature, TypeRow};
 
 use super::Op;
 
+/// Dataflow operations that are (informally) related to control flow.
 #[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub enum ControlFlowOp {
     /// ɣ (gamma) node: conditional operation
     Conditional { inputs: TypeRow, outputs: TypeRow },
     /// θ (theta) node: tail-controlled loop. Here we assume the same inputs + outputs variant.
     Loop { vars: TypeRow },
-    /// β (beta): a CFG basic block node. The signature is that of the internal Dataflow graph.
-    BasicBlock { inputs: TypeRow, outputs: TypeRow },
     /// 𝛋 (kappa): a dataflow node which is defined by a child CFG
     CFG { inputs: TypeRow, outputs: TypeRow },
-}
-
-impl ControlFlowOp {
-    pub fn other_edges(&self) -> Option<EdgeKind> {
-        Some(if let ControlFlowOp::BasicBlock { .. } = self {
-            EdgeKind::ControlFlow
-        } else {
-            EdgeKind::StateOrder
-        })
-    }
 }
 
 impl Op for ControlFlowOp {
@@ -31,7 +20,6 @@ impl Op for ControlFlowOp {
         match self {
             ControlFlowOp::Conditional { .. } => "ɣ",
             ControlFlowOp::Loop { .. } => "θ",
-            ControlFlowOp::BasicBlock { .. } => "β",
             ControlFlowOp::CFG { .. } => "𝛋",
         }
         .into()
@@ -43,10 +31,33 @@ impl Op for ControlFlowOp {
                 Signature::new_df(inputs.clone(), outputs.clone())
             }
             ControlFlowOp::Loop { vars } => Signature::new_linear(vars.clone()),
-            ControlFlowOp::BasicBlock { .. } => Signature::default(),
             ControlFlowOp::CFG { inputs, outputs } => {
                 Signature::new_df(inputs.clone(), outputs.clone())
             }
         }
+    }
+}
+
+/// β (beta): a CFG basic block node. The signature is that of the internal Dataflow graph.
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
+pub struct BasicBlockOp {
+    inputs: TypeRow,
+    outputs: TypeRow,
+}
+
+impl BasicBlockOp {
+    pub fn other_edges(&self) -> Option<EdgeKind> {
+        Some(EdgeKind::ControlFlow)
+    }
+}
+
+impl Op for BasicBlockOp {
+    fn name(&self) -> SmolStr {
+        "β".into()
+    }
+
+    fn signature(&self) -> Signature {
+        // The value edges into/out of the beta-node itself
+        Signature::default()
     }
 }
