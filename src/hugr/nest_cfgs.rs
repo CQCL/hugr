c2f8875c75aebaf0de0a3d1bc8221eeb2dc1fa35
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-04-18 16:38:54 +0100

    fmt

--- src/hugr/nest_cfgs.rs
+++ src/hugr/nest_cfgs.rs
@@ -1,10 +1,9 @@
 use portgraph::NodeIndex;
-use std::collections::{HashMap, HashSet, LinkedList};
 use std::cmp::min;
+use std::collections::{HashMap, HashSet, LinkedList};
 use std::iter::empty;
 
-struct HugrView {
-}
+struct HugrView {}
 impl HugrView {
     pub fn entry_node(&self) -> NodeIndex {
         todo!()
@@ -25,7 +24,7 @@ impl HugrView {
 #[derive(Clone, PartialEq, Eq, Hash)]
 enum EdgeDest {
     Forward(NodeIndex),
-    Backward(NodeIndex)
+    Backward(NodeIndex),
 }
 impl EdgeDest {
     pub fn node_index(&self) -> NodeIndex {
@@ -45,21 +44,21 @@ impl EdgeDest {
 #[derive(Clone, PartialEq, Eq, Hash)]
 enum UDEdge {
     CFGEdge(NodeIndex, EdgeDest),
-    CappingBackedge(NodeIndex, usize)
+    CappingBackedge(NodeIndex, usize),
 }
 
 type CycleClass = (UDEdge, usize);
 
 struct BracketList {
     items: LinkedList<UDEdge>,
-    size: usize // deleted items already taken off
+    size: usize, // deleted items already taken off
 }
 
 impl BracketList {
     pub fn new() -> Self {
         BracketList {
             items: LinkedList::new(),
-            size: 0
+            size: 0,
         }
     }
 
@@ -70,14 +69,14 @@ impl BracketList {
                 self.items.pop_front();
                 //deleted.remove(e); // Only saves memory
             } else {
-                return Some((e.clone(), self.size))
+                return Some((e.clone(), self.size));
             }
         }
         None
     }
 
     pub fn concat(&mut self, other: BracketList) -> () {
-        let BracketList {mut items, size} = other;
+        let BracketList { mut items, size } = other;
         self.items.append(&mut items);
         assert!(items.len() == 0);
         self.size += size;
@@ -106,7 +105,7 @@ struct UndirectedDFSTree<'a> {
 struct TraversalState {
     deleted_backedges: HashSet<UDEdge>,
     capping_edges: HashMap<usize, Vec<UDEdge>>, // Indexed by DFS num, elems all CappingBackedge's
-    edge_classes: HashMap<UDEdge, CycleClass>
+    edge_classes: HashMap<UDEdge, CycleClass>,
 }
 
 impl<'a> UndirectedDFSTree<'a> {
@@ -126,7 +125,7 @@ impl<'a> UndirectedDFSTree<'a> {
         let mut t = UndirectedDFSTree {
             h: h,
             dfs_num: HashMap::new(),
-            dfs_parents: HashMap::new()
+            dfs_parents: HashMap::new(),
         };
         let mut pending = vec![(EdgeDest::Forward(h.exit_node()), h.entry_node())];
         while let Some((p_edge, n)) = pending.pop() {
@@ -137,13 +136,16 @@ impl<'a> UndirectedDFSTree<'a> {
                     pending.push(e.flip(n));
                 }
             }
-        };
+        }
         t
     }
 
-    fn undirected_edges(&self, n: NodeIndex) -> impl Iterator<Item=EdgeDest> {
+    fn undirected_edges(&self, n: NodeIndex) -> impl Iterator<Item = EdgeDest> {
         // Filter reachable? Or no?
-        self.h.successors(n).map(EdgeDest::Forward).chain(self.h.predecessors(n).map(EdgeDest::Backward))
+        self.h
+            .successors(n)
+            .map(EdgeDest::Forward)
+            .chain(self.h.predecessors(n).map(EdgeDest::Backward))
     }
 
     pub fn children_backedges(&self, n: NodeIndex) -> (Vec<EdgeDest>, Vec<EdgeDest>) {
@@ -158,10 +160,13 @@ impl<'a> UndirectedDFSTree<'a> {
     fn traverse(&self, st: &mut TraversalState, n: NodeIndex) -> (usize, BracketList) {
         let n_dfs = *self.dfs_num.get(&n).unwrap(); // should only be called for nodes on path to exit
         let (children, non_capping_backedges) = self.children_backedges(n);
-        let child_results: Vec<_> = children.iter().map(|c| self.traverse(st, c.node_index())).collect();
+        let child_results: Vec<_> = children
+            .iter()
+            .map(|c| self.traverse(st, c.node_index()))
+            .collect();
         let mut min_dfs_target: [Option<usize>; 2] = [None, None];
         let mut bs = BracketList::new();
-        for (tgt,brs) in child_results {
+        for (tgt, brs) in child_results {
             if tgt < min_dfs_target[0].unwrap_or(usize::MAX) {
                 min_dfs_target = [Some(tgt), min_dfs_target[0]]
             } else if tgt < min_dfs_target[1].unwrap_or(usize::MAX) {
@@ -183,7 +188,7 @@ impl<'a> UndirectedDFSTree<'a> {
                 };
             }
         }
-        
+
         // remove edges to here - these are <backedges> to nodes with greater dfs_num
         // add backedges from here - i.e. any <backedges> to nodes with lower dfs_num
         for e in non_capping_backedges.iter() {
@@ -194,7 +199,7 @@ impl<'a> UndirectedDFSTree<'a> {
                 bs.push(b);
                 min_dfs_target[0] = Some(match min_dfs_target[0] {
                     None => target_dfs,
-                    Some(d) => min(target_dfs, d)
+                    Some(d) => min(target_dfs, d),
                 });
             } else if target_dfs > n_dfs {
                 // Remove backedges to here from lower down, including capping
@@ -202,12 +207,19 @@ impl<'a> UndirectedDFSTree<'a> {
             }
         }
         // also remove any capping backedges
-        for e in st.capping_edges.remove(&n_dfs).into_iter().flat_map(|v|v.into_iter()) {
+        for e in st
+            .capping_edges
+            .remove(&n_dfs)
+            .into_iter()
+            .flat_map(|v| v.into_iter())
+        {
             bs.delete(&e, &mut st.deleted_backedges);
         }
         let parent_edge = self.dfs_parents.get(&n).unwrap();
-        st.edge_classes.insert(UDEdge::CFGEdge(n, parent_edge.clone()), bs.tag(&st.deleted_backedges).unwrap());
+        st.edge_classes.insert(
+            UDEdge::CFGEdge(n, parent_edge.clone()),
+            bs.tag(&st.deleted_backedges).unwrap(),
+        );
         (min_dfs_target[0].unwrap(), bs)
     }
-    
 }
