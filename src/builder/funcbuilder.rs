60e98cabf3f4d932bfc58189cdb6f919f49f1742
seyon <seyon.sivarajah@cambridgequantum.com>
2023-04-19 13:14:52 +0100

    rename BaseBuilder to HugrMut

--- src/builder/funcbuilder.rs
+++ src/builder/funcbuilder.rs
@@ -1,8 +1,9 @@
 use portgraph::NodeIndex;
 
-use crate::hugr::BaseBuilder;
-use crate::ops::DataflowOp;
+use crate::hugr::{BuildError, HugrMut};
+use crate::ops::{DataflowOp, ModuleOp};
 use crate::types::{Signature, SimpleType, TypeRow};
+use crate::Hugr;
 use crate::{hugr::HugrError, ops::OpType};
 
 #[derive(Clone, Copy)]
@@ -12,46 +13,38 @@ pub struct DeltaID {
     out_wires: Vec<Wire>,
 }
 
+pub struct FuncID(NodeIndex);
+
+#[derive(Default)]
+pub struct ModuleBuilder(HugrMut);
+
+impl ModuleBuilder {
+    pub fn new() -> Self {
+        Self(HugrMut::new())
+    }
+}
+
 pub struct DeltaBuilder<'f> {
-    base: &'f mut BaseBuilder,
+    base: &'f mut HugrMut,
     delt_node: NodeIndex,
     internal_in_wires: Vec<Wire>,
     external_out_wires: Vec<Wire>,
     io: [NodeIndex; 2],
 }
 
-pub trait DataflowIO<'f> {
-    fn add_io_children(
-        base: &'f mut BaseBuilder,
-        cont_node: NodeIndex,
-        inputs: TypeRow,
-        outputs: TypeRow,
-    ) -> Result<[NodeIndex; 2], HugrError> {
-        let i = base.add_op_with_parent(
-            cont_node,
-            OpType::Function(DataflowOp::Input { types: inputs }),
-        )?;
-        let o = base.add_op_with_parent(
-            cont_node,
-            OpType::Function(DataflowOp::Output { types: outputs }),
-        )?;
-
-        Ok([i, o])
-    }
-}
-
-pub trait ContainerBuilder {
+pub trait Container {
     type ContainerHandle;
-    fn parent(&self) -> NodeIndex;
-    fn base(&mut self) -> &mut BaseBuilder;
+    fn container_node(&self) -> NodeIndex;
+    fn base(&mut self) -> &mut HugrMut;
     fn add_child_op(&mut self, op: impl Into<OpType>) -> Result<NodeIndex, HugrError> {
-        let parent = self.parent();
+        let parent = self.container_node();
         self.base().add_op_with_parent(parent, op)
     }
+
     fn finish(self) -> Self::ContainerHandle;
 }
 
-pub trait DataflowBuilder: ContainerBuilder {
+pub trait Dataflow: Container {
     fn io(&self) -> [NodeIndex; 2];
     fn add_dataflow_op(
         &mut self,
@@ -62,13 +55,24 @@ pub trait DataflowBuilder: ContainerBuilder {
         Ok(wires)
     }
 
-    fn set_outputs(&mut self, outputs: Vec<Wire>) -> Result<(), HugrError> {
+    fn set_outputs(&mut self, outputs: impl IntoIterator<Item = Wire>) -> Result<(), HugrError> {
         let [_, out] = self.io();
         let base = self.base();
         for (dst_port, Wire(src, src_port)) in outputs.into_iter().enumerate() {
             base.connect(src, src_port, out, dst_port)?;
         }
         Ok(())
+    }
+
+    fn finish_with_outputs(
+        mut self,
+        outputs: impl IntoIterator<Item = Wire>,
+    ) -> Result<Self::ContainerHandle, HugrError>
+    where
+        Self: Sized,
+    {
+        self.set_outputs(outputs)?;
+        Ok(self.finish())
     }
 
     fn input_wires(&self) -> &[Wire];
@@ -78,7 +82,6 @@ pub trait DataflowBuilder: ContainerBuilder {
             .try_into()
             .expect(&format!("Incorrect number of wires: {}", N)[..])
     }
-
     fn delta_builder<'a: 'b, 'b>(
         &'a mut self,
         inputs: Vec<(SimpleType, Wire)>,
@@ -97,7 +100,7 @@ pub trait DataflowBuilder: ContainerBuilder {
     }
 }
 
-fn add_op_with_wires<T: DataflowBuilder + ?Sized>(
+fn add_op_with_wires<T: Dataflow + ?Sized>(
     dbuild: &mut T,
     op: impl Into<OpType>,
     inputs: Vec<Wire>,
@@ -117,7 +120,7 @@ fn add_op_with_wires<T: DataflowBuilder + ?Sized>(
 
 impl<'f> DeltaBuilder<'f> {
     pub fn create_with_io(
-        base: &'f mut BaseBuilder,
+        base: &'f mut HugrMut,
         parent: NodeIndex,
         inputs: TypeRow,
         outputs: TypeRow,
@@ -145,13 +148,13 @@ impl<'f> DeltaBuilder<'f> {
     }
 }
 
-impl<'f> ContainerBuilder for DeltaBuilder<'f> {
+impl<'f> Container for DeltaBuilder<'f> {
     type ContainerHandle = DeltaID;
-    fn parent(&self) -> NodeIndex {
+    fn container_node(&self) -> NodeIndex {
         self.delt_node
     }
 
-    fn base(&mut self) -> &mut BaseBuilder {
+    fn base(&mut self) -> &mut HugrMut {
         self.base
     }
     fn finish(self) -> DeltaID {
@@ -162,13 +165,74 @@ impl<'f> ContainerBuilder for DeltaBuilder<'f> {
     }
 }
 
-impl<'f> DataflowBuilder for DeltaBuilder<'f> {
+impl<'f> Dataflow for DeltaBuilder<'f> {
     fn io(&self) -> [NodeIndex; 2] {
         self.io
     }
 
     fn input_wires(&self) -> &[Wire] {
         &self.internal_in_wires[..]
+    }
+}
+
+impl Container for ModuleBuilder {
+    type ContainerHandle = Result<Hugr, BuildError>;
+
+    fn container_node(&self) -> NodeIndex {
+        self.0.root()
+    }
+
+    fn base(&mut self) -> &mut HugrMut {
+        &mut self.0
+    }
+
+    fn finish(self) -> Self::ContainerHandle {
+        self.0.finish()
+    }
+}
+
+pub struct FunctionBuilder<'b>(DeltaBuilder<'b>);
+
+impl<'b> Container for FunctionBuilder<'b> {
+    type ContainerHandle = FuncID;
+
+    fn container_node(&self) -> NodeIndex {
+        self.0.container_node()
+    }
+
+    fn base(&mut self) -> &mut HugrMut {
+        self.0.base()
+    }
+
+    fn finish(self) -> Self::ContainerHandle {
+        let n = self.0.finish();
+        FuncID(n.node)
+    }
+}
+
+impl<'b> Dataflow for FunctionBuilder<'b> {
+    fn io(&self) -> [NodeIndex; 2] {
+        self.0.io
+    }
+
+    fn input_wires(&self) -> &[Wire] {
+        self.0.input_wires()
+    }
+}
+
+impl ModuleBuilder {
+    pub fn function_builder<'a: 'b, 'b>(
+        &'a mut self,
+        _name: impl Into<String>,
+        inputs: TypeRow,
+        outputs: TypeRow,
+    ) -> Result<FunctionBuilder<'b>, HugrError> {
+        let defn = self.add_child_op(OpType::Module(ModuleOp::Def {
+            signature: Signature::new(inputs.clone(), outputs.clone(), None),
+        }))?;
+
+        let db = DeltaBuilder::create_with_io(self.base(), defn, inputs, outputs)?;
+        Ok(FunctionBuilder(db))
     }
 }
 
@@ -188,32 +252,32 @@ mod test {
 
     #[test]
     fn nested_identity() -> Result<(), HugrError> {
-        let mut base = BaseBuilder::new();
-        let root = base.root();
+        let buildres = {
+            let mut modbuilder = ModuleBuilder::new();
 
-        let mut outbuild =
-            DeltaBuilder::create_with_io(&mut base, root, type_row![NAT, QB], type_row![NAT, QB])?;
+            let _fdef = {
+                let mut fbuild =
+                    modbuilder.function_builder("main", type_row![NAT, QB], type_row![NAT, QB])?;
 
-        let [int, qb] = outbuild.input_wires_arr();
+                let [int, qb] = fbuild.input_wires_arr();
 
-        let qout = outbuild.add_dataflow_op(
-            OpType::Function(DataflowOp::Leaf { op: LeafOp::H }),
-            vec![qb],
-        )?;
+                let qout = fbuild.add_dataflow_op(
+                    OpType::Function(DataflowOp::Leaf { op: LeafOp::H }),
+                    vec![qb],
+                )?;
 
-        let indelt: DeltaID;
-        {
-            let mut inbuilder = outbuild.delta_builder(vec![(NAT, int)], type_row![NAT])?;
-            inbuilder.set_outputs(inbuilder.input_wires().iter().cloned().collect())?;
-            indelt = inbuilder.finish();
-        }
+                let indelt: DeltaID = {
+                    let inbuilder = fbuild.delta_builder(vec![(NAT, int)], type_row![NAT])?;
+                    let [iw] = inbuilder.input_wires_arr();
+                    inbuilder.finish_with_outputs([iw])?
+                };
 
-        outbuild.set_outputs([indelt.out_wires, qout].concat())?;
+                fbuild.finish_with_outputs([indelt.out_wires, qout].concat())?
+            };
+            modbuilder.finish()
+        };
 
-        let _ = outbuild.finish();
-
-        let buildres = base.finish();
-        // println!("{}", buildres.unwrap().dot_string());
+        println!("{}", buildres.clone().unwrap().dot_string());
         assert!(buildres.is_ok());
 
         Ok(())
