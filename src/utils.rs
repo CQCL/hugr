ae896663b4e41f2f860d45efa43d441a26ffe018
seyon <seyon.sivarajah@cambridgequantum.com>
2023-04-21 14:43:20 +0100

    prevent visualisation being used in CI runs

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,7 @@
 #[cfg(test)]
 pub(crate) mod test {
     /// Open a browser page to render a dot string graph
+    #[cfg(not(ci_run))]
     pub(crate) fn viz_dotstr(dotstr: &str) {
         let mut base: String = "https://dreampuf.github.io/GraphvizOnline/#".into();
         url_escape::encode_query_to_string(dotstr, &mut base);
