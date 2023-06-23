fc8b69787a42d5b7ccbf79fde201f3daebcd3b0c
seyon <seyon.sivarajah@quantinuum.com>
2023-06-23 12:42:15 +0100

    simplify devenv nix

--- devenv.nix
+++ devenv.nix
@@ -2,25 +2,21 @@
 
 {
   # https://devenv.sh/basics/
-  env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = with pkgs; [ cargo rustc rust-analyzer rustfmt clippy ];
+  # packages = with pkgs; [ cargo rustc rust-analyzer rustfmt clippy ];
 
-  # Certain Rust tools won't work without this
-  # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
-  # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
-  env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  scripts.hello.exec = "echo Welcome to hugr dev shell!";
 
   enterShell = ''
     hello
-    git --version
+    cargo --version
   '';
 
   # https://devenv.sh/languages/
-  # languages.nix.enable = true;
+  # https://devenv.sh/reference/options/#languagesrustversion
+  languages.rust.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
