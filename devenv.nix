e6388a7c8e2ab1a49f4b1b15afedec434fffd61c
seyon <seyon.sivarajah@quantinuum.com>
2023-06-23 12:48:07 +0100

    revert to manual package specification

--- devenv.nix
+++ devenv.nix
@@ -4,7 +4,10 @@
   # https://devenv.sh/basics/
 
   # https://devenv.sh/packages/
-  # packages = with pkgs; [ cargo rustc rust-analyzer rustfmt clippy ];
+  # manually set rust packages rather than use devenv language support because
+  # it doesn't seem to be up to date for macos yet (link error)
+  packages = with pkgs; [ cargo rustc rust-analyzer rustfmt clippy ];
+  env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo Welcome to hugr dev shell!";
@@ -16,7 +19,7 @@
 
   # https://devenv.sh/languages/
   # https://devenv.sh/reference/options/#languagesrustversion
-  languages.rust.enable = true;
+  # languages.rust.enable = true;
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
