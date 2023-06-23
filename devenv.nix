f855ce4cc277f196844ce812c4e730c0247f2389
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-06-23 13:46:40 +0100

    add devenv files (#205)
    * devenv init
    
    * simplify devenv nix
    
    * revert to manual package specification
    
    * add clippy and format pre-commit

---
+++ devenv.nix
@@ -0,0 +1,32 @@
+{ pkgs, ... }:
+
+{
+  # https://devenv.sh/basics/
+
+  # https://devenv.sh/packages/
+  # manually set rust packages rather than use devenv language support because
+  # it doesn't seem to be up to date for macos yet (link error)
+  packages = with pkgs; [ cargo rustc rust-analyzer rustfmt clippy ];
+  env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = "echo Welcome to hugr dev shell!";
+
+  enterShell = ''
+    hello
+    cargo --version
+  '';
+
+  # https://devenv.sh/languages/
+  # https://devenv.sh/reference/options/#languagesrustversion
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/pre-commit-hooks/
+  pre-commit.hooks.clippy.enable = true;
+  pre-commit.hooks.rustfmt.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.ping.exec = "ping example.com";
+
+  # See full reference at https://devenv.sh/reference/options/
+}
