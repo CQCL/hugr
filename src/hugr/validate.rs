19980756a8d02e54b12e84c23bfa7d02c06aa377
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-06-27 18:14:14 +0100

    Test validation of local consts, doc failures (add_other_edge version)

--- src/hugr/validate.rs
+++ src/hugr/validate.rs
@@ -392,6 +392,15 @@ impl<'a> ValidationContext<'a> {
 
         let postorder = DfsPostOrder::new(&region, entry_node);
         let nodes_visited = postorder.iter(&region).filter(|n| *n != parent).count();
+        let _module_op_count = self
+            .hugr
+            .children(parent)
+            .filter(|n| OpTag::ModuleOp.contains(self.hugr.get_optype(*n).tag()))
+            .count();
+        // XXX TODO FIXME we should take _module_op_count off the child_count below,
+        // because e.g. Const nodes should not be reached by the DfsPostOrder in a forwards
+        // traversal. (Currently this would break tests because the traversal goes both forwards
+        // AND backwards).
         if nodes_visited != self.hugr.hierarchy.child_count(parent.index) {
             return Err(ValidationError::NotABoundedDag {
                 node: parent,
@@ -1105,6 +1114,43 @@ mod test {
         );
         //Order edge. This will need metadata indicating its purpose.
         h.add_other_edge(input, sub_dfg)?;
+        h.validate().unwrap();
+        Ok(())
+    }
+
+    #[test]
+    fn test_local_const() -> Result<(), HugrError> {
+        let mut h = Hugr::new(ops::DFG {
+            signature: Signature::new_df(type_row![B], type_row![B]),
+        });
+        let input = h.add_op_with_parent(h.root(), ops::Input::new(type_row![B]))?;
+        let output = h.add_op_with_parent(h.root(), ops::Output::new(type_row![B]))?;
+        let xor = h.add_op_with_parent(h.root(), LeafOp::Xor)?;
+        h.connect(input, 0, xor, 0)?;
+        h.connect(xor, 0, output, 0)?;
+        assert_eq!(
+            h.validate(),
+            Err(ValidationError::UnconnectedPort {
+                node: xor,
+                port: Port::new_incoming(1),
+                port_kind: EdgeKind::Value(B)
+            })
+        );
+        // Second input of Xor from a constant
+        let cst =
+            h.add_op_with_parent(h.root(), ops::Const(ConstValue::Int { width: 1, value: 1 }))?;
+        let lcst = h.add_op_with_parent(
+            h.root(),
+            ops::LoadConstant {
+                datatype: ClassicType::Int(1),
+            },
+        )?;
+        h.connect(cst, 0, lcst, 0)?;
+        h.connect(lcst, 0, xor, 1)?;
+        // XXX TODO FIXME This should fail, but succeeds:
+        h.validate().unwrap();
+        // Now include the LoadConstant node in the causal cone
+        h.add_other_edge(input, lcst)?;
         h.validate().unwrap();
         Ok(())
     }
