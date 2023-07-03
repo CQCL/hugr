4dd0c8b2254bb19e5f67603e39d2d41d3e4c4659
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-07-03 20:06:35 +0100

    ExternalOp::Resource Break out struct ResourceOp, cache Signature

--- src/ops/custom.rs
+++ src/ops/custom.rs
@@ -13,33 +13,18 @@ use super::tag::OpTag;
 use super::{LeafOp, OpName, OpTrait, OpType};
 
 /// An instantiation of an [`OpDef`] with values for the type arguments
-#[derive(Clone, Debug, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 #[serde(into = "OpaqueOp", from = "OpaqueOp")]
 pub enum ExternalOp {
-    Resource { def: Arc<OpDef>, args: Vec<TypeArg> },
-    Opaque(OpaqueOp), // If we can't identify the Resource definition
+    Resource(ResourceOp), // When we've found the Resource definition
+    Opaque(OpaqueOp),     // When we haven't
 }
 
 impl From<ExternalOp> for OpaqueOp {
     fn from(value: ExternalOp) -> Self {
         match value {
             ExternalOp::Opaque(op) => op,
-            ExternalOp::Resource { def, args } => {
-                // There's no way to report a panic here, but serde requires Into not TryInto. Eeeek!
-                let signature = if def.should_serialize_signature() {
-                    // TODO More issues with the input resource set here!
-                    Some(def.signature(&args, &ResourceSet::default()).unwrap())
-                } else {
-                    None
-                };
-                OpaqueOp {
-                    resource: def.resource.clone(),
-                    op_name: def.name.clone(),
-                    description: def.description.clone(),
-                    args,
-                    signature,
-                }
-            }
+            ExternalOp::Resource(op) => op.into(),
         }
     }
 }
@@ -56,8 +41,84 @@ impl From<ExternalOp> for LeafOp {
     }
 }
 
+impl OpName for ExternalOp {
+    fn name(&self) -> SmolStr {
+        let (res_id, op_name) = match self {
+            Self::Opaque(op) => (&op.resource, &op.op_name),
+            Self::Resource(ResourceOp { def, .. }) => (&def.resource, &def.name),
+        };
+        qualify_name(res_id, op_name)
+    }
+}
+
+impl OpTrait for ExternalOp {
+    fn description(&self) -> &str {
+        match self {
+            Self::Opaque(op) => op.description.as_str(),
+            Self::Resource(ResourceOp { def, .. }) => def.description.as_str(),
+        }
+    }
+
+    fn signature_desc(&self) -> SignatureDescription {
+        match self {
+            Self::Opaque(_) => SignatureDescription::default(),
+            Self::Resource(ResourceOp { def, args, .. }) => def.signature_desc(args),
+        }
+    }
+
+    fn tag(&self) -> OpTag {
+        OpTag::DataflowChild
+    }
+
+    /// Note that there is no way to indicate failure here! We could fail in [resolve_extension_ops]?
+    fn signature(&self) -> Signature {
+        match self {
+            Self::Opaque(op) => op.signature.clone().unwrap(),
+            Self::Resource(ResourceOp { signature, .. }) => signature.clone(),
+        }
+    }
+}
+
+// Note *not* Serializable: container (ExternalOp) should have serialized as an OpaqueOp instead.
+#[derive(Clone, Debug)]
+pub struct ResourceOp {
+    def: Arc<OpDef>,
+    args: Vec<TypeArg>,
+    signature: Signature, // Cache
+}
+
+impl Into<OpaqueOp> for ResourceOp {
+    fn into(self) -> OpaqueOp {
+        let ResourceOp {
+            def,
+            args,
+            signature,
+        } = self;
+        let opt_sig = if def.should_serialize_signature() {
+            Some(signature)
+        } else {
+            None
+        };
+        OpaqueOp {
+            resource: def.resource.clone(),
+            op_name: def.name.clone(),
+            description: def.description.clone(),
+            args,
+            signature: opt_sig,
+        }
+    }
+}
+
+impl PartialEq for ResourceOp {
+    fn eq(&self, other: &Self) -> bool {
+        Arc::<OpDef>::ptr_eq(&self.def, &other.def) && self.args == other.args
+    }
+}
+
+impl Eq for ResourceOp {}
+
 /// An opaquely-serialized op that refers to an as-yet-unresolved [`OpDef`]
-#[derive(Clone, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
+#[derive(Clone, Debug, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 pub struct OpaqueOp {
     resource: ResourceId,
     op_name: SmolStr,
@@ -90,62 +151,6 @@ impl OpaqueOp {
     }
 }
 
-impl OpName for ExternalOp {
-    fn name(&self) -> SmolStr {
-        let (res_id, op_name) = match self {
-            Self::Opaque(op) => (&op.resource, &op.op_name),
-            Self::Resource { def, .. } => (&def.resource, &def.name),
-        };
-        qualify_name(res_id, op_name)
-    }
-}
-
-impl OpTrait for ExternalOp {
-    fn description(&self) -> &str {
-        match self {
-            Self::Opaque(op) => op.description.as_str(),
-            Self::Resource { def, .. } => def.description.as_str(),
-        }
-    }
-
-    fn signature_desc(&self) -> SignatureDescription {
-        match self {
-            Self::Opaque(_) => SignatureDescription::default(),
-            Self::Resource { def, args } => def.signature_desc(args),
-        }
-    }
-
-    fn tag(&self) -> OpTag {
-        OpTag::DataflowChild
-    }
-
-    /// Note that there is no way to indicate failure here! We could fail in [resolve_extension_ops]?
-    fn signature(&self) -> Signature {
-        match self {
-            Self::Opaque(op) => op.signature.clone().unwrap(),
-            Self::Resource { def, args } => {
-                // TODO the Resources in inputs and outputs appear in Signature here, so we need to get them from somewhere?
-                // Do we store the input ResourceSet as another field in the CustomOp? If so, we should do the same for *all* ops?
-                def.signature(args, &ResourceSet::default()).unwrap()
-            }
-        }
-    }
-}
-
-impl PartialEq for ExternalOp {
-    fn eq(&self, other: &Self) -> bool {
-        match (self, other) {
-            (Self::Opaque(op1), Self::Opaque(op2)) => op1 == op2,
-            (Self::Resource { def: d1, args: a1 }, Self::Resource { def: d2, args: a2 }) => {
-                Arc::<OpDef>::ptr_eq(d1, d2) && a1 == a2
-            }
-            (_, _) => false,
-        }
-    }
-}
-
-impl Eq for ExternalOp {}
-
 /// Resolve serialized names of operations into concrete implementation (OpDefs) where possible
 #[allow(dead_code)]
 pub fn resolve_extension_ops(h: &mut Hugr, rsrcs: &HashMap<SmolStr, Resource>) {
@@ -157,20 +162,23 @@ pub fn resolve_extension_ops(h: &mut Hugr, rsrcs: &HashMap<SmolStr, Resource>) {
                 let Some(def) = r.operations().get(&opaque.op_name) else {
                     panic!("Conflicting declaration of Resource {}, did not find OpDef for {}", r.name(), opaque.op_name);
                 };
-                // Check Signature is correct if stored
+                // Check Signature is correct if stored. TODO input_resources
+                let computed_sig = def
+                    .signature(&opaque.args, &ResourceSet::default())
+                    .unwrap();
                 if let Some(sig) = &opaque.signature {
-                    let computed_sig = def.signature(&opaque.args, &sig.input_resources).unwrap();
                     if sig != &computed_sig {
                         panic!("Resolved {} to a concrete implementation which computed a conflicting signature: {} vs stored {}", opaque.op_name, computed_sig, sig);
                     };
-                    replacements.push((
-                        n,
-                        ExternalOp::Resource {
-                            def: def.clone(),
-                            args: opaque.args.clone(),
-                        },
-                    ));
-                }
+                };
+                replacements.push((
+                    n,
+                    ExternalOp::Resource(ResourceOp {
+                        def: def.clone(),
+                        args: opaque.args.clone(),
+                        signature: computed_sig,
+                    }),
+                ));
             } else if opaque.signature.is_none() {
                 panic!(
                     "Loaded node with operation {} of unknown resource {} and no stored Signature",
