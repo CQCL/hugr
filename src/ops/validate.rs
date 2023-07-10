a4201ecb5793ab1aad158579a44c28d05d859901
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-07-10 10:39:15 +0100

    Go further - use {case,body}_input_row, body_output_row

--- src/ops/validate.rs
+++ src/ops/validate.rs
@@ -162,11 +162,10 @@ impl ValidateOp for super::Conditional {
         for (i, (child, optype)) in children.into_iter().enumerate() {
             let OpType::Case(case_op) = optype else {panic!("Child check should have already checked valid ops.")};
             let sig = &case_op.signature;
-            let predicate_value = &self.predicate_inputs[i];
-            if sig.input[0..predicate_value.len()] != predicate_value[..]
-                || sig.input[predicate_value.len()..] != self.other_inputs[..]
-                || sig.output != self.outputs
-            {
+            if let Some(expected_inputs) = self.case_input_row(i) {
+                if sig.input == expected_inputs && sig.output == self.outputs {
+                    continue;
+                }
                 return Err(ChildrenValidationError::ConditionalCaseSignature {
                     child,
                     optype: optype.clone(),
@@ -194,18 +193,9 @@ impl ValidateOp for super::TailLoop {
         &self,
         children: impl DoubleEndedIterator<Item = (NodeIndex, &'a OpType)>,
     ) -> Result<(), ChildrenValidationError> {
-        let expected_output =
-            SimpleType::new_predicate(vec![self.just_inputs.clone(), self.just_outputs.clone()]);
-        let mut expected_output = vec![expected_output];
-        expected_output.extend_from_slice(&self.rest);
-        let expected_output: TypeRow = expected_output.into();
-
-        let mut expected_input = self.just_inputs.clone();
-        expected_input.to_mut().extend_from_slice(&self.rest);
-
         validate_io_nodes(
-            &expected_input,
-            &expected_output,
+            &self.body_input_row(),
+            &self.body_output_row(),
             "tail-controlled loop graph",
             children,
         )
