3b8f8f9b9141dbefd228325b5e72152ffcd9c451
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-08-01 17:29:46 +0100

    CustomType's are Simple not necessarily Classic, and report their TypeTag
    CustomType's convert into *correct* SimpleType via Into (like new_tuple)
    Drop .classic_type(), add .tag()

--- src/types/custom.rs
+++ src/types/custom.rs
@@ -6,7 +6,7 @@ use std::fmt::{self, Display};
 
 use crate::resource::ResourceId;
 
-use super::{type_param::TypeArg, ClassicType, Container};
+use super::{type_param::TypeArg, ClassicType, Container, HashableType, SimpleType, TypeTag};
 
 /// An opaque type element. Contains the unique identifier of its definition.
 #[derive(Debug, PartialEq, Eq, Clone, serde::Serialize, serde::Deserialize)]
@@ -21,6 +21,8 @@ pub struct CustomType {
     ///
     /// [`TypeParam`]: super::type_param::TypeParam
     args: Vec<TypeArg>,
+    /// The [TypeTag] describing what can be done to instances of this type
+    tag: TypeTag,
 }
 
 impl CustomType {
@@ -29,17 +31,14 @@ impl CustomType {
         id: impl Into<SmolStr>,
         args: impl Into<Vec<TypeArg>>,
         resource: impl Into<ResourceId>,
+        tag: TypeTag,
     ) -> Self {
         Self {
             id: id.into(),
             args: args.into(),
             resource: resource.into(),
+            tag,
         }
-    }
-
-    /// Returns a [`ClassicType`] containing this opaque type.
-    pub const fn classic_type(self) -> ClassicType {
-        ClassicType::Container(Container::Opaque(self))
     }
 }
 
@@ -66,8 +65,13 @@ impl Display for CustomType {
     }
 }
 
-impl From<CustomType> for ClassicType {
-    fn from(ty: CustomType) -> Self {
-        ty.classic_type()
+/// This parallels [SimpleType::new_tuple] and [SimpleType::new_sum]
+impl From<CustomType> for SimpleType {
+    fn from(value: CustomType) -> Self {
+        match value.tag {
+            TypeTag::Simple => SimpleType::Qontainer(Container::Opaque(value)),
+            TypeTag::Classic => ClassicType::Container(Container::Opaque(value)).into(),
+            TypeTag::Hashable => HashableType::Container(Container::Opaque(value)).into(),
+        }
     }
 }
