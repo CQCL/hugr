49eea58f9fe18e93d5d92e7ee1a0adc036aea514
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-08-15 16:40:00 +0100

    rename `rotation::Type` -> `RotationType`

--- src/extensions/rotation.rs
+++ src/extensions/rotation.rs
@@ -12,8 +12,7 @@ use pyo3::prelude::*;
 
 use crate::resource::ResourceSet;
 use crate::types::type_param::TypeArg;
-use crate::types::Type as HugrType;
-use crate::types::{CustomCheckFail, TypeRow};
+use crate::types::{CustomCheckFail, Type, TypeRow};
 use crate::types::{CustomType, TypeBound};
 use crate::values::CustomConst;
 use crate::Resource;
@@ -24,8 +23,8 @@ pub const RESOURCE_ID: SmolStr = SmolStr::new_inline("rotations");
 pub fn resource() -> Resource {
     let mut resource = Resource::new(RESOURCE_ID);
 
-    Type::Angle.add_to_resource(&mut resource);
-    Type::Quaternion.add_to_resource(&mut resource);
+    RotationType::Angle.add_to_resource(&mut resource);
+    RotationType::Quaternion.add_to_resource(&mut resource);
 
     resource
         .add_op_custom_sig_simple(
@@ -33,7 +32,8 @@ pub fn resource() -> Resource {
             "".into(),
             vec![],
             |_arg_values: &[TypeArg]| {
-                let t: TypeRow = vec![HugrType::new_extension(Type::Angle.custom_type())].into();
+                let t: TypeRow =
+                    vec![Type::new_extension(RotationType::Angle.custom_type())].into();
                 Ok((t.clone(), t, ResourceSet::default()))
             },
         )
@@ -44,23 +44,23 @@ pub fn resource() -> Resource {
 
 /// Custom types defined by this extension.
 #[derive(Clone, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
-pub enum Type {
+pub enum RotationType {
     Angle,
     Quaternion,
 }
 
-impl Type {
+impl RotationType {
     pub const fn name(&self) -> SmolStr {
         match self {
-            Type::Angle => SmolStr::new_inline("angle"),
-            Type::Quaternion => SmolStr::new_inline("quat"),
+            RotationType::Angle => SmolStr::new_inline("angle"),
+            RotationType::Quaternion => SmolStr::new_inline("quat"),
         }
     }
 
     pub const fn description(&self) -> &str {
         match self {
-            Type::Angle => "Floating point angle",
-            Type::Quaternion => "Quaternion specifying rotation.",
+            RotationType::Angle => "Floating point angle",
+            RotationType::Quaternion => "Quaternion specifying rotation.",
         }
     }
 
@@ -80,8 +80,8 @@ impl Type {
     }
 }
 
-impl From<Type> for CustomType {
-    fn from(ty: Type) -> Self {
+impl From<RotationType> for CustomType {
+    fn from(ty: RotationType) -> Self {
         ty.custom_type()
     }
 }
@@ -94,10 +94,10 @@ pub enum Constant {
 }
 
 impl Constant {
-    fn rotation_type(&self) -> Type {
+    fn rotation_type(&self) -> RotationType {
         match self {
-            Constant::Angle(_) => Type::Angle,
-            Constant::Quaternion(_) => Type::Quaternion,
+            Constant::Angle(_) => RotationType::Angle,
+            Constant::Quaternion(_) => RotationType::Quaternion,
         }
     }
 }
