48093d7ccb4f88bfd5df1912abbc9ba9a4d81485
Alec Edgington <alec.edgington@quantinuum.com>
2023-08-17 12:59:03 +0100

    Implement CustomConst for floats.

--- src/extensions/arithmetic/float_types.rs
+++ src/extensions/arithmetic/float_types.rs
@@ -3,7 +3,8 @@
 use smol_str::SmolStr;
 
 use crate::{
-    types::{CustomType, Type, TypeBound},
+    types::{CustomCheckFailure, CustomType, Type, TypeBound},
+    values::CustomConst,
     Resource,
 };
 
@@ -13,14 +14,45 @@ pub const RESOURCE_ID: SmolStr = SmolStr::new_inline("arithmetic.float.types");
 /// Identfier for the 64-bit IEEE 754-2019 floating-point type.
 const FLOAT_TYPE_ID: SmolStr = SmolStr::new_inline("float64");
 
+fn float64_custom_type() -> CustomType {
+    CustomType::new(FLOAT_TYPE_ID, [], RESOURCE_ID, TypeBound::Copyable)
+}
+
 /// 64-bit IEEE 754-2019 floating-point type
 pub fn float64_type() -> Type {
-    Type::new_extension(CustomType::new(
-        FLOAT_TYPE_ID,
-        [],
-        RESOURCE_ID,
-        TypeBound::Copyable,
-    ))
+    Type::new_extension(float64_custom_type())
+}
+
+#[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
+/// A floating-point value.
+pub struct ConstF64(f64);
+
+impl ConstF64 {
+    /// Create a new [`ConstF64`]
+    pub fn new(value: f64) -> Self {
+        Self(value)
+    }
+}
+
+#[typetag::serde]
+impl CustomConst for ConstF64 {
+    fn name(&self) -> SmolStr {
+        format!("f64({})", self.0).into()
+    }
+
+    fn check_custom_type(&self, typ: &CustomType) -> Result<(), CustomCheckFailure> {
+        if typ.clone() == float64_custom_type() {
+            Ok(())
+        } else {
+            Err(CustomCheckFailure::Message(
+                "Floating-point constant type mismatch.".into(),
+            ))
+        }
+    }
+
+    fn equal_consts(&self, other: &dyn CustomConst) -> bool {
+        crate::values::downcast_equal_consts(self, other)
+    }
 }
 
 /// Resource for basic floating-point types.
@@ -49,5 +81,13 @@ mod test {
         assert_eq!(r.name(), "arithmetic.float.types");
         assert_eq!(r.types().count(), 1);
         assert_eq!(r.operations().count(), 0);
+    }
+
+    #[test]
+    fn test_float_consts() {
+        let const_f64_1 = ConstF64::new(1.0);
+        let const_f64_2 = ConstF64::new(2.0);
+        assert_ne!(const_f64_1, const_f64_2);
+        assert_eq!(const_f64_1, ConstF64::new(1.0));
     }
 }
