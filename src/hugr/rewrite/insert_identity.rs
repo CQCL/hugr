8d976e83ad60498a0699c6b78495dbeb4bc34208
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-08-31 10:43:47 +0100

    implement noop node adding

--- src/hugr/rewrite/insert_identity.rs
+++ src/hugr/rewrite/insert_identity.rs
@@ -1,8 +1,8 @@
 //! Implementation of the `InsertIdentity` operation.
 
-use crate::extension::prelude::QB_T;
 use crate::hugr::{HugrMut, Node};
 use crate::ops::LeafOp;
+use crate::types::EdgeKind;
 use crate::{Hugr, HugrView, Port};
 
 use super::Rewrite;
@@ -35,9 +35,9 @@ pub enum IdentityInsertionError {
     /// Invalid node.
     #[error("Node is invalid.")]
     InvalidNode(),
-    /// Invalid port.
-    #[error("post_port is invalid.")]
-    InvalidPort(),
+    /// Invalid port kind.
+    #[error("post_port has invalid kind {0:?}. Must be Value.")]
+    InvalidPortKind(Option<EdgeKind>),
 }
 
 impl Rewrite for IdentityInsertion {
@@ -49,9 +49,10 @@ impl Rewrite for IdentityInsertion {
         /*
         Assumptions:
         1. Value kind inputs can only have one connection.
+        2. Node exists.
         Conditions:
-        1. post_port is Value kind
-        2. post_port is connected to a sibling of post_node
+        1. post_port is Value kind.
+        2. post_port is connected to a sibling of post_node.
          */
 
         unimplemented!()
@@ -61,16 +62,34 @@ impl Rewrite for IdentityInsertion {
             .linked_ports(self.post_node, self.post_port)
             .exactly_one()
             .expect("Value kind input can only have one connection.");
+
+        let kind = h.get_optype(self.post_node).port_kind(self.post_port);
+        let Some(EdgeKind::Value(ty)) = kind else {
+            return Err(IdentityInsertionError::InvalidPortKind(kind));
+        };
+
         h.disconnect(self.post_node, self.post_port).unwrap();
-        let new_node = h.add_op(LeafOp::Noop { ty: QB_T });
+        let new_node = h.add_op(LeafOp::Noop { ty });
+        h.connect(pre_node, pre_port.index(), new_node, 0)
+            .expect("Should only fail if ports don't exist.");
+
         // TODO Check type, insert Noop...
-        h.connect(
-            pre_node,
-            pre_port.index(),
-            self.post_node,
-            self.post_port.index(),
-        )
-        .unwrap();
+        h.connect(new_node, 0, self.post_node, self.post_port.index())
+            .expect("Should only fail if ports don't exist.");
         Ok(new_node)
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::{fixture, rstest};
+
+    use crate::Hugr;
+
+    #[fixture]
+    fn sample() -> Hugr {
+        todo!()
+    }
+    #[rstest]
+    fn correct_insertion() {}
 }
