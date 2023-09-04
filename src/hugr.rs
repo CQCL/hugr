0069a05429c93c45f52e42c01fb3f198ab66ebf2
Luca Mondada <luca@mondada.net>
2023-09-04 13:11:35 +0200

    feat: Port is serializable

--- src/hugr.rs
+++ src/hugr.rs
@@ -179,6 +179,7 @@ pub struct Node {
 }
 
 /// A handle to a port for a node in the HUGR.
+#[serde(transparent)]
 #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Hash, Default, Debug, From)]
 #[cfg_attr(feature = "pyo3", pyclass)]
 pub struct Port {
