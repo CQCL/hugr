07b3d3f3ba9619131662845290bd0e8ef6842a93
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-09-29 11:09:27 +0100

    common up

--- src/hugr/views/sibling.rs
+++ src/hugr/views/sibling.rs
@@ -9,10 +9,9 @@ use portgraph::{LinkView, MultiPortGraph, PortIndex, PortView};
 use crate::hugr::hugrmut::sealed::HugrMutInternals;
 use crate::hugr::{HugrError, HugrMut};
 use crate::ops::handle::NodeHandle;
-use crate::ops::OpTrait;
 use crate::{Direction, Hugr, Node, Port};
 
-use super::{sealed::HugrInternals, HierarchyView, HugrView};
+use super::{check_tag, sealed::HugrInternals, HierarchyView, HugrView};
 
 type FlatRegionGraph<'g> = portgraph::view::FlatRegion<'g, &'g MultiPortGraph>;
 
@@ -195,14 +194,7 @@ where
     Root: NodeHandle,
 {
     fn try_new(hugr: &'a impl HugrView, root: Node) -> Result<Self, HugrError> {
-        hugr.valid_node(root)?;
-        let actual = hugr.get_optype(root).tag();
-        if !Root::TAG.is_superset(actual) {
-            return Err(HugrError::InvalidTag {
-                required: Root::TAG,
-                actual,
-            });
-        }
+        check_tag::<Root>(hugr, root)?;
         Ok(Self::new_unchecked(hugr, root))
     }
 }
@@ -255,14 +247,7 @@ impl<'g, Root: NodeHandle> SiblingMut<'g, Root> {
     /// Create a new SiblingMut from a base.
     /// Equivalent to [HierarchyView::try_new] but takes a *mutable* reference.
     pub fn try_new(hugr: &'g mut impl HugrMut, root: Node) -> Result<Self, HugrError> {
-        hugr.valid_node(root)?;
-        let actual = hugr.get_optype(root).tag();
-        if !Root::TAG.is_superset(actual) {
-            return Err(HugrError::InvalidTag {
-                required: Root::TAG,
-                actual,
-            });
-        }
+        check_tag::<Root>(hugr, root)?;
         Ok(Self {
             hugr: hugr.hugr_mut(),
             root,
