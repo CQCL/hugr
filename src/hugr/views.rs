54eebfe1dc3786eda0f62411a1edc3781bd0f8b7
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2023-10-06 09:11:49 +0100

    Comment about no AsMut

--- src/hugr/views.rs
+++ src/hugr/views.rs
@@ -335,6 +335,8 @@ impl<H: AsRef<Hugr>, Root: NodeHandle> RootTagged for RootChecked<H, Root> {
     type RootHandle = Root;
 }
 
+// Note do not implement AsMut<Hugr> - that would get us the impl HugrMut
+// for unwrapped Hugrs, which would not check the root node OpTag.
 impl<H: AsRef<Hugr>, Root> AsRef<Hugr> for RootChecked<H, Root> {
     fn as_ref(&self) -> &Hugr {
         self.0.as_ref()
