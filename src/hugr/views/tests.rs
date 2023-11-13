36e62af85a237c9ce32a8331f9d07e0e66a85755
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2023-11-13 11:47:04 +0000

    test: use insta snapshots to add dot_string coverage

--- src/hugr/views/tests.rs
+++ src/hugr/views/tests.rs
@@ -1,30 +1,43 @@
 use portgraph::PortOffset;
+use rstest::{fixture, rstest};
 
 use crate::{
-    builder::{BuildError, DFGBuilder, Dataflow, DataflowHugr},
+    builder::{BuildError, BuildHandle, Container, DFGBuilder, Dataflow, DataflowHugr},
     extension::prelude::QB_T,
-    ops::handle::NodeHandle,
+    ops::handle::{DataflowOpID, NodeHandle},
     type_row,
     types::FunctionType,
     utils::test_quantum_extension::cx_gate,
-    HugrView,
+    Hugr, HugrView,
 };
 
-#[test]
-fn node_connections() -> Result<(), BuildError> {
+#[fixture]
+fn sample_hugr() -> (Hugr, BuildHandle<DataflowOpID>, BuildHandle<DataflowOpID>) {
     let mut dfg = DFGBuilder::new(FunctionType::new(
         type_row![QB_T, QB_T],
         type_row![QB_T, QB_T],
-    ))?;
+    ))
+    .unwrap();
 
     let [q1, q2] = dfg.input_wires_arr();
 
-    let n1 = dfg.add_dataflow_op(cx_gate(), [q1, q2])?;
+    let n1 = dfg.add_dataflow_op(cx_gate(), [q1, q2]).unwrap();
     let [q1, q2] = n1.outputs_arr();
-    let n2 = dfg.add_dataflow_op(cx_gate(), [q2, q1])?;
+    let n2 = dfg.add_dataflow_op(cx_gate(), [q2, q1]).unwrap();
+    dfg.add_other_wire(n1.node(), n2.node()).unwrap();
 
-    let h = dfg.finish_prelude_hugr_with_outputs(n2.outputs())?;
+    (
+        dfg.finish_prelude_hugr_with_outputs(n2.outputs()).unwrap(),
+        n1,
+        n2,
+    )
+}
 
+#[rstest]
+fn node_connections(
+    sample_hugr: (Hugr, BuildHandle<DataflowOpID>, BuildHandle<DataflowOpID>),
+) -> Result<(), BuildError> {
+    let (h, n1, n2) = sample_hugr;
     let connections: Vec<_> = h.node_connections(n1.node(), n2.node()).collect();
 
     assert_eq!(
@@ -38,7 +51,19 @@ fn node_connections() -> Result<(), BuildError> {
                 PortOffset::new_outgoing(1).into(),
                 PortOffset::new_incoming(0).into()
             ],
+            // order edge
+            [
+                PortOffset::new_outgoing(2).into(),
+                PortOffset::new_incoming(2).into()
+            ],
         ]
     );
     Ok(())
+}
+
+#[rstest]
+fn dot_string(sample_hugr: (Hugr, BuildHandle<DataflowOpID>, BuildHandle<DataflowOpID>)) {
+    let (h, _, _) = sample_hugr;
+
+    insta::assert_yaml_snapshot!(h.dot_string());
 }
