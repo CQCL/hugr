d5e7d63f11d2a58ebebb86b4522f2d089f453550
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-01-03 11:36:01 +0000

    feat: implement RemoveConst and RemoveConstIgnore (#757)
    as per spec
    
    refactor!: allow Into<Const> for builder.add_const
    
    BREAKING_CHANGES: existing CustomConst.into() calls will error
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Alan Lawrence <alan.lawrence@cambridgequantum.com>
    Co-authored-by: Luca Mondada <72734770+lmondada@users.noreply.github.com>
    Co-authored-by: Luca Mondada <luca@mondada.net>

--- src/hugr/views/tests.rs
+++ src/hugr/views/tests.rs
@@ -140,7 +140,7 @@ fn static_targets() {
     )
     .unwrap();
 
-    let c = dfg.add_constant(ConstUsize::new(1).into()).unwrap();
+    let c = dfg.add_constant(ConstUsize::new(1)).unwrap();
 
     let load = dfg.load_const(&c).unwrap();
 
