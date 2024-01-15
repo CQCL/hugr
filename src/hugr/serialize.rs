b7543db4ee776d8985145103c713036612ee9e05
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-01-15 13:32:24 +0000

    fix: include schema rather than read file (#807)
    otherwise fails miri checks by opening file
    
    running workflow manually to check

--- src/hugr/serialize.rs
+++ src/hugr/serialize.rs
@@ -274,20 +274,16 @@ pub mod test {
     use portgraph::{
         multiportgraph::MultiPortGraph, Hierarchy, LinkMut, PortMut, PortView, UnmanagedDenseMap,
     };
-    use std::fs;
 
     const NAT: Type = crate::extension::prelude::USIZE_T;
     const QB: Type = crate::extension::prelude::QB_T;
 
     lazy_static! {
         static ref SCHEMA: JSONSchema = {
-            let path = format!(
-                "{}/specification/schema/hugr_schema_v0.json",
-                env!("CARGO_MANIFEST_DIR")
-            );
-            let data = fs::read_to_string(path).unwrap();
-            let schema_val: serde_json::Value = serde_json::from_str(&data).unwrap();
-
+            let schema_val: serde_json::Value = serde_json::from_str(include_str!(
+                "../../specification/schema/hugr_schema_v0.json"
+            ))
+            .unwrap();
             JSONSchema::options()
                 .with_draft(Draft::Draft7)
                 .compile(&schema_val)
