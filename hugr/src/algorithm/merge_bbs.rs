9102fe858bec68b1bbbef6a14baf88a04e64c1ec
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2024-04-15 17:54:57 +0100

    Start on test (just build input CFG)

--- hugr/src/algorithm/merge_bbs.rs
+++ hugr/src/algorithm/merge_bbs.rs
@@ -90,3 +90,65 @@ fn mk_rep(
     };
     (rep, dfg1, dfg2)
 }
+
+#[cfg(test)]
+mod test {
+    use crate::builder::{CFGBuilder, Dataflow, HugrBuilder};
+    use crate::extension::prelude::{QB_T, USIZE_T};
+    use crate::extension::{ExtensionRegistry, ExtensionSet, PRELUDE};
+    use crate::ops::{Const, Noop};
+    use crate::types::{FunctionType, Type, TypeRow};
+    use crate::{const_extension_ids, type_row, Extension};
+
+    const_extension_ids! {
+        const EXT_ID: ExtensionId = "TestExt";
+    }
+    #[test]
+    fn in_loop() -> Result<(), Box<dyn std::error::Error>> {
+        /* Noop -> Test -> Exit      NoopTest -> Exit
+           |       |            =>   |    |
+           \-<---<-/                 \<-<-/
+        */
+        let loop_variants = type_row![QB_T];
+        let exit_types = type_row![USIZE_T];
+        let mut e = Extension::new(EXT_ID);
+        e.add_op(
+            "Test".into(),
+            String::new(),
+            FunctionType::new(
+                loop_variants.clone(),
+                TypeRow::from(vec![Type::new_sum(vec![
+                    loop_variants.clone(),
+                    exit_types.clone(),
+                ])]),
+            ),
+        )?;
+        let reg = ExtensionRegistry::try_new([PRELUDE.to_owned(), e])?;
+        let e = reg.get(&EXT_ID.to_string()).unwrap();
+        let mut h = CFGBuilder::new(FunctionType::new(loop_variants.clone(), exit_types.clone()))?;
+        let mut noop_block =
+            h.simple_entry_builder(loop_variants.clone(), 1, ExtensionSet::new())?;
+        let n = noop_block.add_dataflow_op(Noop { ty: QB_T }, noop_block.input_wires())?;
+        let br = noop_block.add_load_const(Const::unary_unit_sum());
+        let noop_block = noop_block.finish_with_outputs(br, n.outputs())?;
+        let mut test_block = h.block_builder(
+            loop_variants.clone(),
+            vec![loop_variants.clone(), exit_types],
+            ExtensionSet::singleton(&EXT_ID),
+            type_row![],
+        )?;
+        let [tst] = test_block
+            .add_dataflow_op(
+                e.instantiate_extension_op("Test", [], &reg)?,
+                test_block.input_wires(),
+            )?
+            .outputs_arr();
+        let test_block = test_block.finish_with_outputs(tst, [])?;
+        let exit_block = h.exit_block();
+        h.branch(&noop_block, 0, &test_block)?;
+        h.branch(&test_block, 0, &noop_block)?;
+        h.branch(&test_block, 1, &exit_block)?;
+        h.finish_hugr(&reg)?;
+        Ok(())
+    }
+}
