ef51577cbb12a97d21e7677a25086e23adc1003e
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2024-04-15 15:33:42 +0100

    Refactor: mk_rep takes immutable view only

--- hugr/src/algorithm/merge_bbs.rs
+++ hugr/src/algorithm/merge_bbs.rs
@@ -6,9 +6,9 @@ use itertools::Itertools;
 
 use crate::hugr::rewrite::inline_dfg::InlineDFG;
 use crate::hugr::rewrite::replace::{NewEdgeKind, NewEdgeSpec, Replacement};
+use crate::hugr::{HugrMut, RootTagged};
 use crate::ops::{handle::CfgID, DataflowBlock, DataflowParent, DFG};
-use crate::Node;
-use crate::{hugr::HugrMut, Hugr, HugrView};
+use crate::{Hugr, HugrView, Node};
 
 /// Merge any basic blocks that are direct children of the specified CFG
 /// i.e. where a basic block B has a single successor B' whose only predecessor
@@ -22,11 +22,20 @@ pub fn merge_basic_blocks(cfg: &mut impl HugrMut<RootHandle = CfgID>) {
             continue;
         };
         assert_eq!(n, p);
-        merge_bbs(cfg, n, succ);
+        let (rep, dfg1, dfg2) = mk_rep(cfg, n, succ);
+        let node_map = cfg.apply_rewrite(rep).unwrap();
+        for dfg_id in [dfg1, dfg2] {
+            let n_id = *node_map.get(&dfg_id).unwrap();
+            cfg.apply_rewrite(InlineDFG(n_id.into())).unwrap();
+        }
     }
 }
 
-fn merge_bbs(cfg: &mut impl HugrMut<RootHandle = CfgID>, pred: Node, succ: Node) {
+fn mk_rep(
+    cfg: &impl RootTagged<RootHandle = CfgID>,
+    pred: Node,
+    succ: Node,
+) -> (Replacement, Node, Node) {
     let pred_ty = cfg.get_optype(pred).as_dataflow_block().unwrap();
     let succ_ty = cfg.get_optype(succ).as_dataflow_block().unwrap().clone();
     let signature = succ_ty.inner_signature().clone();
@@ -52,37 +61,32 @@ fn merge_bbs(cfg: &mut impl HugrMut<RootHandle = CfgID>, pred: Node, succ: Node)
     for (i, _) in pred_ty.inner_signature().output().iter().enumerate() {
         replacement.connect(dfg1, i, dfg2, i)
     }
-    let node_map = cfg
-        .apply_rewrite(Replacement {
-            removal: vec![pred, succ],
-            replacement,
-            adoptions: HashMap::from([(dfg1, pred), (dfg2, succ)]),
-            mu_inp: cfg
-                .all_linked_outputs(pred)
-                .map(|(src, src_pos)| NewEdgeSpec {
-                    src,
-                    tgt: merged,
-                    kind: NewEdgeKind::ControlFlow { src_pos },
-                })
-                .collect(),
-            mu_out: cfg
-                .node_outputs(succ)
-                .map(|src_pos| NewEdgeSpec {
-                    src: merged,
-                    tgt: cfg
-                        .linked_inputs(succ, src_pos)
-                        .exactly_one()
-                        .ok()
-                        .unwrap()
-                        .0,
-                    kind: NewEdgeKind::ControlFlow { src_pos },
-                })
-                .collect(),
-            mu_new: vec![],
-        })
-        .unwrap();
-    for dfg_id in [dfg1, dfg2] {
-        let n_id = *node_map.get(&dfg_id).unwrap();
-        cfg.apply_rewrite(InlineDFG(n_id.into())).unwrap();
-    }
+    let rep = Replacement {
+        removal: vec![pred, succ],
+        replacement,
+        adoptions: HashMap::from([(dfg1, pred), (dfg2, succ)]),
+        mu_inp: cfg
+            .all_linked_outputs(pred)
+            .map(|(src, src_pos)| NewEdgeSpec {
+                src,
+                tgt: merged,
+                kind: NewEdgeKind::ControlFlow { src_pos },
+            })
+            .collect(),
+        mu_out: cfg
+            .node_outputs(succ)
+            .map(|src_pos| NewEdgeSpec {
+                src: merged,
+                tgt: cfg
+                    .linked_inputs(succ, src_pos)
+                    .exactly_one()
+                    .ok()
+                    .unwrap()
+                    .0,
+                kind: NewEdgeKind::ControlFlow { src_pos },
+            })
+            .collect(),
+        mu_new: vec![],
+    };
+    (rep, dfg1, dfg2)
 }
