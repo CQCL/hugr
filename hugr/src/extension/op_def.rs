266eb41f0361f82ad87a089e0ca467a403aa8e31
Craig Roy <craig.roy@cambridgequantum.com>
2024-04-16 10:53:51 +0100

    test: Add a test of instantiating an extension set

--- hugr/src/extension/op_def.rs
+++ hugr/src/extension/op_def.rs
@@ -635,4 +635,34 @@ mod test {
         );
         Ok(())
     }
+
+    #[test]
+    fn instantiate_extension_delta() -> Result<(), Box<dyn std::error::Error>> {
+        use crate::extension::prelude::{BOOL_T, PRELUDE_REGISTRY};
+
+        let mut e = Extension::new(EXT_ID);
+        let ext_id = e.name().clone();
+
+        let es = ExtensionSet::singleton(&ext_id);
+        let params: Vec<TypeParam> = vec![TypeParam::Extensions];
+        let args = [TypeArg::Extensions { es: es.clone() }];
+        let mut db_set = ExtensionSet::new();
+        db_set.insert_type_var(0);
+        let fun_ty = FunctionType::new_endo(vec![BOOL_T]).with_extension_delta(db_set);
+
+        let exp_fun_ty = FunctionType::new_endo(vec![BOOL_T]).with_extension_delta(es);
+
+        let def = e.add_op(
+            "SimpleOp".into(),
+            "".into(),
+            PolyFuncType::new(params.clone(), fun_ty),
+        )?;
+        def.validate_args(&args, &PRELUDE_REGISTRY, &params)
+            .unwrap();
+        assert_eq!(
+            def.compute_signature(&args, &PRELUDE_REGISTRY),
+            Ok(exp_fun_ty)
+        );
+        Ok(())
+    }
 }
