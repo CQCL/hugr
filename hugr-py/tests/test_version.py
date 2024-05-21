024cacc42e8e3575576b6991aafb3520447b7dac
Douglas Wilson <douglas.wilson@quantinuum.com>
2024-05-21 09:26:27 +0100

    very important change

--- hugr-py/tests/test_version.py
+++ hugr-py/tests/test_version.py
@@ -7,6 +7,8 @@ import hugr
 def test_versions_are_in_sync():
     """Checks if the pyproject.toml and package.__init__.py __version__ are in sync."""
 
+    # very important change
+
     path = Path(__file__).resolve().parents[1] / "pyproject.toml"
     pyproject = toml.loads(open(str(path)).read())
     pyproject_version = pyproject["tool"]["poetry"]["version"]
