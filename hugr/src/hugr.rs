46f05bdd4b4a2e3558b5396c81a37300f3b0c360
Agustin Borgna <agustin.borgna@quantinuum.com>
2024-05-29 12:04:03 +0100

    fix: Missing re-exports in `hugr::hugr`

--- hugr/src/hugr.rs
+++ hugr/src/hugr.rs
@@ -2,6 +2,7 @@
 
 // Exports everything except the `internal` module.
 pub use hugr_core::hugr::{
-    hugrmut, rewrite, serialize, validate, views, Hugr, HugrError, NodeMetadata, NodeMetadataMap,
-    NodeType, DEFAULT_NODETYPE,
+    hugrmut, rewrite, serialize, validate, views, Hugr, HugrError, HugrView, IdentList,
+    InvalidIdentifier, NodeMetadata, NodeMetadataMap, NodeType, Rewrite, RootTagged,
+    SimpleReplacement, SimpleReplacementError, ValidationError, DEFAULT_NODETYPE,
 };
