4d9efd8d1881bb90d6e13c46e87e79723f003ca4
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-07-02 11:27:27 +0100

    docs: add redirect from landing to main docs

--- hugr-py/docs/build.sh
+++ hugr-py/docs/build.sh
@@ -3,5 +3,6 @@
 mkdir build
 
 touch build/.nojekyll  # Disable jekyll to keep files starting with underscores
-
+# copy redirect file
+cp ./_static/_redirect.html ./build/index.html
 sphinx-multiversion ./api-docs ./build
