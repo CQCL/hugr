fc71a445bcc6429c76ae14387f34e7c054e41aed
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-07-02 11:17:07 +0100

    docs: add docs publishing + multiversion

--- hugr-py/docs/build.sh
+++ hugr-py/docs/build.sh
@@ -4,4 +4,4 @@ mkdir build
 
 touch build/.nojekyll  # Disable jekyll to keep files starting with underscores
 
-sphinx-build -b html ./api-docs ./build/api-docs
+sphinx-multiversion ./api-docs ./build
