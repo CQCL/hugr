8a00a961e3b1c84840d04a331acedbc082fbfd27
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-07-05 14:49:45 +0100

    refactor!: rename `HugrView` function type methods + simplify behaviour
    Closes #957
    
    - `mono_fn_type` now enforces function value monomorphism
    - get_df_function_type -> inner_function_type so it matches the actual op function being called more obviously.
    - The culprit `get_function_type` changed to `poly_func_type` which _only_ reports polymorphic typr from func defn/decl
    
    refactor(hugr-core): rename `get_df_function_type`
    BREAKING-CHANGE: rename `get_df_function_type` to `inner_function_type`
    
    refactor(hugr-core): `get_function_type` replaced with `poly_func_type` which only reports polymorphic function type for Func(Defn/Decl) rooted HUGRs

--- hugr-core/src/hugr/views.rs
+++ hugr-core/src/hugr/views.rs
@@ -332,11 +332,10 @@ pub trait HugrView: HugrInternals {
     /// signature corresponding to the input and output node of its sibling
     /// graph. Otherwise, returns `None`.
     ///
-    /// In contrast to [`get_function_type`][HugrView::get_function_type], this
+    /// In contrast to [`poly_func_type`][HugrView::poly_func_type], this
     /// method always return a concrete [`FunctionType`].
-    fn get_df_function_type(&self) -> Option<FunctionType> {
-        let op = self.get_optype(self.root());
-        op.inner_function_type()
+    fn inner_function_type(&self) -> Option<FunctionType> {
+        self.root_type().inner_function_type()
     }
 
     /// Returns the function type defined by this HUGR.
@@ -349,12 +348,11 @@ pub trait HugrView: HugrInternals {
     /// of the function.
     ///
     /// Otherwise, returns `None`.
-    fn get_function_type(&self) -> Option<PolyFuncType> {
-        let op = self.get_optype(self.root());
-        match op {
+    fn poly_func_type(&self) -> Option<PolyFuncType> {
+        match self.root_type() {
             OpType::FuncDecl(decl) => Some(decl.signature.clone()),
             OpType::FuncDefn(defn) => Some(defn.signature.clone()),
-            _ => op.inner_function_type().map(PolyFuncType::from),
+            _ => None,
         }
     }
 
