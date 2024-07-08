62caa55d86d87468bb298d201e869314f08bc955
Douglas Wilson <douglas.wilson@quantinuum.com>
2024-07-08 13:18:45 +0100

    feat: make op members consistently public, expose `DataflowOpTrait`

--- hugr-core/src/ops/dataflow.rs
+++ hugr-core/src/ops/dataflow.rs
@@ -10,9 +10,15 @@ use crate::IncomingPort;
 #[cfg(test)]
 use ::proptest_derive::Arbitrary;
 
-pub(crate) trait DataflowOpTrait {
+/// Trait implemented by all dataflow operations.
+pub trait DataflowOpTrait {
+    /// Tag identifying the operation.
     const TAG: OpTag;
+
+    /// A human-readable description of the operation.
     fn description(&self) -> &str;
+
+    /// The signature of the operation.
     fn signature(&self) -> FunctionType;
 
     /// The edge kind for the non-dataflow or constant inputs of the operation,
@@ -158,10 +164,12 @@ impl<T: DataflowOpTrait> StaticTag for T {
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct Call {
-    /// Signature of function being called
-    func_sig: PolyFuncType,
-    type_args: Vec<TypeArg>,
-    instantiation: FunctionType, // Cache, so we can fail in try_new() not in signature()
+    /// Signature of function being called.
+    pub func_sig: PolyFuncType,
+    /// The type arguments that instantiate `func_sig`.
+    pub type_args: Vec<TypeArg>,
+    /// The instantiation of `func_sig`.
+    pub instantiation: FunctionType, // Cache, so we can fail in try_new() not in signature()
 }
 impl_op_name!(Call);
 
@@ -329,9 +337,11 @@ impl LoadConstant {
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct LoadFunction {
     /// Signature of the function
-    func_sig: PolyFuncType,
-    type_args: Vec<TypeArg>,
-    signature: FunctionType, // Cache, so we can fail in try_new() not in signature()
+    pub func_sig: PolyFuncType,
+    /// The type arguments that instantiate `func_sig`.
+    pub type_args: Vec<TypeArg>,
+    /// The instantiation of `func_sig`.
+    pub signature: FunctionType, // Cache, so we can fail in try_new() not in signature()
 }
 impl_op_name!(LoadFunction);
 impl DataflowOpTrait for LoadFunction {
