3e9ad7e934a330ca5c71091ce72c3b3ff35a3368
Lukas Heidemann <lukas@heidemann.me>
2024-07-16 12:47:24 +0100

    feat(cli-subcommands): Created `validate` CLI subcommand.
    Co-authored-by: Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
    
    BREAKING CHANGE:
    - Use `hugr validate` CLI command insead of just `hugr`.

--- hugr-cli/src/lib.rs
+++ hugr-cli/src/lib.rs
@@ -1,68 +1,31 @@
 //! Standard command line tools, used by the hugr binary.
 
-use clap_stdin::FileOrStdin;
-use clap_verbosity_flag::{InfoLevel, Verbosity};
+use std::ffi::OsString;
 use thiserror::Error;
 /// We reexport some clap types that are used in the public API.
 pub use {clap::Parser, clap_verbosity_flag::Level};
 
-use hugr_core::{extension::ExtensionRegistry, Hugr, HugrView};
+pub mod validate;
 
-/// Validate and visualise a HUGR file.
+/// CLI arguments.
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", long_about = None)]
-#[clap(about = "Validate a HUGR.")]
+#[clap(about = "HUGR CLI tools.")]
 #[group(id = "hugr")]
-pub struct CmdLineArgs {
-    input: FileOrStdin,
-    /// Visualise with mermaid.
-    #[arg(short, long, value_name = "MERMAID", help = "Visualise with mermaid.")]
-    mermaid: bool,
-    /// Skip validation.
-    #[arg(short, long, help = "Skip validation.")]
-    no_validate: bool,
-    /// Verbosity.
-    #[command(flatten)]
-    verbose: Verbosity<InfoLevel>,
-    // TODO YAML extensions
+#[non_exhaustive]
+pub enum CliArgs {
+    /// Validate and visualize a HUGR file.
+    Validate(validate::CliArgs),
+    /// External commands
+    #[command(external_subcommand)]
+    External(Vec<OsString>),
 }
 
 /// Error type for the CLI.
-#[derive(Error, Debug)]
+#[derive(Debug, Error)]
+#[error(transparent)]
+#[non_exhaustive]
 pub enum CliError {
-    /// Error reading input.
-    #[error("Error reading input: {0}")]
-    Input(#[from] clap_stdin::StdinError),
-    /// Error parsing input.
-    #[error("Error parsing input: {0}")]
-    Parse(#[from] serde_json::Error),
-    /// Error validating HUGR.
-    #[error("Error validating HUGR: {0}")]
-    Validate(#[from] hugr_core::hugr::ValidationError),
-}
-
-/// String to print when validation is successful.
-pub const VALID_PRINT: &str = "HUGR valid!";
-
-impl CmdLineArgs {
-    /// Run the HUGR cli and validate against an extension registry.
-    pub fn run(&self, registry: &ExtensionRegistry) -> Result<Hugr, CliError> {
-        let mut hugr: Hugr = serde_json::from_reader(self.input.clone().into_reader()?)?;
-        if self.mermaid {
-            println!("{}", hugr.mermaid_string());
-        }
-
-        if !self.no_validate {
-            hugr.update_validate(registry)?;
-            if self.verbosity(Level::Info) {
-                eprintln!("{}", VALID_PRINT);
-            }
-        }
-        Ok(hugr)
-    }
-
-    /// Test whether a `level` message should be output.
-    pub fn verbosity(&self, level: Level) -> bool {
-        self.verbose.log_level_filter() >= level
-    }
+    /// Errors produced by the `validate` subcommand.
+    Validate(#[from] validate::CliError),
 }
--- hugr-cli/src/main.rs
+++ hugr-cli/src/main.rs
@@ -1,5 +1,6 @@
 //! Validate serialized HUGR on the command line
 
+use clap::Parser as _;
 use hugr_core::std_extensions::arithmetic::{
     conversions::EXTENSION as CONVERSIONS_EXTENSION, float_ops::EXTENSION as FLOAT_OPS_EXTENSION,
     float_types::EXTENSION as FLOAT_TYPES_EXTENSION, int_ops::EXTENSION as INT_OPS_EXTENSION,
@@ -7,16 +8,30 @@ use hugr_core::std_extensions::arithmetic::{
 };
 use hugr_core::std_extensions::logic::EXTENSION as LOGICS_EXTENSION;
 
+use hugr_cli::{validate, CliArgs};
 use hugr_core::extension::{ExtensionRegistry, PRELUDE};
 
-use hugr_cli::CmdLineArgs;
-
-use clap::Parser;
 use clap_verbosity_flag::Level;
 
 fn main() {
-    let opts = CmdLineArgs::parse();
+    match CliArgs::parse() {
+        CliArgs::Validate(args) => run_validate(args),
+        CliArgs::External(_) => {
+            // TODO: Implement support for external commands.
+            // Running `hugr COMMAND` would look for `hugr-COMMAND` in the path
+            // and run it.
+            eprintln!("External commands are not supported yet.");
+            std::process::exit(1);
+        }
+        _ => {
+            eprintln!("Unknown command");
+            std::process::exit(1);
+        }
+    };
+}
 
+/// Run the `validate` subcommand.
+fn run_validate(args: validate::CliArgs) {
     // validate with all std extensions
     let reg = ExtensionRegistry::try_new([
         PRELUDE.to_owned(),
@@ -29,8 +44,10 @@ fn main() {
     ])
     .unwrap();
 
-    if let Err(e) = opts.run(&reg) {
-        if opts.verbosity(Level::Error) {
+    let result = args.run(&reg);
+
+    if let Err(e) = result {
+        if args.verbosity(Level::Error) {
             eprintln!("{}", e);
         }
         std::process::exit(1);
