687a4962f04a8b7865f2b8b6690253899d1825b6
Alec Edgington <alec.edgington@quantinuum.com>
2024-07-18 11:43:52 +0100

    Implement v1-to-v2 conversion.

--- hugr-core/src/hugr/serialize/upgrade.rs
+++ hugr-core/src/hugr/serialize/upgrade.rs
@@ -1,3 +1,4 @@
+use serde::de::Error;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -12,8 +13,23 @@ pub enum UpgradeError {
     UnknownVersionUnsupported,
 }
 
-#[allow(unused_mut)]
 pub fn v1_to_v2(mut input: serde_json::Value) -> Result<serde_json::Value, UpgradeError> {
+    let input_obj = input
+        .as_object_mut()
+        .ok_or(serde_json::Error::custom("Value is not an object"))?;
+    let nodes = input_obj
+        .get_mut("nodes")
+        .ok_or(serde_json::Error::custom("No nodes field"))?
+        .as_array_mut()
+        .ok_or(serde_json::Error::custom("nodes is not an array"))?;
+    for node in nodes.iter_mut() {
+        let node_obj = node
+            .as_object_mut()
+            .ok_or(serde_json::Error::custom("node is not an object"))?;
+        if let Some(name) = node_obj.remove("op_name") {
+            node_obj.insert("name".to_owned(), name);
+        }
+    }
     Ok(input)
 }
 
