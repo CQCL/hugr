b44e87037f850508497a20f5e0ee469b75af8cf5
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-08-12 17:22:04 +0100

    fix merge

--- hugr-py/src/hugr/std/int.py
+++ hugr-py/src/hugr/std/int.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import TYPE_CHECKING
 
 from typing_extensions import Self
