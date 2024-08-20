5a02fdb234d266635bf04f74bcfd223a7d971424
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-08-20 16:06:35 +0100

    add more serializations

--- hugr-model/src/serialization.rs
+++ hugr-model/src/serialization.rs
@@ -13,9 +13,7 @@ pub mod hugr_capnp {
 }
 
 /// Convert v0::Module to hugr_capnp::module.
-pub fn convert_module(
-    module: &v0::Module,
-) -> Result<capnp::message::Builder<HeapAllocator>, Box<dyn std::error::Error>> {
+pub fn convert_module(module: &v0::Module) -> capnp::message::Builder<HeapAllocator> {
     let mut builder = capnp::message::Builder::new_default();
     let mut module_builder = builder.init_root::<hugr_capnp::module::Builder>();
     {
@@ -24,7 +22,7 @@ pub fn convert_module(
             .init_nodes(module.nodes.len() as u32);
         for (i, node) in module.nodes.iter().enumerate() {
             let mut node_builder = nodes.reborrow().get(i as u32);
-            build_node(node, &mut node_builder)?;
+            node.build(&mut node_builder);
         }
     }
     {
@@ -33,7 +31,7 @@ pub fn convert_module(
             .init_ports(module.ports.len() as u32);
         for (i, port) in module.ports.iter().enumerate() {
             let mut port_builder = ports.reborrow().get(i as u32);
-            build_port(port, &mut port_builder)?;
+            port.build(&mut port_builder);
         }
     }
 
@@ -58,59 +56,177 @@ pub fn convert_module(
             .init_term_table(term_hash.len() as u32);
         for (term, id) in term_hash {
             let mut term_builder = term_table.reborrow().get(id);
-            build_term(term, &mut term_builder)?;
+            term.build(&mut term_builder);
         }
     }
 
     // Set the fields of the module_builder using the data from the v0::Module
 
-    Ok(builder)
+    builder
 }
 
-fn build_node(
-    node: &v0::Node,
-    builder: &mut hugr_capnp::node::Builder,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // Set the fields of the node_builder using the data from the v0::Node
-    Ok(())
+trait Build {
+    type SerBuilder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>);
 }
+impl Build for v0::Node {
+    type SerBuilder<'a> = hugr_capnp::node::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        let mut operation_build = builder.reborrow().init_operation();
+        self.operation.build(&mut operation_build);
 
-fn build_port(
-    port: &v0::Port,
-    builder: &mut hugr_capnp::port::Builder,
-) -> Result<(), Box<dyn std::error::Error>> {
-    // Set the fields of the port_builder using the data from the v0::Port
-    Ok(())
-}
-
-fn build_term(
-    term: &v0::Term,
-    builder: &mut hugr_capnp::term::Builder,
-) -> Result<(), Box<dyn std::error::Error>> {
-    match term {
-        v0::Term::Wildcard => todo!(),
-        v0::Term::Type => todo!(),
-        v0::Term::Constraint => todo!(),
-        v0::Term::Var(_) => todo!(),
-        v0::Term::Named(_) => todo!(),
-        v0::Term::List(_) => todo!(),
-        v0::Term::ListType(_) => todo!(),
-        v0::Term::Str(s) => {
-            let mut s_build = builder.reborrow().init_str(s.len() as u32);
-            s_build.push_str(s);
+        let mut ports = builder.reborrow().init_params(self.params.len() as u32);
+        for (i, param_id) in self.params.iter().enumerate() {
+            ports.set(i as u32, param_id.0);
         }
-        v0::Term::StrType => todo!(),
-        v0::Term::Nat(_) => todo!(),
-        v0::Term::NatType => todo!(),
-        v0::Term::ExtSet(_) => todo!(),
-        v0::Term::ExtSetType => todo!(),
-        v0::Term::Tuple(_) => todo!(),
-        v0::Term::ProductType(_) => todo!(),
-        v0::Term::Tagged(_) => todo!(),
-        v0::Term::SumType(_) => todo!(),
-        v0::Term::FuncType(_) => todo!(),
+
+        let mut inputs = builder.reborrow().init_inputs(self.inputs.len() as u32);
+        for (i, input_id) in self.inputs.iter().enumerate() {
+            inputs.set(i as u32, input_id.0);
+        }
+
+        let mut outputs = builder.reborrow().init_outputs(self.outputs.len() as u32);
+        for (i, output_id) in self.outputs.iter().enumerate() {
+            outputs.set(i as u32, output_id.0);
+        }
+
+        let mut children = builder.reborrow().init_children(self.children.len() as u32);
+        for (i, child_id) in self.children.iter().enumerate() {
+            children.set(i as u32, child_id.0);
+        }
+
+        let meta_builder = builder.reborrow().init_meta(self.meta.len() as u32);
+        build_meta_list(self.meta.iter(), meta_builder);
     }
-    Ok(())
+}
+
+impl Build for v0::MetaItem {
+    type SerBuilder<'a> = hugr_capnp::meta_item::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        builder.set_name(&self.name);
+        let mut value_builder = builder.reborrow().init_value();
+        self.value.build(&mut value_builder);
+    }
+}
+
+fn build_meta_list<'a>(
+    metas: impl Iterator<Item = &'a v0::MetaItem>,
+    mut metadata: capnp::struct_list::Builder<hugr_capnp::meta_item::Owned>,
+) {
+    for (i, meta_item) in metas.enumerate() {
+        let mut meta_builder = metadata.reborrow().get(i as u32);
+        meta_item.build(&mut meta_builder);
+    }
+}
+
+impl Build for v0::Operation {
+    type SerBuilder<'a> = hugr_capnp::operation::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        match self {
+            v0::Operation::Module => builder.set_module(()),
+            v0::Operation::Input => builder.set_input(()),
+            v0::Operation::Output => builder.set_output(()),
+            v0::Operation::Dfg => builder.set_dfg(()),
+            v0::Operation::Cfg => builder.set_cfg(()),
+            v0::Operation::Block => builder.set_block(()),
+            v0::Operation::Exit => builder.set_exit(()),
+            v0::Operation::Case => builder.set_case(()),
+            v0::Operation::DefineFunc(defn) => {
+                defn.build(&mut builder.reborrow().init_define_func());
+            }
+            v0::Operation::DeclareFunc(decl) => {
+                decl.build(&mut builder.reborrow().init_define_func());
+            }
+            v0::Operation::CallFunc(_) => todo!(),
+            v0::Operation::LoadFunc(_) => todo!(),
+            v0::Operation::Custom(cust) => {
+                let mut cust_builder = builder.reborrow().init_custom();
+                cust.build(&mut cust_builder);
+            }
+            v0::Operation::DefineAlias(_) => todo!(),
+            v0::Operation::DeclareAlias(_) => todo!(),
+        }
+    }
+}
+
+impl Build for v0::operation::DefineFunc {
+    type SerBuilder<'a> = hugr_capnp::operation::define_func::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        builder.set_name(&self.name.0);
+        let mut type_builder = builder.reborrow().init_type();
+
+        self.r#type.build(&mut type_builder);
+    }
+}
+
+impl Build for v0::operation::DeclareFunc {
+    type SerBuilder<'a> = hugr_capnp::operation::define_func::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        builder.set_name(&self.name.0);
+        let mut type_builder = builder.reborrow().init_type();
+
+        self.r#type.build(&mut type_builder);
+    }
+}
+
+impl Build for v0::operation::Custom {
+    type SerBuilder<'a> = hugr_capnp::operation::custom::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        builder.set_name(&self.name.0);
+    }
+}
+
+impl Build for v0::Scheme {
+    type SerBuilder<'a> = hugr_capnp::scheme::Builder<'a>;
+
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        todo!()
+    }
+}
+
+impl Build for v0::Port {
+    type SerBuilder<'a> = hugr_capnp::port::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        builder.set_type(self.r#type.0);
+        let meta_builder = builder.reborrow().init_meta(self.meta.len() as u32);
+        build_meta_list(self.meta.iter(), meta_builder);
+    }
+}
+
+impl Build for v0::Term {
+    type SerBuilder<'a> = hugr_capnp::term::Builder<'a>;
+    fn build<'s>(&self, builder: &mut Self::SerBuilder<'s>) {
+        match self {
+            v0::Term::Wildcard => todo!(),
+            v0::Term::Type => todo!(),
+            v0::Term::Constraint => todo!(),
+            v0::Term::Var(_) => todo!(),
+            v0::Term::Named(_) => todo!(),
+            v0::Term::List(_) => todo!(),
+            v0::Term::ListType(_) => todo!(),
+            v0::Term::Str(s) => {
+                let mut s_build = builder.reborrow().init_str(s.len() as u32);
+                s_build.push_str(s);
+            }
+            v0::Term::StrType => todo!(),
+            v0::Term::Nat(_) => todo!(),
+            v0::Term::NatType => todo!(),
+            v0::Term::ExtSet(_) => todo!(),
+            v0::Term::ExtSetType => todo!(),
+            v0::Term::Tuple(_) => todo!(),
+            v0::Term::ProductType(_) => todo!(),
+            v0::Term::Tagged(_) => todo!(),
+            v0::Term::SumType(_) => todo!(),
+            v0::Term::FuncType(_) => todo!(),
+        };
+    }
+}
+
+pub fn to_bytes(module: &v0::Module) -> Vec<u8> {
+    let message = convert_module(module);
+    let mut buf: Vec<u8> = Vec::new();
+    capnp::serialize_packed::write_message(&mut buf, &message).unwrap();
+    buf
 }
 
 #[cfg(test)]
@@ -127,10 +243,7 @@ mod test {
         module.add_term(v0::Term::Str(s.clone()));
         module.add_term(v0::Term::Str(SmolStr::new_inline("Hello, world")));
 
-        let message = convert_module(&module).unwrap();
-        let mut buf: Vec<u8> = Vec::new();
-        capnp::serialize_packed::write_message(&mut buf, &message).unwrap();
-
+        let buf = to_bytes(&module);
         let message_reader =
             capnp::serialize_packed::read_message(&buf[..], ::capnp::message::ReaderOptions::new())
                 .unwrap();
