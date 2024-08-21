e6d08ad14bbfcb7f1406330db88868f5ebcebaba
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2024-08-21 17:31:10 +0100

    use pkgutil

--- hugr-py/src/hugr/std/__init__.py
+++ hugr-py/src/hugr/std/__init__.py
@@ -1,15 +1,13 @@
 """Types and operations from the standard extension set."""
 
-from pathlib import Path
+import pkgutil
 
 from hugr.ext import Extension
 from hugr.serialization.extension import Extension as PdExtension
 
-_EXT_DIR = Path(__file__).parent / "_json_defs"
-
 
 def _load_extension(name: str) -> Extension:
     replacement = name.replace(".", "/")
-    path = _EXT_DIR / f"{replacement}.json"
-    with path.open() as f:
-        return PdExtension.model_validate_json(f.read()).deserialize()
+    json_str = pkgutil.get_data(__name__, f"_json_defs/{replacement}.json")
+    assert json_str is not None
+    return PdExtension.model_validate_json(json_str).deserialize()
