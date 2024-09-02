90da80ed16208d741c27601d3925108ee3e9f782
Agustin Borgna <agustin.borgna@quantinuum.com>
2024-09-02 17:00:28 +0100

    fix: IndexError on node slicing

--- hugr-py/src/hugr/hugr/node_port.py
+++ hugr-py/src/hugr/hugr/node_port.py
@@ -176,22 +176,22 @@ class Node(ToNode):
                     )
                     raise ValueError(msg)
 
-                start = self._normalize_index(start)
-                stop = self._normalize_index(stop, allow_eq_len=True)
+                start = self._normalize_index(start, allow_overflow=True)
+                stop = self._normalize_index(stop, allow_overflow=True)
                 step = index.step or 1
 
                 return (self[i] for i in range(start, stop, step))
             case tuple(xs):
                 return (self[i] for i in xs)
 
-    def _normalize_index(self, index: int, allow_eq_len: bool = False) -> int:
+    def _normalize_index(self, index: int, allow_overflow: bool = False) -> int:
         """Given an index passed to `__getitem__`, normalize it to be within the
         range of output ports.
 
         Args:
             index: index to normalize.
-            allow_eq_len: whether to allow the index to be equal to the number of
-                output ports.
+            allow_overflow: whether to allow indices beyond the number of outputs.
+                If True, indices over `self._num_out_ports` will be truncated.
 
         Returns:
             Normalized index.
@@ -202,9 +202,7 @@ class Node(ToNode):
         msg = f"Index {index} out of range"
 
         if self._num_out_ports is not None:
-            if index > self._num_out_ports:
-                raise IndexError(msg)
-            if index == self._num_out_ports and not allow_eq_len:
+            if index >= self._num_out_ports and not allow_overflow:
                 raise IndexError(msg)
             if index < -self._num_out_ports:
                 raise IndexError(msg)
@@ -212,7 +210,9 @@ class Node(ToNode):
             if index < 0:
                 raise IndexError(msg)
 
-        if index >= 0:
+        if index >= 0 and self._num_out_ports is not None:
+            return min(index, self._num_out_ports)
+        elif index >= 0:
             return index
         else:
             assert self._num_out_ports is not None
