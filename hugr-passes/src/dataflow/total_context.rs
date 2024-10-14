1f410568e35c62fde9c2fc7b51d58342def9ae2b
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2024-10-14 11:09:18 +0100

    Odd updates to total_context.rs - REVERT ??

--- hugr-passes/src/dataflow/total_context.rs
+++ hugr-passes/src/dataflow/total_context.rs
@@ -1,19 +1,15 @@
 use std::hash::Hash;
 
-use ascent::lattice::BoundedLattice;
 use hugr_core::{ops::OpTrait, Hugr, HugrView, IncomingPort, Node, OutgoingPort, PortIndex};
 
-use super::{AbstractValue, DFContext, PartialValue, Sum};
+use super::partial_value::{AbstractValue, PartialValue, ValueOrSum};
+use super::DFContext;
 
 /// A simpler interface like [DFContext] but where the context only cares about
-/// values that are completely known (as `V`s), i.e. not `Bottom`, `Top`, or
-/// Sums of potentially multiple variants.
+/// values that are completely known (in the lattice `V`)
+/// rather than e.g. Sums potentially of two variants each of known values.
 pub trait TotalContext<V>: Clone + Eq + Hash + std::ops::Deref<Target = Hugr> {
-    /// The representation of values on which [Self::interpret_leaf_op] operates
-    type InterpretableVal: From<V> + TryFrom<Sum<Self::InterpretableVal>>;
-    /// Interpret a leaf op.
-    /// `ins` gives the input ports for which we know (interpretable) values, and will be non-empty.
-    /// Returns a list of output ports for which we know (abstract) values (may be empty).
+    type InterpretableVal: TryFrom<ValueOrSum<V>>;
     fn interpret_leaf_op(
         &self,
         node: Node,
@@ -36,14 +32,17 @@ impl<V: AbstractValue, T: TotalContext<V>> DFContext<V> for T {
             .zip(ins.iter())
             .filter_map(|((i, ty), pv)| {
                 pv.clone()
-                    .try_into_value::<<Self as TotalContext<V>>::InterpretableVal>(ty)
+                    .try_into_value(ty)
+                    // Discard PVs which don't produce ValueOrSum, i.e. Bottom/Top :-)
                     .ok()
+                    // And discard any ValueOrSum that don't produce V - this is a bit silent :-(
+                    .and_then(|v_s| T::InterpretableVal::try_from(v_s).ok())
                     .map(|v| (IncomingPort::from(i), v))
             })
             .collect::<Vec<_>>();
         let known_outs = self.interpret_leaf_op(node, &known_ins);
         (!known_outs.is_empty()).then(|| {
-            let mut res = vec![PartialValue::bottom(); sig.output_count()];
+            let mut res = vec![PartialValue::Bottom; sig.output_count()];
             for (p, v) in known_outs {
                 res[p.index()] = v.into();
             }
