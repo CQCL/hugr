a99cb1e730cd05748b619f1b986eea24869dbf02
Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
2024-10-21 11:58:46 +0100

    feat: Ensure packages always have modules at the root (#1589)
    Followup to #1587.
    
    Ensures that rust `Package`s only contain module-rooted hugrs. Returns
    errors at construction times if the condition is not met.
    This required fixing `hugr-cli`, as it should be able to load both
    packages and arbitrary hugrs.
    Added `Package::from_hugr{,s}` methods that try to wrap the hugrs if
    possible. We'll need these for tket2.
    
    Packages are not on the spec yet, their description should include this
    restriction. See #1388.
    
    This PR does only modify the (unpublished) API introduced in #1587, so
    I'm not marking it as breaking.

--- hugr-core/src/package.rs
+++ hugr-core/src/package.rs
@@ -1,11 +1,15 @@
 //! Bundles of hugr modules along with the extension required to load them.
 
 use derive_more::{Display, Error, From};
+use std::collections::HashMap;
 use std::path::Path;
-use std::{fs, io};
+use std::{fs, io, mem};
 
+use crate::builder::{Container, Dataflow, DataflowSubContainer, ModuleBuilder};
 use crate::extension::{ExtensionRegistry, ExtensionRegistryError};
-use crate::hugr::ValidationError;
+use crate::hugr::internal::HugrMutInternals;
+use crate::hugr::{HugrView, ValidationError};
+use crate::ops::{Module, NamedOp, OpTag, OpTrait, OpType};
 use crate::{Extension, Hugr};
 
 #[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
@@ -20,20 +24,79 @@ pub struct Package {
 
 impl Package {
     /// Create a new package from a list of hugrs and extensions.
+    ///
+    /// All the HUGRs must have a `Module` operation at the root.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if any of the HUGRs does not have a `Module` root.
     pub fn new(
         modules: impl IntoIterator<Item = Hugr>,
         extensions: impl IntoIterator<Item = Extension>,
-    ) -> Self {
-        Self {
-            modules: modules.into_iter().collect(),
-            extensions: extensions.into_iter().collect(),
+    ) -> Result<Self, PackageError> {
+        let modules: Vec<Hugr> = modules.into_iter().collect();
+        for (idx, module) in modules.iter().enumerate() {
+            let root_op = module.get_optype(module.root());
+            if !root_op.is_module() {
+                return Err(PackageError::NonModuleHugr {
+                    module_index: idx,
+                    root_op: root_op.clone(),
+                });
+            }
         }
+        Ok(Self {
+            modules,
+            extensions: extensions.into_iter().collect(),
+        })
     }
 
+    /// Create a new package from a list of hugrs and extensions.
+    ///
+    /// HUGRs that do not have a `Module` root will be wrapped in a new `Module` root,
+    /// depending on the root optype.
+    ///
+    /// - Currently all non-module roots will raise [PackageError::CannotWrapHugr].
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if any of the HUGRs cannot be wrapped in a module.
+    pub fn from_hugrs(
+        modules: impl IntoIterator<Item = Hugr>,
+        extensions: impl IntoIterator<Item = Extension>,
+    ) -> Result<Self, PackageError> {
+        let modules: Vec<Hugr> = modules
+            .into_iter()
+            .map(to_module_hugr)
+            .collect::<Result<_, PackageError>>()?;
+        Ok(Self {
+            modules,
+            extensions: extensions.into_iter().collect(),
+        })
+    }
+
+    /// Create a new package containing a single HUGR, and no extension definitions.
+    ///
+    /// If the Hugr is not a module, a new [OpType::Module] root will be added.
+    /// This behaviours depends on the root optype.
+    ///
+    /// - Currently all non-module roots will raise [PackageError::CannotWrapHugr].
+    ///
+    /// # Errors
+    ///
+    /// Returns an error if the hugr cannot be wrapped in a module.
+    pub fn from_hugr(hugr: Hugr) -> Result<Self, PackageError> {
+        let mut package = Self::default();
+        let module = to_module_hugr(hugr)?;
+        package.modules.push(module);
+        Ok(package)
+    }
     /// Validate the package against an extension registry.
     ///
     /// `reg` is updated with any new extensions.
-    pub fn validate(&mut self, reg: &mut ExtensionRegistry) -> Result<(), PackageValidationError> {
+    pub fn update_validate(
+        &mut self,
+        reg: &mut ExtensionRegistry,
+    ) -> Result<(), PackageValidationError> {
         for ext in &self.extensions {
             reg.register_updated_ref(ext)?;
         }
@@ -54,7 +117,7 @@ impl Package {
         };
 
         if let Ok(hugr) = serde_json::from_value::<Hugr>(val) {
-            return Ok(Package::new([hugr], []));
+            return Ok(Package::from_hugr(hugr)?);
         }
 
         // Return the original error from parsing the package.
@@ -99,6 +162,90 @@ impl Package {
     }
 }
 
+impl PartialEq for Package {
+    fn eq(&self, other: &Self) -> bool {
+        if self.modules != other.modules || self.extensions.len() != other.extensions.len() {
+            return false;
+        }
+        // Extensions may be in different orders, so we compare them as sets.
+        let exts = self
+            .extensions
+            .iter()
+            .map(|e| (&e.name, e))
+            .collect::<HashMap<_, _>>();
+        other
+            .extensions
+            .iter()
+            .all(|e| exts.get(&e.name).map_or(false, |&e2| e == e2))
+    }
+}
+
+impl AsRef<[Hugr]> for Package {
+    fn as_ref(&self) -> &[Hugr] {
+        &self.modules
+    }
+}
+
+/// Alter an arbitrary hugr to contain an [OpType::Module] root.
+///
+/// The behaviour depends on the root optype. See [Package::from_hugr] for details.
+///
+/// # Errors
+///
+/// Returns [PackageError::]
+fn to_module_hugr(mut hugr: Hugr) -> Result<Hugr, PackageError> {
+    let root = hugr.root();
+    let root_op = hugr.get_optype(root);
+    let tag = root_op.tag();
+
+    // Modules can be returned as is.
+    if root_op.is_module() {
+        return Ok(hugr);
+    }
+    // If possible, wrap the hugr directly in a module.
+    if OpTag::ModuleOp.is_superset(tag) {
+        let new_root = hugr.add_node(Module::new().into());
+        hugr.set_root(new_root);
+        hugr.set_parent(root, new_root);
+        return Ok(hugr);
+    }
+    // Wrap it in a function definition named "main" inside the module otherwise.
+    if OpTag::DataflowChild.is_superset(tag) && !root_op.is_input() && !root_op.is_output() {
+        let signature = root_op
+            .dataflow_signature()
+            .unwrap_or_else(|| panic!("Dataflow child {} without signature", root_op.name()));
+        let mut new_hugr = ModuleBuilder::new();
+        let mut func = new_hugr.define_function("main", signature).unwrap();
+        let dataflow_node = func.add_hugr_with_wires(hugr, func.input_wires()).unwrap();
+        func.finish_with_outputs(dataflow_node.outputs()).unwrap();
+        return Ok(mem::take(new_hugr.hugr_mut()));
+    }
+    // Reject all other hugrs.
+    Err(PackageError::CannotWrapHugr {
+        root_op: root_op.clone(),
+    })
+}
+
+/// Error raised while loading a package.
+#[derive(Debug, Display, Error, PartialEq)]
+#[non_exhaustive]
+pub enum PackageError {
+    /// A hugr in the package does not have an [OpType::Module] root.
+    #[display("Module {module_index} in the package does not have an OpType::Module root, but {}", root_op.name())]
+    NonModuleHugr {
+        /// The module index.
+        module_index: usize,
+        /// The invalid root operation.
+        root_op: OpType,
+    },
+    /// Tried to initialize a package with a hugr that cannot be wrapped in a module.
+    #[display("A hugr with optype {} cannot be wrapped in a module.", root_op.name())]
+    CannotWrapHugr {
+        /// The invalid root operation.
+        root_op: OpType,
+    },
+}
+
 /// Error raised while loading a package.
 #[derive(Debug, Display, Error, From)]
 #[non_exhaustive]
@@ -107,6 +254,8 @@ pub enum PackageEncodingError {
     JsonEncoding(serde_json::Error),
     /// Error raised while reading from a file.
     IOError(io::Error),
+    /// Improper package definition.
+    Package(PackageError),
 }
 
 /// Error raised while validating a package.
@@ -117,4 +266,88 @@ pub enum PackageValidationError {
     Extension(ExtensionRegistryError),
     /// Error raised while validating the package hugrs.
     Validation(ValidationError),
+}
+
+#[cfg(test)]
+mod test {
+    use cool_asserts::assert_matches;
+
+    use crate::builder::test::{
+        simple_cfg_hugr, simple_dfg_hugr, simple_funcdef_hugr, simple_module_hugr,
+    };
+    use crate::extension::{ExtensionId, EMPTY_REG};
+    use crate::ops::dataflow::IOTrait;
+    use crate::ops::Input;
+
+    use super::*;
+    use rstest::{fixture, rstest};
+    use semver::Version;
+
+    #[fixture]
+    fn simple_package() -> Package {
+        let hugr0 = simple_module_hugr();
+        let hugr1 = simple_module_hugr();
+
+        let ext_1_id = ExtensionId::new("ext1").unwrap();
+        let ext_2_id = ExtensionId::new("ext2").unwrap();
+        let ext1 = Extension::new(ext_1_id.clone(), Version::new(2, 4, 8));
+        let ext2 = Extension::new(ext_2_id, Version::new(1, 0, 0));
+
+        Package {
+            modules: vec![hugr0, hugr1],
+            extensions: vec![ext1, ext2],
+        }
+    }
+
+    #[fixture]
+    fn simple_input_node() -> Hugr {
+        Hugr::new(Input::new(vec![]))
+    }
+
+    #[rstest]
+    #[case::empty(Package::default())]
+    #[case::simple(simple_package())]
+    fn package_roundtrip(#[case] package: Package) {
+        let json = package.to_json().unwrap();
+        let new_package = Package::from_json(&json).unwrap();
+        assert_eq!(package, new_package);
+    }
+
+    #[rstest]
+    #[case::module(simple_module_hugr(), false)]
+    #[case::funcdef(simple_funcdef_hugr(), false)]
+    #[case::dfg(simple_dfg_hugr(), false)]
+    #[case::cfg(simple_cfg_hugr(), false)]
+    #[case::unsupported_input(simple_input_node(), true)]
+    fn hugr_to_package(#[case] hugr: Hugr, #[case] errors: bool) {
+        match (&Package::from_hugr(hugr), errors) {
+            (Ok(package), false) => {
+                assert_eq!(package.modules.len(), 1);
+                let root_op = package.modules[0].get_optype(package.modules[0].root());
+                assert!(root_op.is_module());
+            }
+            (Err(_), true) => {}
+            (p, _) => panic!("Unexpected result {:?}", p),
+        }
+    }
+
+    #[rstest]
+    fn package_properties() {
+        let module = simple_module_hugr();
+        let dfg = simple_dfg_hugr();
+
+        assert_matches!(
+            Package::new([module.clone(), dfg.clone()], []),
+            Err(PackageError::NonModuleHugr {
+                module_index: 1,
+                root_op: OpType::DFG(_),
+            })
+        );
+
+        let mut pkg = Package::from_hugrs([module, dfg], []).unwrap();
+        let mut reg = EMPTY_REG.clone();
+        pkg.update_validate(&mut reg).unwrap();
+
+        assert_eq!(pkg.modules.len(), 2);
+    }
 }
