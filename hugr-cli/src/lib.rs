CQCL/hugr

commit d899bd3da9e548f3ea4d678b3777993d758369da

feat: Add `Package` definition on `hugr-core` (#1587)
Moves the definition of packages out of `hugr-cli`. (We leave a
re-export there to avoid breaking the API, it should be removed on the
next breaking release).

Adds some helper functions to load and validate the package.

Closes #1530 

drive-by: Replace `hugr-core` dependency on `hugr-cli` by `hugr`.
There's no need to access the internals there.

hugr-cli/Cargo.toml
@@ -15,7 +15,8 @@ categories = ["compilers"]
 [dependencies]
 clap = { workspace = true, features = ["derive"] }
 clap-verbosity-flag.workspace = true
-hugr-core = { path = "../hugr-core", version = "0.13.1" }
+derive_more = { workspace = true, features = ["display", "error", "from"] }
+hugr = { path = "../hugr", version = "0.13.1" }
 serde_json.workspace = true
 serde.workspace = true
 thiserror.workspace = true

hugr-cli/src/lib.rs
@@ -3,13 +3,16 @@
 use clap::Parser;
 use clap_verbosity_flag::{InfoLevel, Verbosity};
 use clio::Input;
-use hugr_core::{Extension, Hugr};
+use derive_more::{Display, Error, From};
+use hugr::package::{PackageEncodingError, PackageValidationError};
 use std::{ffi::OsString, path::PathBuf};
-use thiserror::Error;
 
 pub mod extensions;
 pub mod mermaid;
 pub mod validate;
+
+// TODO: Deprecated re-export. Remove on a breaking release.
+pub use hugr::package::Package;
 
 /// CLI arguments.
 #[derive(Parser, Debug)]
@@ -30,18 +33,21 @@ pub enum CliArgs {
 }
 
 /// Error type for the CLI.
-#[derive(Debug, Error)]
-#[error(transparent)]
+#[derive(Debug, Display, Error, From)]
 #[non_exhaustive]
 pub enum CliError {
     /// Error reading input.
-    #[error("Error reading from path: {0}")]
-    InputFile(#[from] std::io::Error),
+    #[display("Error reading from path: {_0}")]
+    InputFile(std::io::Error),
     /// Error parsing input.
-    #[error("Error parsing input: {0}")]
-    Parse(#[from] serde_json::Error),
+    #[display("Error parsing input: {_0}")]
+    Parse(serde_json::Error),
+    /// Error loading a package.
+    #[display("Error parsing package: {_0}")]
+    Package(PackageEncodingError),
+    #[display("Error validating HUGR: {_0}")]
     /// Errors produced by the `validate` subcommand.
-    Validate(#[from] validate::ValError),
+    Validate(PackageValidationError),
 }
 
 /// Validate and visualise a HUGR file.
@@ -68,36 +74,10 @@ pub struct HugrArgs {
     pub extensions: Vec<PathBuf>,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-/// Package of module HUGRs and extensions.
-/// The HUGRs are validated against the extensions.
-pub struct Package {
-    /// Module HUGRs included in the package.
-    pub modules: Vec<Hugr>,
-    /// Extensions to validate against.
-    pub extensions: Vec<Extension>,
-}
-
-impl Package {
-    /// Create a new package.
-    pub fn new(modules: Vec<Hugr>, extensions: Vec<Extension>) -> Self {
-        Self {
-            modules,
-            extensions,
-        }
-    }
-}
-
 impl HugrArgs {
     /// Read either a package or a single hugr from the input.
     pub fn get_package(&mut self) -> Result<Package, CliError> {
-        let val: serde_json::Value = serde_json::from_reader(&mut self.input)?;
-        // read either a package or a single hugr
-        if let Ok(p) = serde_json::from_value::<Package>(val.clone()) {
-            Ok(p)
-        } else {
-            let hugr: Hugr = serde_json::from_value(val)?;
-            Ok(Package::new(vec![hugr], vec![]))
-        }
+        let pkg = Package::from_json_reader(&mut self.input)?;
+        Ok(pkg)
     }
 }

hugr-cli/src/validate.rs
@@ -2,10 +2,10 @@
 
 use clap::Parser;
 use clap_verbosity_flag::Level;
-use hugr_core::{extension::ExtensionRegistry, Extension, Hugr};
-use thiserror::Error;
+use hugr::package::PackageValidationError;
+use hugr::{extension::ExtensionRegistry, Extension, Hugr};
 
-use crate::{CliError, HugrArgs, Package};
+use crate::{CliError, HugrArgs};
 
 /// Validate and visualise a HUGR file.
 #[derive(Parser, Debug)]
@@ -17,18 +17,6 @@ pub struct ValArgs {
     #[command(flatten)]
     /// common arguments
     pub hugr_args: HugrArgs,
-}
-
-/// Error type for the CLI.
-#[derive(Error, Debug)]
-#[non_exhaustive]
-pub enum ValError {
-    /// Error validating HUGR.
-    #[error("Error validating HUGR: {0}")]
-    Validate(#[from] hugr_core::hugr::ValidationError),
-    /// Error registering extension.
-    #[error("Error registering extension: {0}")]
-    ExtReg(#[from] hugr_core::extension::ExtensionRegistryError),
 }
 
 /// String to print when validation is successful.
@@ -50,49 +38,30 @@ impl ValArgs {
     }
 }
 
-impl Package {
-    /// Validate the package against an extension registry.
-    ///
-    /// `reg` is updated with any new extensions.
-    ///
-    /// Returns the validated modules.
-    pub fn validate(mut self, reg: &mut ExtensionRegistry) -> Result<Vec<Hugr>, ValError> {
-        // register packed extensions
-        for ext in self.extensions {
-            reg.register_updated(ext)?;
-        }
-
-        for hugr in self.modules.iter_mut() {
-            hugr.update_validate(reg)?;
-        }
-
-        Ok(self.modules)
-    }
-}
-
 impl HugrArgs {
     /// Load the package and validate against an extension registry.
     ///
     /// Returns the validated modules and the extension registry the modules
     /// were validated against.
     pub fn validate(&mut self) -> Result<(Vec<Hugr>, ExtensionRegistry), CliError> {
-        let package = self.get_package()?;
+        let mut package = self.get_package()?;
 
         let mut reg: ExtensionRegistry = if self.no_std {
-            hugr_core::extension::PRELUDE_REGISTRY.to_owned()
+            hugr::extension::PRELUDE_REGISTRY.to_owned()
         } else {
-            hugr_core::std_extensions::STD_REG.to_owned()
+            hugr::std_extensions::STD_REG.to_owned()
         };
 
         // register external extensions
         for ext in &self.extensions {
             let f = std::fs::File::open(ext)?;
             let ext: Extension = serde_json::from_reader(f)?;
-            reg.register_updated(ext).map_err(ValError::ExtReg)?;
+            reg.register_updated(ext)
+                .map_err(PackageValidationError::Extension)?;
         }
 
-        let modules = package.validate(&mut reg)?;
-        Ok((modules, reg))
+        package.validate(&mut reg)?;
+        Ok((package.modules, reg))
     }
 
     /// Test whether a `level` message should be output.

hugr-core/src/package.rs
@@ -1,0 +1,120 @@
+//! Bundles of hugr modules along with the extension required to load them.
+
+use derive_more::{Display, Error, From};
+use std::path::Path;
+use std::{fs, io};
+
+use crate::extension::{ExtensionRegistry, ExtensionRegistryError};
+use crate::hugr::ValidationError;
+use crate::{Extension, Hugr};
+
+#[derive(Debug, Default, Clone, serde::Serialize, serde::Deserialize)]
+/// Package of module HUGRs and extensions.
+/// The HUGRs are validated against the extensions.
+pub struct Package {
+    /// Module HUGRs included in the package.
+    pub modules: Vec<Hugr>,
+    /// Extensions to validate against.
+    pub extensions: Vec<Extension>,
+}
+
+impl Package {
+    /// Create a new package from a list of hugrs and extensions.
+    pub fn new(
+        modules: impl IntoIterator<Item = Hugr>,
+        extensions: impl IntoIterator<Item = Extension>,
+    ) -> Self {
+        Self {
+            modules: modules.into_iter().collect(),
+            extensions: extensions.into_iter().collect(),
+        }
+    }
+
+    /// Validate the package against an extension registry.
+    ///
+    /// `reg` is updated with any new extensions.
+    pub fn validate(&mut self, reg: &mut ExtensionRegistry) -> Result<(), PackageValidationError> {
+        for ext in &self.extensions {
+            reg.register_updated_ref(ext)?;
+        }
+        for hugr in self.modules.iter_mut() {
+            hugr.update_validate(reg)?;
+        }
+        Ok(())
+    }
+
+    /// Read a Package in json format from an io reader.
+    ///
+    /// If the json encodes a single [Hugr] instead, it will be inserted in a new [Package].
+    pub fn from_json_reader(reader: impl io::Read) -> Result<Self, PackageEncodingError> {
+        let val: serde_json::Value = serde_json::from_reader(reader)?;
+        let pkg_load_err = match serde_json::from_value::<Package>(val.clone()) {
+            Ok(p) => return Ok(p),
+            Err(e) => e,
+        };
+
+        if let Ok(hugr) = serde_json::from_value::<Hugr>(val) {
+            return Ok(Package::new([hugr], []));
+        }
+
+        // Return the original error from parsing the package.
+        Err(PackageEncodingError::JsonEncoding(pkg_load_err))
+    }
+
+    /// Read a Package from a json string.
+    ///
+    /// If the json encodes a single [Hugr] instead, it will be inserted in a new [Package].
+    pub fn from_json(json: impl AsRef<str>) -> Result<Self, PackageEncodingError> {
+        Self::from_json_reader(json.as_ref().as_bytes())
+    }
+
+    /// Read a Package from a json file.
+    ///
+    /// If the json encodes a single [Hugr] instead, it will be inserted in a new [Package].
+    pub fn from_json_file(path: impl AsRef<Path>) -> Result<Self, PackageEncodingError> {
+        let file = fs::File::open(path)?;
+        let reader = io::BufReader::new(file);
+        Self::from_json_reader(reader)
+    }
+
+    /// Write the Package in json format into an io writer.
+    pub fn to_json_writer(&self, writer: impl io::Write) -> Result<(), PackageEncodingError> {
+        serde_json::to_writer(writer, self)?;
+        Ok(())
+    }
+
+    /// Write the Package into a json string.
+    ///
+    /// If the json encodes a single [Hugr] instead, it will be inserted in a new [Package].
+    pub fn to_json(&self) -> Result<String, PackageEncodingError> {
+        let json = serde_json::to_string(self)?;
+        Ok(json)
+    }
+
+    /// Write the Package into a json file.
+    pub fn to_json_file(&self, path: impl AsRef<Path>) -> Result<(), PackageEncodingError> {
+        let file = fs::File::open(path)?;
+        let writer = io::BufWriter::new(file);
+        self.to_json_writer(writer)
+    }
+}
+
+/// Error raised while loading a package.
+#[derive(Debug, Display, Error, From)]
+#[non_exhaustive]
+pub enum PackageEncodingError {
+    /// Error raised while parsing the package json.
+    JsonEncoding(serde_json::Error),
+    /// Error raised while reading from a file.
+    IOError(io::Error),
+}
+
+/// Error raised while validating a package.
+#[derive(Debug, Display, Error, From)]
+#[non_exhaustive]
+pub enum PackageValidationError {
+    /// Error raised while processing the package extensions.
+    Extension(ExtensionRegistryError),
+    /// Error raised while validating the package hugrs.
+    Validation(ValidationError),
+}

hugr-cli/tests/validate.rs
@@ -6,10 +6,9 @@
 
 use assert_cmd::Command;
 use assert_fs::{fixture::FileWriteStr, NamedTempFile};
-use hugr_cli::{validate::VALID_PRINT, Package};
-use hugr_core::builder::DFGBuilder;
-use hugr_core::types::Type;
-use hugr_core::{
+use hugr::builder::DFGBuilder;
+use hugr::types::Type;
+use hugr::{
     builder::{Container, Dataflow},
     extension::prelude::{BOOL_T, QB_T},
     std_extensions::arithmetic::float_types::FLOAT64_TYPE,
@@ -17,6 +16,7 @@ use hugr_core::{
     types::Signature,
     Hugr,
 };
+use hugr_cli::{validate::VALID_PRINT, Package};
 use predicates::{prelude::*, str::contains};
 use rstest::{fixture, rstest};
 
@@ -128,7 +128,7 @@ fn test_bad_json(mut val_cmd: Command) {
     val_cmd
         .assert()
         .failure()
-        .stderr(contains("Error parsing input"));
+        .stderr(contains("Error parsing package"));
 }
 
 #[rstest]
@@ -139,7 +139,7 @@ fn test_bad_json_silent(mut val_cmd: Command) {
     val_cmd
         .assert()
         .failure()
-        .stderr(contains("Error parsing input").not());
+        .stderr(contains("Error parsing package").not());
 }
 
 #[rstest]
@@ -188,7 +188,7 @@ fn test_float_extension(float_hugr_string: String, mut val_cmd: Command) {
 #[fixture]
 fn package_string(#[with(FLOAT64_TYPE)] test_hugr: Hugr) -> String {
     let rdr = std::fs::File::open(FLOAT_EXT_FILE).unwrap();
-    let float_ext: hugr_core::Extension = serde_json::from_reader(rdr).unwrap();
+    let float_ext: hugr::Extension = serde_json::from_reader(rdr).unwrap();
     let package = Package::new(vec![test_hugr], vec![float_ext]);
     serde_json::to_string(&package).unwrap()
 }

hugr-core/Cargo.toml
@@ -37,7 +37,7 @@ serde = { workspace = true, features = ["derive", "rc"] }
 serde_yaml = { workspace = true, optional = true }
 typetag = { workspace = true }
 smol_str = { workspace = true, features = ["serde"] }
-derive_more = { workspace = true, features = ["display", "from"] }
+derive_more = { workspace = true, features = ["display", "error", "from"] }
 itertools = { workspace = true }
 html-escape = { workspace = true }
 bitvec = { workspace = true, features = ["serde"] }

hugr-core/src/lib.rs
@@ -17,6 +17,7 @@ pub mod hugr;
 pub mod import;
 pub mod macros;
 pub mod ops;
+pub mod package;
 pub mod std_extensions;
 pub mod types;
 pub mod utils;

hugr/src/lib.rs
@@ -135,7 +135,7 @@
 
 // These modules are re-exported as-is. If more control is needed, define a new module in this crate with the desired exports.
 // The doc inline directive is necessary for renamed modules to appear as if they were defined in this crate.
-pub use hugr_core::{builder, core, extension, ops, std_extensions, types, utils};
+pub use hugr_core::{builder, core, extension, ops, package, std_extensions, types, utils};
 #[doc(inline)]
 pub use hugr_passes as algorithms;
 

commit 45c3106deeb448e2c30ba7377376293d26136cec

chore: Unbreak release (#1591)
Replaces the breaking changes introduced in #1587 with non-breaking
deprecations.
We should be able to remove these on the next breaking release.

hugr-cli/Cargo.toml
@@ -25,6 +25,13 @@ clio = { workspace = true, features = ["clap-parse"] }
 [lints]
 workspace = true
 
+[package.metadata.cargo-semver-checks.lints]
+workspace = true
+# Temporarily disabled due to Package being moved to `hugr-core` triggering an error in rustdoc
+# https://github.com/obi1kenobi/cargo-semver-checks/issues/355
+enum_missing = "warn"
+struct_missing = "warn"
+
 [dev-dependencies]
 assert_cmd = { workspace = true }
 assert_fs = { workspace = true }

hugr-cli/src/lib.rs
@@ -14,6 +14,8 @@ pub mod mermaid;
 pub mod validate;
 
 // TODO: Deprecated re-export. Remove on a breaking release.
+#[doc(inline)]
+#[deprecated(since = "0.13.2", note = "Use `hugr::package::Package` instead.")]
 pub use hugr::package::Package;
 
 /// CLI arguments.
@@ -125,5 +127,18 @@ impl HugrArgs {
         }
         let pkg = serde_json::from_value::<Package>(val.clone())?;
         Ok(PackageOrHugr::Package(pkg))
+    }
+
+    /// Read either a package from the input.
+    ///
+    /// deprecated: use [HugrArgs::get_package_or_hugr] instead.
+    #[deprecated(
+        since = "0.13.2",
+        note = "Use `HugrArgs::get_package_or_hugr` instead."
+    )]
+    pub fn get_package(&mut self) -> Result<Package, CliError> {
+        let val: serde_json::Value = serde_json::from_reader(&mut self.input)?;
+        let pkg = serde_json::from_value::<Package>(val.clone())?;
+        Ok(pkg)
     }
 }

hugr-cli/src/validate.rs
@@ -7,6 +7,14 @@ use hugr::{extension::ExtensionRegistry, Extension, Hugr};
 
 use crate::{CliError, HugrArgs};
 
+// TODO: Deprecated re-export. Remove on a breaking release.
+#[doc(inline)]
+#[deprecated(
+    since = "0.13.2",
+    note = "Use `hugr::package::PackageValidationError` instead."
+)]
+pub use hugr::package::PackageValidationError as ValError;
+
 /// Validate and visualise a HUGR file.
 #[derive(Parser, Debug)]
 #[clap(version = "1.0", long_about = None)]

hugr-core/src/package.rs
@@ -106,6 +106,22 @@ impl Package {
         Ok(())
     }
 
+    /// Validate the package against an extension registry.
+    ///
+    /// `reg` is updated with any new extensions.
+    ///
+    /// Returns the validated modules.
+    ///
+    /// deprecated: use [Package::update_validate] instead.
+    #[deprecated(since = "0.13.2", note = "Replaced by `Package::update_validate`")]
+    pub fn validate(
+        mut self,
+        reg: &mut ExtensionRegistry,
+    ) -> Result<Vec<Hugr>, PackageValidationError> {
+        self.update_validate(reg)?;
+        Ok(self.modules)
+    }
+
     /// Read a Package in json format from an io reader.
     ///
     /// If the json encodes a single [Hugr] instead, it will be inserted in a new [Package].
@@ -259,13 +275,57 @@ pub enum PackageEncodingError {
 }
 
 /// Error raised while validating a package.
-#[derive(Debug, Display, Error, From)]
+#[derive(Debug, From)]
 #[non_exhaustive]
 pub enum PackageValidationError {
     /// Error raised while processing the package extensions.
     Extension(ExtensionRegistryError),
     /// Error raised while validating the package hugrs.
     Validation(ValidationError),
+    /// Error validating HUGR.
+    // TODO: Remove manual Display and Error impls when removing deprecated variants.
+    #[from(ignore)]
+    #[deprecated(
+        since = "0.13.2",
+        note = "Replaced by `PackageValidationError::Validation`"
+    )]
+    Validate(ValidationError),
+    /// Error registering extension.
+    // TODO: Remove manual Display and Error impls when removing deprecated variants.
+    #[from(ignore)]
+    #[deprecated(
+        since = "0.13.2",
+        note = "Replaced by `PackageValidationError::Extension`"
+    )]
+    ExtReg(ExtensionRegistryError),
+}
+
+// Note: We cannot use the `derive_more::Error` derive due to a bug with deprecated elements.
+// See https://github.com/JelteF/derive_more/issues/419
+#[allow(deprecated)]
+impl std::error::Error for PackageValidationError {
+    fn source(&self) -> Option<&(dyn derive_more::Error + 'static)> {
+        match self {
+            PackageValidationError::Extension(source) => Some(source),
+            PackageValidationError::Validation(source) => Some(source),
+            PackageValidationError::Validate(source) => Some(source),
+            PackageValidationError::ExtReg(source) => Some(source),
+        }
+    }
+}
+
+// Note: We cannot use the `derive_more::Display` derive due to a bug with deprecated elements.
+// See https://github.com/JelteF/derive_more/issues/419
+impl Display for PackageValidationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        #[allow(deprecated)]
+        match self {
+            PackageValidationError::Extension(e) => write!(f, "Error processing extensions: {}", e),
+            PackageValidationError::Validation(e) => write!(f, "Error validating HUGR: {}", e),
+            PackageValidationError::Validate(e) => write!(f, "Error validating HUGR: {}", e),
+            PackageValidationError::ExtReg(e) => write!(f, "Error registering extension: {}", e),
+        }
+    }
 }
 
 #[cfg(test)]