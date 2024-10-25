9f9a218a049ce5bdbc128fd1b7c1ccf81fb83725
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2024-10-25 19:06:13 +0100

    (?to revert?) Handle some LoadFunction's by reading subgraphs from Hugr

--- hugr-passes/src/const_fold2/context.rs
+++ hugr-passes/src/const_fold2/context.rs
@@ -1,6 +1,7 @@
-use hugr_core::ops::constant::OpaqueValue;
-use hugr_core::ops::{ExtensionOp, Value};
-use hugr_core::{IncomingPort, Node, OutgoingPort};
+use hugr_core::hugr::views::{DescendantsGraph, ExtractHugr, HierarchyView};
+use hugr_core::ops::{constant::OpaqueValue, handle::FuncID, ExtensionOp, Value};
+use hugr_core::types::TypeArg;
+use hugr_core::{HugrView, IncomingPort, Node, OutgoingPort};
 
 use super::value_handle::ValueHandle;
 use crate::dataflow::{ConstLoader, PartialValue, TotalContext};
@@ -11,9 +12,9 @@ use crate::dataflow::{ConstLoader, PartialValue, TotalContext};
 /// Just stores a Hugr (actually any [HugrView]),
 /// (there is )no state for operation-interpretation.
 #[derive(Debug)]
-pub struct ConstFoldContext;
+pub struct ConstFoldContext<H>(pub H);
 
-impl ConstLoader<ValueHandle> for ConstFoldContext {
+impl<H: HugrView> ConstLoader<ValueHandle> for ConstFoldContext<H> {
     fn value_from_opaque(
         &self,
         node: Node,
@@ -36,10 +37,23 @@ impl ConstLoader<ValueHandle> for ConstFoldContext {
         ))
     }
 
-    // Do not handle (Load)Function/value_from_function yet.
+    fn value_from_function(&self, node: Node, type_args: &[TypeArg]) -> Option<ValueHandle> {
+        if type_args.len() > 0 {
+            // TODO: substitution across Hugr (https://github.com/CQCL/hugr/issues/709)
+            return None;
+        };
+        // Returning the function body as a value, here, would be sufficient for inlining IndirectCall
+        // but not for transforming to a direct Call.
+        let func = DescendantsGraph::<FuncID<true>>::try_new(&self.0, node).ok()?;
+        Some(ValueHandle::new_const_hugr(
+            node,
+            &[],
+            Box::new(func.extract_hugr()),
+        ))
+    }
 }
 
-impl TotalContext<ValueHandle> for ConstFoldContext {
+impl<H: HugrView> TotalContext<ValueHandle> for ConstFoldContext<H> {
     type InterpretableVal = Value;
 
     fn interpret_leaf_op(
@@ -66,7 +80,7 @@ impl TotalContext<ValueHandle> for ConstFoldContext {
 mod test {
     use hugr_core::ops::{constant::CustomConst, Value};
     use hugr_core::std_extensions::arithmetic::{float_types::ConstF64, int_types::ConstInt};
-    use hugr_core::{types::SumType, Node};
+    use hugr_core::{types::SumType, Hugr, Node};
     use itertools::Itertools;
     use rstest::rstest;
 
@@ -82,7 +96,8 @@ mod test {
         let n = Node::from(portgraph::NodeIndex::new(7));
         let st = SumType::new([vec![k.get_type()], vec![]]);
         let subject_val = Value::sum(0, [k.clone().into()], st).unwrap();
-        let v1 = ConstFoldContext.value_from_const(n, &subject_val);
+        let ctx: ConstFoldContext<Hugr> = ConstFoldContext(Hugr::default());
+        let v1 = ctx.value_from_const(n, &subject_val);
 
         let v1_subfield = {
             let PartialValue::PartialSum(ps1) = v1 else {
@@ -98,7 +113,7 @@ mod test {
                 .unwrap()
         };
 
-        let v2 = ConstFoldContext.value_from_const(n, &k.into());
+        let v2 = ctx.value_from_const(n, &k.into());
         if eq {
             assert_eq!(v1_subfield, v2);
         } else {
