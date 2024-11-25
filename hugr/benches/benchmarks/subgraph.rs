925c1ea331b1551abee05b5b3afa5d5837fd6f3e
Agustín Borgna <agustin.borgna@quantinuum.com>
2024-11-25 14:39:59 +0000

    Add benchmarks for subgraphs with few nodes

--- hugr/benches/benchmarks/subgraph.rs
+++ hugr/benches/benchmarks/subgraph.rs
@@ -30,6 +30,35 @@ fn bench_singleton_subgraph(c: &mut Criterion) {
     group.finish();
 }
 
+fn bench_fewnode_subgraph(c: &mut Criterion) {
+    let mut group = c.benchmark_group("fewnode_subgraph");
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+
+    let num_layers = [10, 100, 1000];
+
+    for layers in num_layers.into_iter() {
+        let (hugr, layer_ids) = circuit(layers);
+
+        // Get a subgraph with a fixed number of nodes in the middle of the circuit.
+        group.bench_with_input(
+            BenchmarkId::from_parameter(layers),
+            &layers,
+            |b, &_layers| {
+                // Pick all the nodes in four layers in the middle of the circuit.
+                let nodes: Vec<_> = layer_ids
+                    .iter()
+                    .skip(layers / 2)
+                    .take(4)
+                    .flat_map(|ids| [ids.h, ids.cx1, ids.cx2])
+                    .collect();
+                b.iter(|| black_box(SiblingSubgraph::try_from_nodes(nodes.clone(), &hugr)))
+            },
+        );
+    }
+
+    group.finish();
+}
+
 fn bench_multinode_subgraph(c: &mut Criterion) {
     let mut group = c.benchmark_group("multinode_subgraph");
     group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
@@ -59,5 +88,6 @@ criterion_group! {
     config = Criterion::default();
     targets =
         bench_singleton_subgraph,
+        bench_fewnode_subgraph,
         bench_multinode_subgraph,
 }
