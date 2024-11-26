7866a62a99287d8c582377b946f63c13fa67a6c9
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2024-11-26 18:22:04 +0000

    Use continue, match instead of if let |

--- hugr-core/src/hugr/monomorphize.rs
+++ hugr-core/src/hugr/monomorphize.rs
@@ -2,7 +2,7 @@ use std::collections::{hash_map::Entry, HashMap};
 
 use crate::{
     extension::ExtensionRegistry,
-    ops::{Call, FuncDefn, LoadFunction, OpTrait},
+    ops::{FuncDefn, OpTrait},
     types::{Substitution, TypeArg},
     Node,
 };
@@ -53,49 +53,41 @@ pub(super) fn mono_scan(
             mono_scan(h, ch, None, cache, reg)
         }
         let ch_op = h.get_optype(ch);
-        if let OpType::Call(Call {
-            type_args,
-            instantiation,
-            ..
-        })
-        | OpType::LoadFunction(LoadFunction {
-            type_args,
-            signature: instantiation,
-            ..
-        }) = ch_op
-        {
-            if !type_args.is_empty() {
-                let fn_inp = ch_op.static_input_port().unwrap();
-                let tgt = h.static_source(ch).unwrap();
-                let new_tgt = match cache.entry((tgt, type_args.clone())) {
-                    Entry::Occupied(n) => *n.get(),
-                    Entry::Vacant(ve) => {
-                        let type_args = type_args.clone(); // Need to mutate Hugr...
-                        let tgt = h.add_node_after(
-                            tgt,
-                            FuncDefn {
-                                name: name_mangle(
-                                    &h.get_optype(tgt).as_func_defn().unwrap().name,
-                                    &type_args,
-                                ),
-                                signature: instantiation.clone().into(),
-                            },
-                        );
-                        ve.insert(tgt);
-                        mono_scan(
-                            h,
-                            tgt,
-                            Some((&Substitution::new(&type_args, reg), tgt)),
-                            cache,
-                            reg,
-                        );
-                        tgt
-                    }
-                };
-                h.disconnect(ch, fn_inp);
-                h.connect(new_tgt, h.num_outputs(new_tgt) - 1, ch, fn_inp);
+        let (type_args, instantiation) = match ch_op {
+            OpType::Call(c) => (&c.type_args, &c.instantiation),
+            OpType::LoadFunction(lf) => (&lf.type_args, &lf.signature),
+            _ => continue,
+        };
+        if type_args.is_empty() {
+            continue;
+        };
+        let fn_inp = ch_op.static_input_port().unwrap();
+        let tgt = h.static_source(ch).unwrap();
+        let new_tgt = match cache.entry((tgt, type_args.clone())) {
+            Entry::Occupied(n) => *n.get(),
+            Entry::Vacant(ve) => {
+                let type_args = type_args.clone(); // Need to mutate Hugr...
+                let name = name_mangle(&h.get_optype(tgt).as_func_defn().unwrap().name, &type_args);
+                let mono_tgt = h.add_node_after(
+                    tgt,
+                    FuncDefn {
+                        name,
+                        signature: instantiation.clone().into(),
+                    },
+                );
+                ve.insert(mono_tgt);
+                mono_scan(
+                    h,
+                    tgt,
+                    Some((&Substitution::new(&type_args, reg), mono_tgt)),
+                    cache,
+                    reg,
+                );
+                mono_tgt
             }
-        }
+        };
+        h.disconnect(ch, fn_inp);
+        h.connect(new_tgt, h.num_outputs(new_tgt) - 1, ch, fn_inp);
     }
     if subst_into.is_some() {
         todo!("Copy edges!")
