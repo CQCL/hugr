459991b65a3a22c1da8a1fa1380d65e62ed2d49b
Lukas Heidemann <lukas@heidemann.me>
2024-12-02 16:39:20 +0000

    Improved bindings data structure.

--- hugr-model/src/v0/resolve/bindings.rs
+++ hugr-model/src/v0/resolve/bindings.rs
@@ -2,239 +2,152 @@ use fxhash::FxHashMap;
 use std::collections::hash_map::Entry as HashMapEntry;
 use std::hash::Hash;
 
-#[derive(Debug, Clone)]
 pub struct Bindings<K, S, V> {
-    /// The bindings that are currently visible.
-    bindings: FxHashMap<K, (V, ScopeId)>,
+    key_to_binding: FxHashMap<K, BindingIndex>,
+    bindings: Vec<Binding<K, V>>,
+    value_to_binding: FxHashMap<V, BindingIndex>,
+    scopes: Vec<Scope<S>>,
+}
 
-    /// An undo log that keeps track of changes to the bindings.
-    undo_log: Vec<(K, Option<(V, ScopeId)>)>,
+type BindingIndex = usize;
+type ScopeIndex = usize;
 
-    /// Stack of scopes together with the depth of the undo log when the scope was entered.
-    scopes: Vec<(S, UndoDepth)>,
+#[derive(Debug, Clone, Copy)]
+struct Binding<K, V> {
+    name: K,
+    value: V,
+    scope: ScopeIndex,
+    shadows: BindingIndex,
+    shadowed: bool,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Scope<S> {
+    binding_depth: usize,
+    isolation_depth: usize,
+    scope: S,
 }
 
 impl<K, S, V> Bindings<K, S, V>
 where
     K: Eq + Hash + Copy,
-    V: Copy,
+    V: Eq + Hash + Copy,
+    S: Copy,
 {
-    /// Create a new `Bindings` instance.
     pub fn new() -> Self {
         Self {
-            bindings: FxHashMap::default(),
-            undo_log: Vec::new(),
-            scopes: vec![],
+            key_to_binding: FxHashMap::default(),
+            scopes: Vec::new(),
+            bindings: Vec::new(),
+            value_to_binding: FxHashMap::default(),
         }
     }
 
-    pub fn entry(&mut self, key: K) -> Entry<K, V> {
+    pub fn get_value_by_key(&self, key: K) -> Option<V> {
+        let binding_index = *self.key_to_binding.get(&key)?;
+        let binding = self.bindings[binding_index];
+
+        if binding.shadowed || binding.scope < self.isolation_depth() {
+            None
+        } else {
+            Some(binding.value)
+        }
+    }
+
+    pub fn get_key_by_value(&self, value: V) -> Option<K> {
+        let binding_index = *self.value_to_binding.get(&value)?;
+        let binding = self.bindings[binding_index];
+
+        if binding.shadowed || binding.scope < self.isolation_depth() {
+            None
+        } else {
+            Some(binding.name)
+        }
+    }
+
+    pub fn try_insert(&mut self, key: K, value: V) -> Result<(), V> {
         let scope = self
             .scopes
             .len()
             .checked_sub(1)
             .expect("no scope to insert into");
 
-        let undo_log = &mut self.undo_log;
+        let shadows = match self.key_to_binding.entry(key) {
+            HashMapEntry::Occupied(mut entry) => {
+                let shadows = *entry.get();
+                let shadows_binding = &mut self.bindings[shadows];
 
-        match self.bindings.entry(key) {
-            HashMapEntry::Occupied(entry) if entry.get().1 == scope => {
-                Entry::Occupied(OccupiedEntry { entry })
+                if shadows_binding.scope == scope {
+                    return Err(shadows_binding.value);
+                }
+
+                shadows_binding.shadowed = true;
+                self.value_to_binding.remove(&shadows_binding.value);
+                entry.insert(self.bindings.len());
+                shadows
             }
-            HashMapEntry::Occupied(entry) => Entry::Visible(VisibleEntry {
-                entry,
-                scope,
-                undo_log,
-            }),
-            HashMapEntry::Vacant(entry) => Entry::Vacant(VacantEntry {
-                scope,
-                entry,
-                undo_log,
-            }),
-        }
+            HashMapEntry::Vacant(entry) => {
+                entry.insert(self.bindings.len());
+                BindingIndex::MAX
+            }
+        };
+
+        self.value_to_binding.insert(value, self.bindings.len());
+
+        self.bindings.push(Binding {
+            name: key,
+            value,
+            scope,
+            shadows,
+            shadowed: false,
+        });
+
+        Ok(())
     }
 
-    /// Try to insert a new binding into the current scope.
-    ///
-    /// If the key is already bound in the current scope, the old value is returned
-    /// and the bindings remain unchanged.
-    ///
-    /// # Panics
-    ///
-    /// Panics if there is no current scope.
-    pub fn try_insert(&mut self, key: K, value: V) -> Result<(), V> {
-        self.entry(key).try_insert(value)
+    fn isolation_depth(&self) -> usize {
+        self.scopes.last().map_or(0, |scope| scope.isolation_depth)
     }
 
-    /// Retrieve a binding with the given key.
-    pub fn get(&self, key: K) -> Option<V> {
-        Some(self.bindings.get(&key)?.0)
-    }
-
-    /// Enter a new scope.
     pub fn enter(&mut self, scope: S) {
-        let depth = self.undo_log.len();
-        self.scopes.push((scope, depth));
+        self.scopes.push(Scope {
+            scope,
+            binding_depth: self.bindings.len(),
+            isolation_depth: self.isolation_depth(),
+        });
     }
 
-    /// Enter a new scope that is isolated from the parent scope.
-    ///
-    /// No bindings from the parent scope or any of its ancestors will be visible in the new scope.
-    /// When the new scope is exited, the previous bindings will be restored.
     pub fn enter_isolated(&mut self, scope: S) {
-        self.enter(scope);
-
-        self.undo_log.extend(
-            self.bindings
-                .drain()
-                .map(|(name, binding)| (name, Some(binding))),
-        );
+        self.scopes.push(Scope {
+            scope,
+            binding_depth: self.bindings.len(),
+            isolation_depth: self.scopes.len(),
+        });
     }
 
-    /// Exit the current scope.
-    ///
-    /// This will remove all bindings that were added in the current scope, and
-    /// restore the bindings that were shadowed by the current scope.
-    ///
-    /// # Panics
-    ///
-    /// Panics if there is no current scope.
     pub fn exit(&mut self) -> S {
-        let (scope, depth) = self.scopes.pop().expect("unbalanced scopes");
+        let scope = self.scopes.pop().expect("no scope to exit");
 
-        while self.undo_log.len() > depth {
-            let (key, change) = self.undo_log.pop().unwrap();
-            match change {
-                Some(binding) => self.bindings.insert(key, binding),
-                None => self.bindings.remove(&key),
-            };
+        while self.bindings.len() > scope.binding_depth {
+            let binding = self.bindings.pop().unwrap();
+            self.value_to_binding.remove(&binding.value);
+
+            if let Some(shadows) = self.bindings.get_mut(binding.shadows) {
+                debug_assert!(shadows.shadowed);
+                debug_assert!(shadows.name == binding.name);
+                shadows.shadowed = false;
+                self.key_to_binding.insert(shadows.name, binding.shadows);
+                self.value_to_binding.insert(shadows.value, binding.shadows);
+            } else {
+                self.key_to_binding.remove(&binding.name);
+            }
         }
 
-        scope
+        scope.scope
     }
 
-    /// Get the current scope.
     pub fn scope(&self) -> Option<&S> {
-        Some(&self.scopes.last()?.0)
-    }
-}
-
-impl<K, S, V> Default for Bindings<K, S, V>
-where
-    K: Eq + Hash + Copy,
-    V: Copy,
-{
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-type ScopeId = usize;
-type UndoDepth = usize;
-
-/// An entry in a [`Bindings`] map.
-pub enum Entry<'a, K, V> {
-    Occupied(OccupiedEntry<'a, K, V>),
-    Vacant(VacantEntry<'a, K, V>),
-    Visible(VisibleEntry<'a, K, V>),
-}
-
-impl<'a, K, V> Entry<'a, K, V>
-where
-    K: Eq + Hash + Copy,
-    V: Copy,
-{
-    pub fn key(&self) -> K {
-        match self {
-            Entry::Occupied(entry) => entry.key(),
-            Entry::Vacant(entry) => entry.key(),
-            Entry::Visible(entry) => entry.key(),
-        }
-    }
-
-    pub fn try_insert(self, value: V) -> Result<(), V> {
-        match self {
-            Entry::Occupied(entry) => Err(entry.get()),
-            Entry::Vacant(entry) => {
-                entry.insert(value);
-                Ok(())
-            }
-            Entry::Visible(entry) => {
-                entry.insert(value);
-                Ok(())
-            }
-        }
-    }
-}
-
-/// An entry in a [`Bindings`] map that is occupied by a binding in a parent scope,
-/// but vacant in the current scope.
-pub struct VisibleEntry<'a, K, V> {
-    entry: std::collections::hash_map::OccupiedEntry<'a, K, (V, ScopeId)>,
-    undo_log: &'a mut Vec<(K, Option<(V, ScopeId)>)>,
-    scope: ScopeId,
-}
-
-impl<'a, K, V> VisibleEntry<'a, K, V>
-where
-    K: Eq + Hash + Copy,
-    V: Copy,
-{
-    pub fn key(&self) -> K {
-        *self.entry.key()
-    }
-
-    pub fn get(&self) -> V {
-        self.entry.get().0
-    }
-
-    pub fn insert(mut self, value: V) {
-        let key = self.key();
-        let shadowed = self.entry.insert((value, self.scope));
-        self.undo_log.push((key, Some(shadowed)));
-    }
-}
-
-/// An entry in a [`Bindings`] map that is occupied in the current scope.
-pub struct OccupiedEntry<'a, K, V> {
-    entry: std::collections::hash_map::OccupiedEntry<'a, K, (V, ScopeId)>,
-}
-
-impl<'a, K, V> OccupiedEntry<'a, K, V>
-where
-    K: Eq + Hash + Copy,
-    V: Copy,
-{
-    pub fn key(&self) -> K {
-        *self.entry.key()
-    }
-
-    pub fn get(&self) -> V {
-        self.entry.get().0
-    }
-}
-
-/// An entry in a [`Bindings`] map that is vacant in the current scope and not visible
-/// from any parent scope.
-pub struct VacantEntry<'a, K, V> {
-    entry: std::collections::hash_map::VacantEntry<'a, K, (V, ScopeId)>,
-    undo_log: &'a mut Vec<(K, Option<(V, ScopeId)>)>,
-    scope: ScopeId,
-}
-
-impl<'a, K, V> VacantEntry<'a, K, V>
-where
-    K: Eq + Hash + Copy,
-    V: Copy,
-{
-    pub fn key(&self) -> K {
-        *self.entry.key()
-    }
-
-    pub fn insert(self, value: V) {
-        let key = self.key();
-        self.entry.insert((value, self.scope));
-        self.undo_log.push((key, None));
+        Some(&self.scopes.last()?.scope)
     }
 }
 
@@ -247,19 +160,26 @@ mod test {
         let mut bindings = Bindings::<&'static str, (), usize>::new();
         bindings.enter(());
         assert_eq!(bindings.try_insert("foo", 1), Ok(()));
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
+        assert_eq!(bindings.get_key_by_value(1), Some("foo"));
         bindings.enter(());
         // The binding for `foo` is still visible in the new scope:
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
+        assert_eq!(bindings.get_key_by_value(1), Some("foo"));
         assert_eq!(bindings.try_insert("foo", 2), Ok(()));
         // The new binding shadows the old one:
-        assert_eq!(bindings.get("foo"), Some(2));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(2));
+        assert_eq!(bindings.get_key_by_value(2), Some("foo"));
+        assert_eq!(bindings.get_key_by_value(1), None);
         bindings.exit();
         // The old binding is restored:
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
+        assert_eq!(bindings.get_key_by_value(1), Some("foo"));
+        assert_eq!(bindings.get_key_by_value(2), None);
         bindings.exit();
         // The binding is removed:
-        assert_eq!(bindings.get("foo"), None);
+        assert_eq!(bindings.get_value_by_key("foo"), None);
+        assert_eq!(bindings.get_key_by_value(1), None);
     }
 
     #[test]
@@ -267,17 +187,19 @@ mod test {
         let mut bindings = Bindings::<&'static str, (), usize>::new();
         bindings.enter(());
         assert_eq!(bindings.try_insert("foo", 1), Ok(()));
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
         bindings.enter_isolated(());
         // The binding for `foo` is not visible in the new scope:
-        assert_eq!(bindings.get("foo"), None);
+        assert_eq!(bindings.get_value_by_key("foo"), None);
+        assert_eq!(bindings.get_key_by_value(1), None);
         assert_eq!(bindings.try_insert("foo", 2), Ok(()));
-        assert_eq!(bindings.get("foo"), Some(2));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(2));
         bindings.exit();
         // The old binding is restored:
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
+        assert_eq!(bindings.get_key_by_value(1), Some("foo"));
         bindings.exit();
-        assert_eq!(bindings.get("foo"), None);
+        assert_eq!(bindings.get_value_by_key("foo"), None);
     }
 
     #[test]
@@ -285,9 +207,12 @@ mod test {
         let mut bindings = Bindings::<&'static str, (), usize>::new();
         bindings.enter(());
         assert_eq!(bindings.try_insert("foo", 1), Ok(()));
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.try_insert("bar", 2), Ok(()));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
         assert_eq!(bindings.try_insert("foo", 2), Err(1));
         // The old binding is still there and unchanged:
-        assert_eq!(bindings.get("foo"), Some(1));
+        assert_eq!(bindings.get_value_by_key("foo"), Some(1));
+        assert_eq!(bindings.get_key_by_value(1), Some("foo"));
+        assert_eq!(bindings.get_key_by_value(2), Some("bar"));
     }
 }
--- hugr-model/src/v0/resolve/mod.rs
+++ hugr-model/src/v0/resolve/mod.rs
@@ -80,6 +80,9 @@ struct Resolver<'m, 'a> {
     /// Links that are visible in the current scope.
     link_scope: Bindings<&'a str, RegionId, LinkId>,
 
+    /// The regions in which links are defined. Indexed by [`LinkId`]s.
+    link_regions: Vec<Option<RegionId>>,
+
     /// Bit vector to keep track of visited terms so that we do not visit them multiple times.
     term_visited: BitVec,
 }
@@ -88,6 +91,7 @@ impl<'m, 'a> Resolver<'m, 'a> {
     fn new(module: &'m mut Module<'a>, bump: &'a Bump) -> Self {
         Self {
             term_visited: BitVec::repeat(false, module.terms.len()),
+            link_regions: vec![None; module.links.len()],
             module,
             bump,
             symbol_scope: Bindings::new(),
@@ -99,9 +103,7 @@ impl<'m, 'a> Resolver<'m, 'a> {
 
     /// Resolve a non-isolated region.
     fn resolve_region(&mut self, region: RegionId) -> Result<(), ModelError> {
-        self.link_scope.enter(region);
         self.resolve_region_inner(region)?;
-        self.link_scope.exit();
         Ok(())
     }
 
@@ -309,15 +311,17 @@ impl<'m, 'a> Resolver<'m, 'a> {
                     // TODO: Check if the link is visible.
                     link_id
                 }
-                LinkRef::Named(name) => match self.link_scope.entry(name) {
-                    bindings::Entry::Occupied(entry) => entry.get(),
-                    bindings::Entry::Visible(entry) => entry.get(),
-                    bindings::Entry::Vacant(entry) => {
-                        let link_id = self.module.insert_link(Link { name });
-                        entry.insert(link_id);
-                        link_id
-                    }
-                },
+                LinkRef::Named(name) => todo!(),
+                // match self.link_scope.try_insert(name, ) {
+                //     bindings::Entry::Occupied(entry) => entry.get(),
+                //     bindings::Entry::Visible(entry) => entry.get(),
+                //     bindings::Entry::Vacant(entry) => {
+                //         let link_id = self.module.insert_link(Link { name });
+                //         entry.insert(link_id);
+                //         self.link_regions.push(self.link_scope.scope().copied());
+                //         link_id
+                //     }
+                // },
             };
 
             resolved.push(LinkRef::Id(link_id));
@@ -448,7 +452,7 @@ impl<'m, 'a> Resolver<'m, 'a> {
             VarRef::Named(name) => {
                 let index = self
                     .var_scope
-                    .get(name)
+                    .get_value_by_key(name)
                     .ok_or_else(|| VarRefError::NameNotFound(name.to_string()))?;
 
                 // UNWRAP: If the local is in scope, then the scope must be set.
@@ -462,17 +466,14 @@ impl<'m, 'a> Resolver<'m, 'a> {
         match symbol_ref {
             SymbolRef::Direct(node) => {
                 // Check whether the global is visible at this point.
-                let name = self.module.symbol_name(symbol_ref)?;
-                let visible = self.symbol_scope.get(name);
-
-                if visible != Some(node) {
+                if self.symbol_scope.get_key_by_value(node).is_none() {
                     return Err(SymbolRefError::NotVisible(node).into());
                 }
 
                 Ok(symbol_ref)
             }
             SymbolRef::Named(name) => {
-                if let Some(node) = self.symbol_scope.get(name) {
+                if let Some(node) = self.symbol_scope.get_value_by_key(name) {
                     return Ok(SymbolRef::Direct(node));
                 }
 
