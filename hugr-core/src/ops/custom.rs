3dcb88e9372dda33f58c5c311da910a0dc75364c
Agustín Borgna <agustin.borgna@quantinuum.com>
2024-12-10 15:20:18 +0000

    undo workaround

--- hugr-core/src/ops/custom.rs
+++ hugr-core/src/ops/custom.rs
@@ -257,14 +257,7 @@ impl DataflowOpTrait for OpaqueOp {
     }
 
     fn signature(&self) -> Cow<'_, Signature> {
-        // TODO: Return a borrowed cow once
-        // https://github.com/CQCL/hugr/issues/1758
-        // gets fixed
-        Cow::Owned(
-            self.signature
-                .clone()
-                .with_extension_delta(self.extension.clone()),
-        )
+        Cow::Borrowed(&self.signature)
     }
 }
 
