b05a419e08dfcccf10fa081a22fc83af0d11502b
tatiana-s <tatiana.sedelnikov@quantinuum.com>
2024-12-19 11:33:19 +0000

    feat: Add `StringVal` to hugr-py (#1818)
    Closes #1817
    
    Enables strings in Guppy, see CQCL/guppylang#695

---
+++ hugr-py/src/hugr/std/prelude.py
@@ -0,0 +1,34 @@
+"""HUGR prelude values."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from hugr import val
+from hugr.std import _load_extension
+
+PRELUDE_EXTENSION = _load_extension("prelude")
+
+STRING_T_DEF = PRELUDE_EXTENSION.types["string"]
+
+STRING_T = STRING_T_DEF.instantiate([])
+
+
+@dataclass
+class StringVal(val.ExtensionValue):
+    """Custom value for a string."""
+
+    v: str
+
+    def to_value(self) -> val.Extension:
+        name = "ConstString"
+        payload = {"value": self.v}
+        return val.Extension(
+            name,
+            typ=STRING_T,
+            val=payload,
+            extensions=[PRELUDE_EXTENSION.name],
+        )
+
+    def __str__(self) -> str:
+        return f"{self.v}"
