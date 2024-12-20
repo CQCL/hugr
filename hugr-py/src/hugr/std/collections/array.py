1aa5619b9bfcc89844763affead1790061f2087e
Agustín Borgna <agustin.borgna@quantinuum.com>
2024-12-20 13:11:33 +0100

    fix(py): Fix array/list value serialization

--- hugr-py/src/hugr/std/collections/array.py
+++ hugr-py/src/hugr/std/collections/array.py
@@ -60,7 +60,7 @@ class ArrayVal(val.ExtensionValue):
     """Constant value for a statically sized array of elements."""
 
     v: list[val.Value]
-    ty: tys.Type
+    ty: Array
 
     def __init__(self, v: list[val.Value], elem_ty: tys.Type) -> None:
         self.v = v
@@ -71,7 +71,11 @@ class ArrayVal(val.ExtensionValue):
         # The value list must be serialized at this point, otherwise the
         # `Extension` value would not be serializable.
         vs = [v._to_serial_root() for v in self.v]
-        return val.Extension(name, typ=self.ty, val=vs, extensions=[EXTENSION.name])
+        element_ty = self.ty.ty._to_serial_root()
+        serial_val = {"values": vs, "typ": element_ty}
+        return val.Extension(
+            name, typ=self.ty, val=serial_val, extensions=[EXTENSION.name]
+        )
 
     def __str__(self) -> str:
         return f"array({comma_sep_str(self.v)})"
