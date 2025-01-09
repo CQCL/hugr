de14aa66ad8b7da566663d46dd6df32bdb4d8dcd
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2025-01-09 18:37:18 +0000

    [revertme] use stable channel

--- devenv.nix
+++ devenv.nix
@@ -70,7 +70,7 @@ in
     # https://devenv.sh/languages/
     # https://devenv.sh/reference/options/#languagesrustversion
     languages.rust = {
-      channel = "nixpkgs";
+      channel = "beta";
       enable = true;
       components = [ "rustc" "cargo" "clippy" "rustfmt" "rust-analyzer" ];
     };
