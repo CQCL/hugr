0124f7eb74379dde124ea9507ab823d284125247
Douglas Wilson <141026920+doug-q@users.noreply.github.com>
2025-02-10 11:08:18 +0000

    feat: Add `Type::as_sum` and `SumType::variants`. (#1914)

--- hugr-core/src/std_extensions/arithmetic/float_types.rs
+++ hugr-core/src/std_extensions/arithmetic/float_types.rs
@@ -65,6 +65,7 @@ impl std::ops::Deref for ConstF64 {
 
 impl ConstF64 {
     /// Name of the constructor for creating constant 64bit floats.
+    #[cfg_attr(not(feature = "model_unstable"), allow(dead_code))]
     pub(crate) const CTR_NAME: &'static str = "arithmetic.float.const-f64";
 
     /// Create a new [`ConstF64`]
