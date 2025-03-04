4b6867772b47b702f34ed307bc8fb74cae99acc7
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-03-04 15:57:03 +0000

    GC find_main

--- hugr-passes/src/lib.rs
+++ hugr-passes/src/lib.rs
@@ -12,7 +12,6 @@ mod half_node;
 pub mod lower;
 pub mod merge_bbs;
 mod monomorphize;
-use hugr_core::HugrView;
 
 // TODO: Deprecated re-export. Remove on a breaking release.
 #[deprecated(
@@ -35,15 +34,3 @@ pub mod validation;
 pub use force_order::{force_order, force_order_by_key};
 pub use lower::{lower_ops, replace_many_ops};
 pub use non_local::{ensure_no_nonlocal_edges, nonlocal_edges};
-
-fn find_main<N: Copy>(h: &impl HugrView<Node = N>) -> Option<N> {
-    let root = h.root();
-    if !h.get_optype(root).is_module() {
-        return None;
-    }
-    h.children(root).find(|n| {
-        h.get_optype(*n)
-            .as_func_defn()
-            .is_some_and(|f| f.name == "main")
-    })
-}
