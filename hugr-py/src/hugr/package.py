0efb8497e731d28f351435890d77396c4809241e
Agustín Borgna <agustin.borgna@quantinuum.com>
2025-03-13 16:02:27 -0300

    Deprecation warnings are only in python313 -.-

--- hugr-py/src/hugr/package.py
+++ hugr-py/src/hugr/package.py
@@ -4,7 +4,17 @@ from __future__ import annotations
 
 from dataclasses import dataclass, field
 from typing import TYPE_CHECKING, Generic, TypeVar, cast
-from warnings import deprecated
+
+try:
+    from warnings import deprecated  # type: ignore[attr-defined]
+except ImportError:
+    # Python < 3.13
+    def deprecated(_msg):  # type: ignore[no-redef]
+        def _deprecated(func):
+            return func
+
+        return _deprecated
+
 
 import hugr._serialization.extension as ext_s
 from hugr.envelope import (
