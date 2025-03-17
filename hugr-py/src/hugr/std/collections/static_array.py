353c7d287c1402a15cf30081cdd26991e5a5bfd9
Mark Koch <mark.koch@quantinuum.com>
2025-03-17 11:13:10 +0000

    feat(hugr-py): Add  to standard extensions

---
+++ hugr-py/src/hugr/std/collections/static_array.py
@@ -0,0 +1,56 @@
+"""Statically sized immutable array type and its operations."""
+
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from hugr import tys, val
+from hugr.std import _load_extension
+from hugr.utils import comma_sep_str
+
+EXTENSION = _load_extension("collections.static_array")
+
+
+@dataclass(eq=False)
+class StaticArray(tys.ExtType):
+    """Fixed size immutable array of `ty` elements."""
+
+    def __init__(self, ty: tys.Type) -> None:
+        self.type_def = EXTENSION.types["static_array"]
+        self.args = [tys.TypeTypeArg(ty)]
+
+    @property
+    def ty(self) -> tys.Type:
+        assert isinstance(
+            self.args[0], tys.TypeTypeArg
+        ), "Array elements must have a valid type"
+        return self.args[0].ty
+
+    def type_bound(self) -> tys.TypeBound:
+        return self.ty.type_bound()
+
+
+@dataclass
+class StaticArrayVal(val.ExtensionValue):
+    """Constant value for a statically sized immutable array of elements."""
+
+    v: list[val.Value]
+    ty: tys.Type
+    name: str
+
+    def __init__(self, v: list[val.Value], elem_ty: tys.Type, name: str) -> None:
+        self.v = v
+        self.ty = StaticArray(elem_ty)
+        self.name = name
+
+    def to_value(self) -> val.Extension:
+        # The value list must be serialized at this point, otherwise the
+        # `Extension` value would not be serializable.
+        vs = [v._to_serial_root() for v in self.v]
+        serial_val = {"values": vs, "name": self.name}
+        return val.Extension(
+            "StaticArrayValue", typ=self.ty, val=serial_val, extensions=[EXTENSION.name]
+        )
+
+    def __str__(self) -> str:
+        return f"static_array({comma_sep_str(self.v)})"
