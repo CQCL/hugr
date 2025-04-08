4823c95771caf476cf26586559029b320443ffd2
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-04-08 16:51:54 +0100

    Early return for discard; generalize to n-way copy (oops); comments

--- hugr-passes/src/replace_types/handlers.rs
+++ hugr-passes/src/replace_types/handlers.rs
@@ -61,10 +61,8 @@ pub fn linearize_array(
     let [TypeArg::BoundedNat { n }, TypeArg::Type { ty }] = args else {
         panic!("Illegal TypeArgs to array: {:?}", args)
     };
-    // The num_outports==0 and num_outports>1 cases will combine, and become much easier,
-    // when we have a more generalized ArrayScan https://github.com/CQCL/hugr/issues/2041
     if num_outports == 0 {
-        // Make a function that maps the linear element to unit
+        // "Simple" discard - first map each element to unit (via type-specific discard):
         let map_fn = {
             let mut dfb = DFGBuilder::new(inout_sig(ty.clone(), Type::UNIT)).unwrap();
             let [to_discard] = dfb.input_wires_arr();
@@ -77,7 +75,7 @@ pub fn linearize_array(
         // Now array.scan that over the input array to get an array of unit (which can be discarded)
         let array_scan = ArrayScan::new(ty.clone(), Type::UNIT, vec![], *n, runtime_reqs(&map_fn));
         let in_type = array_type(*n, ty.clone());
-        Ok(NodeTemplate::CompoundOp(Box::new({
+        return Ok(NodeTemplate::CompoundOp(Box::new({
             let mut dfb = DFGBuilder::new(inout_sig(in_type, type_row![])).unwrap();
             let [in_array] = dfb.input_wires_arr();
             let map_fn = dfb.add_load_value(Value::Function {
@@ -86,123 +84,153 @@ pub fn linearize_array(
             // scan has one output, an array of unit, so just ignore/discard that
             dfb.add_dataflow_op(array_scan, [in_array, map_fn]).unwrap();
             dfb.finish_hugr_with_outputs([]).unwrap()
-        })))
-    } else {
-        let i64_t = INT_TYPES[6].to_owned();
-        let option_sty = option_type(ty.clone());
-        let option_ty = Type::from(option_sty.clone());
-        let option_array = array_type(*n, option_ty.clone());
-        let copy_elem = {
-            let mut io = vec![ty.clone(), i64_t.clone()];
-            io.extend(vec![option_array.clone(); num_outports - 1]);
-            let mut dfb = DFGBuilder::new(endo_sig(io)).unwrap();
-            let mut inputs = dfb.input_wires();
-            let elem = inputs.next().unwrap();
-            let idx = inputs.next().unwrap();
-            let opt_arrays = inputs.collect::<Vec<_>>();
-            let [idx_usz] = dfb
-                .add_dataflow_op(ConvertOpDef::itousize.without_log_width(), [idx])
-                .unwrap()
-                .outputs_arr();
-            let mut copies = lin
-                .copy_discard_op(ty, num_outports)?
-                .add(&mut dfb, [elem])
-                .unwrap()
-                .outputs();
-            let copy0 = copies.next().unwrap(); // We'll return this directly
-                                                // Wrap remaining copies into an option
-            let set_op = OpType::from(ArrayOpDef::set.to_concrete(option_ty.clone(), *n));
-            let either_st = set_op.dataflow_signature().unwrap().output[0]
-                .as_sum()
-                .unwrap()
-                .clone();
-            let opt_arrays = opt_arrays
-                .into_iter()
-                .zip_eq(copies)
-                .map(|(opt_array, copy1)| {
-                    let [tag] = dfb
-                        .add_dataflow_op(Tag::new(1, vec![type_row![], ty.clone().into()]), [copy1])
-                        .unwrap()
-                        .outputs_arr();
-                    let [set_result] = dfb
-                        .add_dataflow_op(set_op.clone(), [opt_array, idx_usz, tag])
-                        .unwrap()
-                        .outputs_arr();
-                    // set should always be successful
-                    let [none, opt_array] = dfb
-                        .build_unwrap_sum(1, either_st.clone(), set_result)
-                        .unwrap();
-                    //the removed element is an option, which should always be none (and thus discardable)
-                    let [] = dfb
-                        .build_unwrap_sum(0, SumType::new_option(ty.clone()), none)
-                        .unwrap();
-                    opt_array
-                })
-                .collect::<Vec<_>>(); // stop borrowing dfb
+        })));
+    };
+    // The num_outports>1 case will simplify, and unify with the previous, when we have a
+    // more general ArrayScan https://github.com/CQCL/hugr/issues/2041. In the meantime:
+    let num_new = num_outports - 1;
+    let array_ty = array_type(*n, ty.clone());
+    let mut dfb = DFGBuilder::new(inout_sig(array_ty.clone(), vec![array_ty.clone(); 2])).unwrap();
+    // 1. make num_new array<SZ, Option<T>>, initialized to None...
+    let option_sty = option_type(ty.clone());
+    let option_ty = Type::from(option_sty.clone());
+    let arrays_of_none = {
+        let fn_none = {
+            let mut dfb = DFGBuilder::new(inout_sig(vec![], option_ty.clone())).unwrap();
+            let none = dfb
+                .add_dataflow_op(Tag::new(0, vec![type_row![], ty.clone().into()]), [])
+                .unwrap();
+            dfb.finish_hugr_with_outputs(none.outputs()).unwrap()
+        };
+        let repeats =
+            vec![ArrayRepeat::new(option_ty.clone(), *n, runtime_reqs(&fn_none)); num_new];
+        let fn_none = dfb.add_load_value(Value::function(fn_none).unwrap());
+        repeats
+            .into_iter()
+            .map(|rpt| {
+                let [arr] = dfb.add_dataflow_op(rpt, [fn_none]).unwrap().outputs_arr();
+                arr
+            })
+            .collect::<Vec<_>>()
+    };
 
-            let cst1 = dfb.add_load_value(ConstInt::new_u(6, 1).unwrap());
-            let [new_idx] = dfb
-                .add_dataflow_op(IntOpDef::iadd.with_log_width(6), [idx, cst1])
-                .unwrap()
-                .outputs_arr();
-            dfb.finish_hugr_with_outputs([copy0, new_idx].into_iter().chain(opt_arrays))
-                .unwrap()
-        };
-        let unwrap_elem = {
-            let mut dfb =
-                DFGBuilder::new(inout_sig(Type::from(option_ty.clone()), ty.clone())).unwrap();
-            let [opt] = dfb.input_wires_arr();
-            let [val] = dfb.build_unwrap_sum(1, option_sty.clone(), opt).unwrap();
-            dfb.finish_hugr_with_outputs([val]).unwrap()
-        };
-        let array_ty = array_type(*n, ty.clone());
-        let mut dfb =
-            DFGBuilder::new(inout_sig(array_ty.clone(), vec![array_ty.clone(); 2])).unwrap();
-        let [in_array] = dfb.input_wires_arr();
-        // First, Scan `copy_elem` to get the original array back and an array of Some's of the copies
-        let scan1 = ArrayScan::new(
-            ty.clone(),
-            ty.clone(),
-            vec![i64_t, option_array],
-            *n,
-            runtime_reqs(&copy_elem),
-        );
-        let copy_elem = dfb.add_load_value(Value::function(copy_elem).unwrap());
-        let cst0 = dfb.add_load_value(ConstInt::new_u(6, 0).unwrap());
-        let [array_of_none] = {
-            let fn_none = {
-                let mut dfb = DFGBuilder::new(inout_sig(vec![], option_ty.clone())).unwrap();
-                let none = dfb
-                    .add_dataflow_op(Tag::new(0, vec![type_row![], ty.clone().into()]), [])
+    // 2. use a scan through the input array, copying the element num_outputs times;
+    // return the first copy, and put each of the other copies into one of the array<option>
+
+    let i64_t = INT_TYPES[6].to_owned();
+    let option_array = array_type(*n, option_ty.clone());
+    let copy_elem = {
+        let mut io = vec![ty.clone(), i64_t.clone()];
+        io.extend(vec![option_array.clone(); num_new]);
+        let mut dfb = DFGBuilder::new(endo_sig(io)).unwrap();
+        let mut inputs = dfb.input_wires();
+        let elem = inputs.next().unwrap();
+        let idx = inputs.next().unwrap();
+        let opt_arrays = inputs.collect::<Vec<_>>();
+        let [idx_usz] = dfb
+            .add_dataflow_op(ConvertOpDef::itousize.without_log_width(), [idx])
+            .unwrap()
+            .outputs_arr();
+        let mut copies = lin
+            .copy_discard_op(ty, num_outports)?
+            .add(&mut dfb, [elem])
+            .unwrap()
+            .outputs();
+        let copy0 = copies.next().unwrap(); // We'll return this directly
+                                            // Wrap remaining copies into an option
+        let set_op = OpType::from(ArrayOpDef::set.to_concrete(option_ty.clone(), *n));
+        let either_st = set_op.dataflow_signature().unwrap().output[0]
+            .as_sum()
+            .unwrap()
+            .clone();
+        let opt_arrays = opt_arrays
+            .into_iter()
+            .zip_eq(copies)
+            .map(|(opt_array, copy1)| {
+                let [tag] = dfb
+                    .add_dataflow_op(Tag::new(1, vec![type_row![], ty.clone().into()]), [copy1])
+                    .unwrap()
+                    .outputs_arr();
+                let [set_result] = dfb
+                    .add_dataflow_op(set_op.clone(), [opt_array, idx_usz, tag])
+                    .unwrap()
+                    .outputs_arr();
+                // set should always be successful
+                let [none, opt_array] = dfb
+                    .build_unwrap_sum(1, either_st.clone(), set_result)
                     .unwrap();
-                dfb.finish_hugr_with_outputs(none.outputs()).unwrap()
-            };
-            let rpt = ArrayRepeat::new(option_ty.clone(), *n, runtime_reqs(&fn_none));
-            let fn_none = dfb.add_load_value(Value::function(fn_none).unwrap());
-            dfb.add_dataflow_op(rpt, [fn_none]).unwrap()
-        }
-        .outputs_arr();
-        let [out_array1, _idx_out, opt_array] = dfb
-            .add_dataflow_op(scan1, [in_array, copy_elem, cst0, array_of_none])
+                //the removed element is an option, which should always be none (and thus discardable)
+                let [] = dfb
+                    .build_unwrap_sum(0, SumType::new_option(ty.clone()), none)
+                    .unwrap();
+                opt_array
+            })
+            .collect::<Vec<_>>(); // stop borrowing dfb
+
+        let cst1 = dfb.add_load_value(ConstInt::new_u(6, 1).unwrap());
+        let [new_idx] = dfb
+            .add_dataflow_op(IntOpDef::iadd.with_log_width(6), [idx, cst1])
             .unwrap()
             .outputs_arr();
-
-        let scan2 = ArrayScan::new(
-            option_ty.clone(),
-            ty.clone(),
-            vec![],
-            *n,
-            runtime_reqs(&unwrap_elem),
-        );
-        let unwrap_elem = dfb.add_load_value(Value::function(unwrap_elem).unwrap());
-        let [out_array2] = dfb
-            .add_dataflow_op(scan2, [opt_array, unwrap_elem])
+        dfb.finish_hugr_with_outputs([copy0, new_idx].into_iter().chain(opt_arrays))
             .unwrap()
-            .outputs_arr();
+    };
+    let [in_array] = dfb.input_wires_arr();
+    let scan1 = ArrayScan::new(
+        ty.clone(),
+        ty.clone(),
+        std::iter::once(i64_t)
+            .chain(vec![option_array; num_new])
+            .collect(),
+        *n,
+        runtime_reqs(&copy_elem),
+    );
 
-        Ok(NodeTemplate::CompoundOp(Box::new(
-            dfb.finish_hugr_with_outputs([out_array1, out_array2])
-                .unwrap(),
-        )))
-    }
+    let copy_elem = dfb.add_load_value(Value::function(copy_elem).unwrap());
+    let cst0 = dfb.add_load_value(ConstInt::new_u(6, 0).unwrap());
+
+    let mut outs = dfb
+        .add_dataflow_op(
+            scan1,
+            [in_array, copy_elem, cst0]
+                .into_iter()
+                .chain(arrays_of_none),
+        )
+        .unwrap()
+        .outputs();
+    let out_array1 = outs.next().unwrap();
+    let _idx_out = outs.next().unwrap();
+    let opt_arrays = outs;
+
+    //3. Scan each array-of-options, 'unwrapping' each element into a non-option
+    let unwrap_elem = {
+        let mut dfb =
+            DFGBuilder::new(inout_sig(Type::from(option_ty.clone()), ty.clone())).unwrap();
+        let [opt] = dfb.input_wires_arr();
+        let [val] = dfb.build_unwrap_sum(1, option_sty.clone(), opt).unwrap();
+        dfb.finish_hugr_with_outputs([val]).unwrap()
+    };
+
+    let unwrap_scan = ArrayScan::new(
+        option_ty.clone(),
+        ty.clone(),
+        vec![],
+        *n,
+        runtime_reqs(&unwrap_elem),
+    );
+    let unwrap_elem = dfb.add_load_value(Value::function(unwrap_elem).unwrap());
+
+    let out_arrays = std::iter::once(out_array1)
+        .chain(opt_arrays.map(|opt_array| {
+            let [out_array] = dfb
+                .add_dataflow_op(unwrap_scan.clone(), [opt_array, unwrap_elem])
+                .unwrap()
+                .outputs_arr();
+            out_array
+        }))
+        .collect::<Vec<_>>();
+
+    Ok(NodeTemplate::CompoundOp(Box::new(
+        dfb.finish_hugr_with_outputs(out_arrays).unwrap(),
+    )))
 }
