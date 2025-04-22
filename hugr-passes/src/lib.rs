89c2680912b47950ffd73a7c29a21386fdd0aee7
Alan Lawrence <alan.lawrence@quantinuum.com>
2025-04-22 17:16:31 +0100

    feat!: ComposablePass trait allowing sequencing and validation (#1895)
    Currently We have several "passes": monomorphization, dead function
    removal, constant folding. Each has its own code to allow setting a
    validation level (before and after that pass).
    
    This PR adds the ability chain (sequence) passes;, and to add validation
    before+after any pass or sequence; and commons up validation code. The
    top-level `constant_fold_pass` (etc.) functions are left as wrappers
    that do a single pass with validation only in test.
    
    I've left ConstFoldPass as always including DCE, but an alternative
    could be to return a sequence of the two - ATM that means a tuple
    `(ConstFoldPass, DeadCodeElimPass)`.
    
    I also wondered about including a method `add_entry_point` in
    ComposablePass (e.g. for ConstFoldPass, that means `with_inputs` but no
    inputs, i.e. all Top). I feel this is not applicable to *all* passes,
    but near enough. This could be done in a later PR but `add_entry_point`
    would need a no-op default for that to be a non-breaking change. So if
    we wouldn't be happy with the no-op default then I could just add it
    here...
    
    Finally...docs are extremely minimal ATM (this is hugr-passes), I am
    hoping that most of this is reasonably obvious (it doesn't really do a
    lot!), but please flag anything you think is particularly in need of a
    doc comment!
    
    BREAKING CHANGE: quite a lot of calls to current pass routines will
    break, specific cases include (a) `with_validation_level` should be done
    by wrapping a ValidatingPass around the receiver; (b) XXXPass::run()
    requires `use ...ComposablePass` (however, such calls will cease to do
    any validation).
    
    closes #1832

--- hugr-passes/src/lib.rs
+++ hugr-passes/src/lib.rs
@@ -1,6 +1,8 @@
 //! Compilation passes acting on the HUGR program representation.
 
 pub mod call_graph;
+pub mod composable;
+pub use composable::ComposablePass;
 pub mod const_fold;
 pub mod dataflow;
 pub mod dead_code;
@@ -21,19 +23,11 @@ pub mod untuple;
 )]
 #[allow(deprecated)]
 pub use monomorphize::remove_polyfuncs;
-// TODO: Deprecated re-export. Remove on a breaking release.
-#[deprecated(
-    since = "0.14.1",
-    note = "Use `hugr_passes::MonomorphizePass` instead."
-)]
-#[allow(deprecated)]
-pub use monomorphize::monomorphize;
-pub use monomorphize::{MonomorphizeError, MonomorphizePass};
+pub use monomorphize::{monomorphize, MonomorphizePass};
 pub mod replace_types;
 pub use replace_types::ReplaceTypes;
 pub mod nest_cfgs;
 pub mod non_local;
-pub mod validation;
 pub use force_order::{force_order, force_order_by_key};
 pub use lower::{lower_ops, replace_many_ops};
 pub use non_local::{ensure_no_nonlocal_edges, nonlocal_edges};
