1fadcbf724336e8424768ca74fadeb54fbb792e3
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2025-04-24 12:10:00 +0100

    deprecation fixes

--- hugr-llvm/src/utils.rs
+++ hugr-llvm/src/utils.rs
@@ -9,7 +9,7 @@ pub mod logic_op_builder;
 pub mod type_map;
 
 #[deprecated(note = "Import from hugr_core::std_extensions::collections::array.")]
-pub use array_op_builder::ArrayOpBuilder;
+pub use hugr_core::std_extensions::collections::array::ArrayOpBuilder;
 pub use inline_constant_functions::inline_constant_functions;
 pub use int_op_builder::IntOpBuilder;
 pub use logic_op_builder::LogicOpBuilder;
