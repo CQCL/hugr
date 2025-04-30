baaac100a6a9718aca1ba4b0ddd47da58670d3c2
Lukas Heidemann <lukas@heidemann.me>
2025-04-30 17:17:15 +0100

    Fixes.

--- hugr-passes/src/replace_types.rs
+++ hugr-passes/src/replace_types.rs
@@ -595,17 +595,17 @@ mod test {
         FunctionBuilder, HugrBuilder, ModuleBuilder, SubContainer, TailLoopBuilder,
     };
     use hugr_core::extension::prelude::{
-        bool_t, option_type, qb_t, usize_t, ConstUsize, UnwrapBuilder, PRELUDE_ID,
+        bool_t, option_type, qb_t, usize_t, ConstUsize, UnwrapBuilder,
     };
-    use hugr_core::extension::{simple_op::MakeExtensionOp, ExtensionSet, TypeDefBound, Version};
+    use hugr_core::extension::{simple_op::MakeExtensionOp, TypeDefBound, Version};
     use hugr_core::hugr::hugrmut::HugrMut;
     use hugr_core::hugr::{IdentList, ValidationError};
     use hugr_core::ops::constant::OpaqueValue;
     use hugr_core::ops::{ExtensionOp, NamedOp, OpTrait, OpType, Tag, Value};
-    use hugr_core::std_extensions::arithmetic::conversions::{self, ConvertOpDef};
+    use hugr_core::std_extensions::arithmetic::conversions::ConvertOpDef;
     use hugr_core::std_extensions::arithmetic::int_types::{ConstInt, INT_TYPES};
     use hugr_core::std_extensions::collections::{
-        array::{self, array_type, array_type_def, ArrayOp, ArrayOpDef, ArrayValue},
+        array::{array_type, array_type_def, ArrayOp, ArrayOpDef, ArrayValue},
         list::{list_type, list_type_def, ListOp, ListValue},
     };
     use hugr_core::types::{PolyFuncType, Signature, SumType, Type, TypeArg, TypeBound, TypeRow};
@@ -682,12 +682,7 @@ mod test {
         let mut dfb = new(Signature::new(
             vec![array_type(64, elem_ty.clone()), i64_t()],
             elem_ty.clone(),
-        )
-        .with_extension_delta(ExtensionSet::from_iter([
-            PRELUDE_ID,
-            array::EXTENSION_ID,
-            conversions::EXTENSION_ID,
-        ])))
+        ))
         .unwrap();
         let [val, idx] = dfb.input_wires_arr();
         let [idx] = dfb
@@ -1061,7 +1056,7 @@ mod test {
         repl.replace_consts_parametrized(array_type_def(), array_const);
         let mut h = backup;
         repl.run(&mut h).unwrap();
-        h.validate_no_extensions().unwrap();
+        h.validate().unwrap();
     }
 
     #[test]
