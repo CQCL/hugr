a24908715842a7e2042afc1f5edf04f89556780b
Lukas Heidemann <lukas@heidemann.me>
2025-05-21 17:56:58 +0100

    .

--- hugr-core/src/terms.rs
+++ hugr-core/src/terms.rs
@@ -2,8 +2,10 @@ use hugr_model::v0::{Literal, SymbolName};
 use hugr_model::v0::{VarName, ast};
 use std::fmt::Display;
 use std::sync::Arc;
+use triomphe::ThinArc;
+use views::{CoreBytes, CoreFloat, CoreNat, CoreStr, ViewError};
 
-mod views;
+pub mod views;
 
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub enum Term {
@@ -14,6 +16,38 @@ pub enum Term {
     Tuple(Tuple),
     Apply(Apply),
     Var(Var),
+    StaticType,
+}
+
+impl Term {
+    pub fn type_(&self) -> Term {
+        match self {
+            Term::Wildcard => Term::Wildcard,
+            Term::Literal(literal) => match literal {
+                Literal::Str(_) => CoreStr.into(),
+                Literal::Nat(_) => CoreNat.into(),
+                Literal::Bytes(_) => CoreBytes.into(),
+                Literal::Float(_) => CoreFloat.into(),
+            },
+            Term::List(list) => todo!(),
+            Term::Tuple(tuple) => todo!(),
+            Term::Apply(apply) => apply.type_(),
+            Term::Var(var) => todo!(),
+            Term::StaticType => Term::StaticType,
+        }
+    }
+
+    pub fn view_apply<const N: usize>(&self, symbol: &SymbolName) -> Result<[Term; N], ViewError> {
+        match self {
+            Term::Wildcard => Err(ViewError::Uninferred),
+            Term::Literal(_) => Err(ViewError::Mismatch),
+            Term::List(_) => Err(ViewError::Mismatch),
+            Term::Tuple(_) => Err(ViewError::Mismatch),
+            Term::Apply(apply) => apply.view(symbol),
+            Term::Var(_) => Err(ViewError::Variable),
+            Term::StaticType => Err(ViewError::Mismatch),
+        }
+    }
 }
 
 impl From<&Term> for ast::Term {
@@ -25,43 +59,32 @@ impl From<&Term> for ast::Term {
             Term::Tuple(tuple) => tuple.into(),
             Term::Apply(apply) => apply.into(),
             Term::Var(var) => var.into(),
+            Term::StaticType => {
+                ast::Term::Apply(SymbolName::new_static("core.static"), Default::default())
+            }
         }
     }
 }
 
+macro_rules! impl_from {
+    ($name:ident) => {
+        impl From<$name> for Term {
+            fn from(value: $name) -> Self {
+                Self::$name(value)
+            }
+        }
+    };
+}
+
+impl_from!(Literal);
+impl_from!(List);
+impl_from!(Tuple);
+impl_from!(Apply);
+impl_from!(Var);
+
 impl Display for Term {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", ast::Term::from(self))
-    }
-}
-
-impl From<Literal> for Term {
-    fn from(value: Literal) -> Self {
-        Self::Literal(value)
-    }
-}
-
-impl From<List> for Term {
-    fn from(value: List) -> Self {
-        Self::List(value)
-    }
-}
-
-impl From<Tuple> for Term {
-    fn from(value: Tuple) -> Self {
-        Self::Tuple(value)
-    }
-}
-
-impl From<Apply> for Term {
-    fn from(value: Apply) -> Self {
-        Self::Apply(value)
-    }
-}
-
-impl From<Var> for Term {
-    fn from(value: Var) -> Self {
-        Self::Var(value)
     }
 }
 
@@ -105,13 +128,13 @@ impl Display for List {
 }
 
 impl TryFrom<Term> for List {
-    type Error = ();
+    type Error = ViewError;
 
     fn try_from(value: Term) -> Result<Self, Self::Error> {
         match value {
             Term::List(list) => Ok(list),
             Term::Var(var) => Ok(List::new([SeqPart::Splice(Term::Var(var))])),
-            _ => Err(()),
+            _ => Err(ViewError::Mismatch),
         }
     }
 }
@@ -132,25 +155,58 @@ impl Display for Tuple {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Apply {
+pub struct Apply(ThinArc<ApplyHeader, Term>);
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct ApplyHeader {
     name: SymbolName,
-    args: Arc<[Term]>,
+    type_: Term,
 }
 
 impl Apply {
-    pub fn new(name: SymbolName, args: impl IntoIterator<Item = Term>) -> Self {
-        Self {
-            name,
-            args: args.into_iter().collect(),
-        }
+    pub fn new<A>(name: SymbolName, args: A, type_: Term) -> Self
+    where
+        A: IntoIterator<Item = Term>,
+        A::IntoIter: ExactSizeIterator,
+    {
+        Self(ThinArc::from_header_and_iter(
+            ApplyHeader { name, type_ },
+            args.into_iter(),
+        ))
     }
 
     pub fn name(&self) -> &SymbolName {
-        &self.name
+        &self.0.header.header.name
     }
 
     pub fn args(&self) -> &[Term] {
-        &self.args
+        &self.0.slice
+    }
+
+    pub fn type_(&self) -> Term {
+        self.0.header.header.type_.clone()
+    }
+
+    pub fn view<const N: usize>(&self, symbol: &SymbolName) -> Result<[Term; N], ViewError> {
+        if self.name() != symbol {
+            return Err(ViewError::Mismatch);
+        }
+
+        if self.args().len() > N {
+            return Err(ViewError::Invalid(format!(
+                "`{}` expects at most {} arguments",
+                symbol, N
+            )));
+        }
+
+        let result = std::array::from_fn(|i| {
+            (i + self.args().len())
+                .checked_sub(N)
+                .map(|i| self.args()[i].clone())
+                .unwrap_or_default()
+        });
+
+        Ok(result)
     }
 }
 
@@ -168,9 +224,27 @@ impl Display for Apply {
     }
 }
 
+impl TryFrom<Term> for Apply {
+    type Error = ViewError;
+
+    fn try_from(value: Term) -> Result<Self, Self::Error> {
+        match value {
+            Term::Apply(apply) => Ok(apply),
+            Term::Wildcard => Err(ViewError::Uninferred),
+            Term::Literal(_) => Err(ViewError::Mismatch),
+            Term::List(_) => Err(ViewError::Mismatch),
+            Term::Tuple(_) => Err(ViewError::Mismatch),
+            Term::Var(_) => Err(ViewError::Variable),
+            Term::StaticType => Err(ViewError::Mismatch),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Var {
     name: VarName,
+    index: u16,
+    type_: Arc<Term>,
 }
 
 impl From<&Var> for ast::Term {
@@ -182,5 +256,11 @@ impl From<&Var> for ast::Term {
 impl Display for Var {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", ast::Term::from(self))
+    }
+}
+
+impl Var {
+    pub fn type_(&self) -> Term {
+        self.type_.as_ref().clone()
     }
 }
--- hugr-core/src/terms/views.rs
+++ hugr-core/src/terms/views.rs
@@ -1,5 +1,72 @@
+use std::convert::Infallible;
+
 use super::{Apply, List, Term};
 use hugr_model::v0::SymbolName;
+use thiserror::Error;
+
+macro_rules! make_ctr {
+    ($ident:ident { $($arg:ident),* }, $symbol:expr) => {
+        impl TryFrom<Term> for $ident {
+            type Error = ViewError;
+
+            fn try_from(value: Term) -> Result<Self, Self::Error> {
+                let [$($arg),*] = value.view_apply(&Self::SYMBOL)?;
+                $(let $arg = $arg.try_into()?;)*
+                Ok(Self { $($arg),* })
+            }
+        }
+
+        impl $ident {
+            pub const SYMBOL: SymbolName = SymbolName::new_static($symbol);
+        }
+
+        impl From<$ident> for Apply {
+            fn from(value: $ident) -> Self {
+                Apply::new(
+                    $ident::SYMBOL,
+                    [$(value.$arg.into()),*],
+                    Term::default(),
+                )
+            }
+        }
+
+        impl From<$ident> for Term {
+            fn from(value: $ident) -> Self {
+                Term::Apply(value.into())
+            }
+        }
+    };
+
+    ($ident:ident, $symbol:expr) => {
+        impl TryFrom<Term> for $ident {
+            type Error = ViewError;
+
+            fn try_from(value: Term) -> Result<Self, Self::Error> {
+                let [] = value.view_apply(&Self::SYMBOL)?;
+                Ok(Self)
+            }
+        }
+
+        impl $ident {
+            pub const SYMBOL: SymbolName = SymbolName::new_static($symbol);
+        }
+
+        impl From<$ident> for Apply {
+            fn from($ident: $ident) -> Self {
+                static TERM: ::once_cell::sync::Lazy<Apply> = ::once_cell::sync::Lazy::new(|| {
+                    Apply::new($ident::SYMBOL, [], Term::Wildcard)
+                });
+                TERM.clone()
+            }
+        }
+
+        impl From<$ident> for Term {
+            fn from(value: $ident) -> Self {
+                Term::Apply(value.into())
+            }
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct CoreFn {
@@ -7,35 +74,94 @@ pub struct CoreFn {
     pub outputs: List,
 }
 
-impl CoreFn {
-    pub const SYMBOL: SymbolName = SymbolName::new_static("core.fn");
+make_ctr!(CoreFn { inputs, outputs }, "core.fn");
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CoreList {
+    pub item_type: Term,
 }
 
-impl TryFrom<Term> for CoreFn {
-    type Error = ();
+make_ctr!(CoreList { item_type }, "core.list");
 
-    fn try_from(value: Term) -> Result<Self, Self::Error> {
-        let Term::Apply(apply) = value else {
-            return Err(());
-        };
-
-        if apply.name() != &Self::SYMBOL {
-            return Err(());
-        };
-
-        if apply.args().len() < 2 {
-            return Err(());
-        };
-
-        let inputs = apply.args()[0].clone().try_into()?;
-        let outputs = apply.args()[1].clone().try_into()?;
-
-        Ok(CoreFn { inputs, outputs })
-    }
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct CoreTuple {
+    pub item_types: List,
 }
 
-impl From<CoreFn> for Term {
-    fn from(value: CoreFn) -> Self {
-        Apply::new(CoreFn::SYMBOL, [value.inputs.into(), value.outputs.into()]).into()
+make_ctr!(CoreTuple { item_types }, "core.tuple");
+
+#[derive(Debug, Clone, PartialEq, Eq, Default, Copy)]
+pub struct CoreStr;
+
+make_ctr!(CoreStr, "core.str");
+
+#[derive(Debug, Clone, PartialEq, Eq, Default, Copy)]
+pub struct CoreNat;
+
+make_ctr!(CoreNat, "core.nat");
+
+#[derive(Debug, Clone, PartialEq, Eq, Default, Copy)]
+pub struct CoreBytes;
+
+make_ctr!(CoreBytes, "core.bytes");
+
+#[derive(Debug, Clone, PartialEq, Eq, Default, Copy)]
+pub struct CoreFloat;
+
+make_ctr!(CoreFloat, "core.float");
+
+// impl CoreFn {
+//     pub const SYMBOL: SymbolName = SymbolName::new_static("core.fn");
+// }
+
+// impl TryFrom<Term> for CoreFn {
+//     type Error = ViewError;
+
+//     fn try_from(value: Term) -> Result<Self, Self::Error> {
+//         let [inputs, outputs] = value.view_apply(&Self::SYMBOL)?;
+//         let inputs = inputs.try_into()?;
+//         let outputs = outputs.try_into()?;
+//         Ok(CoreFn { inputs, outputs })
+//     }
+// }
+
+// impl From<CoreFn> for Term {
+//     fn from(value: CoreFn) -> Self {
+//         Apply::new(
+//             CoreFn::SYMBOL,
+//             [value.inputs.into(), value.outputs.into()],
+//             Term::default(),
+//         )
+//         .into()
+//     }
+// }
+
+#[derive(Debug, Error)]
+pub enum ViewError {
+    /// The viewed term does not match the pattern of the view.
+    ///
+    /// This is distinct from [`ViewError::Invalid`] in order to allow for
+    /// views to be used in pattern matching.
+    #[error("term mismatch")]
+    Mismatch,
+    /// The viewed term contains an uninferred subterm.
+    ///
+    /// This does not necessarily mean that the term is invalid since it
+    /// may at a later stage be inferred to be a concrete term.
+    #[error("uninferred term")]
+    Uninferred,
+    /// The viewed term contains a variable subterm.
+    ///
+    /// This does not necessarily mean that the term is invalid since the
+    /// variable may at a later stage be substituted with a concrete term.
+    #[error("term variable")]
+    Variable,
+    #[error("invalid term: {0}")]
+    Invalid(String),
+}
+
+impl From<Infallible> for ViewError {
+    fn from(value: Infallible) -> Self {
+        match value {}
     }
 }
