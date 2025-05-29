e7d3acf9adb1e70ae5bb9358892fdea4bb818f4a
Lukas Heidemann <lukas@heidemann.me>
2025-05-29 14:13:53 +0100

    Wildcard terms, Display for terms.

--- hugr-core/src/terms.rs
+++ hugr-core/src/terms.rs
@@ -1,34 +1,80 @@
 use fxhash::FxHasher;
+use hugr_model::v0::ast;
 use hugr_model::v0::{Literal, SymbolName, VarName};
+use once_cell::sync::Lazy;
 use servo_arc::ThinArc;
+use std::fmt::Display;
 use std::hash::{Hash, Hasher};
 
 use crate::Node;
 
-/// ...
+/// A term in the static language that is used to parameterise `Hugr`s.
 ///
-/// To pattern match the values of a term use [`Term::view`].
+/// To pattern match the values of a term use [`Term::get`].
+///
+/// Terms are immutable and have no identity beyond structural equality. Two
+/// structurally equal terms should be considered logically indistinguishable.
+/// Equal terms can share their representation and this sharing can be observed
+/// to enable optimisations.
+///
+/// Terms can be cloned cheaply without requiring a copy of the term's data.
+/// Equality comparisons and hash codes are optimised so that [`Term`]s can be
+/// used as keys in hash tables.
+///
+/// A term has a unique type within the context of a hugr. In particular, local
+/// variables refer to their binding site. The same term may have different
+/// types when interpreted in the context of different hugrs. This enables
+/// caching types and constraints in a side table instead of requiring them to
+/// be stored inline with the term.
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Term(ThinArc<TermHeader, Term>);
 
+/// Assert that [`Term`] is the same size as a pointer.
+const _: () = assert!(std::mem::size_of::<Term>() == std::mem::size_of::<usize>());
+
+impl From<Term> for ast::Term {
+    fn from(value: Term) -> Self {
+        Self::from(value.get())
+    }
+}
+
+impl From<&Term> for ast::Term {
+    fn from(value: &Term) -> Self {
+        Self::from(value.get())
+    }
+}
+
+impl Display for Term {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        Display::fmt(&self.get(), f)
+    }
+}
+
 /// The constant-sized part of the [`Term`].
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash, Default)]
 enum TermData {
+    #[default]
+    Wildcard,
     Apply(SymbolName),
     Literal(Literal),
     List,
     ListConcat,
+    Tuple,
+    TupleConcat,
     Var(Var),
 }
 
 impl TermData {
-    pub fn split<'a>(view: TermView<'a>) -> (Self, &'a [Term]) {
+    pub fn split<'a>(view: TermKind<'a>) -> (Self, &'a [Term]) {
         match view {
-            TermView::Apply(symbol, terms) => (TermData::Apply(symbol.clone()), terms),
-            TermView::Literal(literal) => (TermData::Literal(literal.clone()), &[] as &[_]),
-            TermView::List(terms) => (TermData::List, terms),
-            TermView::ListConcat(terms) => (TermData::ListConcat, terms),
-            TermView::Var(var) => (TermData::Var(var.clone()), &[] as &[_]),
+            TermKind::Wildcard => (TermData::Wildcard, &[]),
+            TermKind::Apply(symbol, terms) => (TermData::Apply(symbol.clone()), terms),
+            TermKind::Literal(literal) => (TermData::Literal(literal.clone()), &[] as &[_]),
+            TermKind::List(terms) => (TermData::List, terms),
+            TermKind::ListConcat(terms) => (TermData::ListConcat, terms),
+            TermKind::Tuple(terms) => (TermData::Tuple, terms),
+            TermKind::TupleConcat(terms) => (TermData::TupleConcat, terms),
+            TermKind::Var(var) => (TermData::Var(var.clone()), &[] as &[_]),
         }
     }
 }
@@ -63,7 +109,7 @@ impl TermHeader {
 
 impl Term {
     /// Create a new [`Term`].
-    pub fn new(view: TermView) -> Self {
+    pub fn new(view: TermKind) -> Self {
         let (data, terms) = TermData::split(view);
         Self(ThinArc::from_header_and_iter(
             TermHeader::new(data, terms),
@@ -75,7 +121,7 @@ impl Term {
     ///
     /// Terms created with this method will never be deallocated. This is useful
     /// for [`Term`]s that are known at Rust compile time.
-    pub fn new_static(view: TermView) -> Self {
+    pub fn new_static(view: TermKind) -> Self {
         let (data, terms) = TermData::split(view);
         Self(ThinArc::from_header_and_iter_alloc(
             |layout| unsafe { std::alloc::alloc(layout) },
@@ -86,15 +132,18 @@ impl Term {
         ))
     }
 
-    /// Borrow this term's data as a [`TermView`].
+    /// Borrow this term's data as a [`TermKind`].
     #[inline]
-    pub fn view(&self) -> TermView {
+    pub fn get(&self) -> TermKind {
         match &self.0.header.data {
-            TermData::Apply(symbol) => TermView::Apply(symbol, self.0.slice()),
-            TermData::Literal(literal) => TermView::Literal(literal),
-            TermData::List => TermView::List(self.0.slice()),
-            TermData::ListConcat => TermView::ListConcat(self.0.slice()),
-            TermData::Var(var) => TermView::Var(var),
+            TermData::Wildcard => TermKind::Wildcard,
+            TermData::Apply(symbol) => TermKind::Apply(symbol, self.0.slice()),
+            TermData::Literal(literal) => TermKind::Literal(literal),
+            TermData::List => TermKind::List(self.0.slice()),
+            TermData::ListConcat => TermKind::ListConcat(self.0.slice()),
+            TermData::Tuple => TermKind::Tuple(self.0.slice()),
+            TermData::TupleConcat => TermKind::TupleConcat(self.0.slice()),
+            TermData::Var(var) => TermKind::Var(var),
         }
     }
 }
@@ -105,14 +154,77 @@ impl Hash for Term {
     }
 }
 
+/// The default [`Term`] is a wildcard term.
+///
+/// ```
+/// # use hugr_core::terms::{Term, TermKind};
+/// assert_eq!(Term::default().get(), TermKind::Wildcard);
+/// ```
+impl Default for Term {
+    fn default() -> Self {
+        static WILDCARD: Lazy<Term> = Lazy::new(|| Term::new_static(TermKind::Wildcard));
+        WILDCARD.clone()
+    }
+}
+
 /// A borrowed view into a [`Term`].
-#[derive(Debug, Clone)]
-pub enum TermView<'a> {
+#[derive(Debug, Clone, PartialEq, Eq, Default)]
+pub enum TermKind<'a> {
+    #[default]
+    Wildcard,
     Apply(&'a SymbolName, &'a [Term]),
     Literal(&'a Literal),
     List(&'a [Term]),
     ListConcat(&'a [Term]),
+    Tuple(&'a [Term]),
+    TupleConcat(&'a [Term]),
     Var(&'a Var),
+}
+
+impl From<TermKind<'_>> for ast::Term {
+    fn from(value: TermKind<'_>) -> Self {
+        match value {
+            TermKind::Wildcard => ast::Term::Wildcard,
+            TermKind::Apply(symbol, args) => {
+                let symbol = symbol.clone();
+                let args = args.iter().map(ast::Term::from).collect();
+                ast::Term::Apply(symbol, args)
+            }
+            TermKind::Literal(literal) => ast::Term::Literal(literal.clone()),
+            TermKind::List(items) => ast::Term::List(
+                items
+                    .iter()
+                    .map(|item| ast::SeqPart::Item(item.into()))
+                    .collect(),
+            ),
+            TermKind::ListConcat(lists) => ast::Term::List(
+                lists
+                    .iter()
+                    .map(|list| ast::SeqPart::Splice(list.into()))
+                    .collect(),
+            ),
+            TermKind::Var(var) => ast::Term::Var(var.name().clone()),
+            TermKind::Tuple(items) => ast::Term::Tuple(
+                items
+                    .iter()
+                    .map(|item| ast::SeqPart::Item(item.into()))
+                    .collect(),
+            ),
+            TermKind::TupleConcat(lists) => ast::Term::Tuple(
+                lists
+                    .iter()
+                    .map(|list| ast::SeqPart::Splice(list.into()))
+                    .collect(),
+            ),
+        }
+    }
+}
+
+impl Display for TermKind<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let term: ast::Term = self.clone().into();
+        Display::fmt(&term, f)
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -120,4 +232,21 @@ pub struct Var {
     name: VarName,
     node: Node,
     index: u16,
+}
+
+impl Var {
+    #[inline]
+    pub fn name(&self) -> &VarName {
+        &self.name
+    }
+
+    #[inline]
+    pub fn node(&self) -> Node {
+        self.node
+    }
+
+    #[inline]
+    pub fn index(&self) -> u16 {
+        self.index
+    }
 }
