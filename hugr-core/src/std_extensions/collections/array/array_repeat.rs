a99badab693ee64a8c4b5f00b8b30c1e78c4cefd
Lukas Heidemann <lukas@heidemann.me>
2025-06-06 19:22:37 +0200

    `...Type` suffix for `TypeParam` variants.

--- hugr-core/src/std_extensions/collections/array/array_repeat.rs
+++ hugr-core/src/std_extensions/collections/array/array_repeat.rs
@@ -52,8 +52,8 @@ impl<AK: ArrayKind> FromStr for GenericArrayRepeatDef<AK> {
 impl<AK: ArrayKind> GenericArrayRepeatDef<AK> {
     /// To avoid recursion when defining the extension, take the type definition as an argument.
     fn signature_from_def(&self, array_def: &TypeDef) -> SignatureFunc {
-        let params = vec![TypeParam::max_nat(), TypeBound::Any.into()];
-        let n = TypeArg::new_var_use(0, TypeParam::max_nat());
+        let params = vec![TypeParam::max_nat_type(), TypeBound::Any.into()];
+        let n = TypeArg::new_var_use(0, TypeParam::max_nat_type());
         let t = Type::new_var_use(1, TypeBound::Any);
         let func = Type::new_function(Signature::new(vec![], vec![t.clone()]));
         let array_ty =
