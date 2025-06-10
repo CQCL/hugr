becc6b9857939e029c7903d82365ba023beb7ad2
Seyon Sivarajah <seyon.sivarajah@quantinuum.com>
2025-06-10 10:26:48 +0100

    fix(py): correct ConstString JSON encoding

--- hugr-py/tests/test_prelude.py
+++ hugr-py/tests/test_prelude.py
@@ -1,8 +1,18 @@
+from hugr.build.dfg import Dfg
 from hugr.std.prelude import STRING_T, StringVal
+
+from .conftest import validate
 
 
 def test_string_val():
-    ext_val = StringVal("test").to_value()
+    val = StringVal("test")
+    ext_val = val.to_value()
     assert ext_val.name == "ConstString"
     assert ext_val.typ == STRING_T
-    assert ext_val.val == {"value": "test"}
+    assert ext_val.val == "test"
+
+    dfg = Dfg()
+    v = dfg.load(val)
+    dfg.set_outputs(v)
+
+    validate(dfg.hugr)
