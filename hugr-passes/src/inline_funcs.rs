35ca32003797e40a19d9f89029463b623db2b663
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-07-13 22:07:14 +0100

    Just inline nodes in a pre-order traversal beneath entrypoint

--- hugr-passes/src/inline_funcs.rs
+++ hugr-passes/src/inline_funcs.rs
@@ -1,65 +1,78 @@
-use std::collections::HashSet;
+/// Pass to inline calls to selected functions in a Hugr.
+use std::collections::{HashSet, VecDeque};
 
-use hugr_core::hugr::{hugrmut::HugrMut, patch::inline_call::InlineCall};
-use petgraph::{Direction, algo::tarjan_scc, visit::EdgeRef};
+use hugr_core::hugr::hugrmut::HugrMut;
+use hugr_core::hugr::patch::inline_call::{InlineCall, InlineCallError};
+use petgraph::algo::tarjan_scc;
 
-use crate::call_graph::{CallGraph, CallGraphEdge, CallGraphNode};
+use crate::call_graph::{CallGraph, CallGraphNode};
 
-/// Inline all [Call]s except those to functions in cycles on the call graph,
-/// and subject to a filter function that is given the [Call] node and the target
-/// [FuncDefn] node.
+/// Error raised by [inline_acyclic]
+#[derive(Clone, Debug, thiserror::Error)]
+pub enum InlineAllError<N> {
+    /// Raised to indicate a request to inline calls to a function that is part of an SCC
+    /// in the call graph.
+    #[error("Cannot inline calls to {0} as in a cycle {1:?}")]
+    FunctionOnCycle(N, Vec<N>),
+    /// An error inlining a call.
+    #[error(transparent)]
+    InlineCallError(#[from] InlineCallError),
+}
+
+/// Inline all [Call]s to the specified `target_funcs` subject to a filter function
+/// that is given the [Call] node and the target [FuncDefn] node. (Note the [Call]
+/// may be created as a result of inlining and so may not have existed in the input
+/// Hugr).
+///
+/// # Errors
+///
+/// [InlineAllError::FunctionOnCycle] if any element of `target_funcs` is in a cycle
+/// in the call graph
 ///
 /// [Call]: hugr_core::ops::Call
 /// [FuncDefn]: hugr_core::ops::FuncDefn
 pub fn inline_acyclic<H: HugrMut>(
     h: &mut H,
     cg: CallGraph<H::Node>,
+    target_funcs: HashSet<H::Node>, // If empty, inline ALL
     filt_func: impl Fn(H::Node, H::Node) -> bool,
-) {
+) -> Result<(), InlineAllError<H::Node>> {
     let g = cg.graph();
-    let sccs = tarjan_scc(cg.graph());
-    let all_sccs: HashSet<_> = sccs.iter().flatten().collect::<HashSet<_>>();
-    if h.entrypoint() == h.module_root() {
-        // Can modify everywhere. Traverse post-order (callee before caller).
-        let mut seen = HashSet::new();
-        let mut stack: Vec<_> = h
-            .children(h.module_root())
-            .map(|n| (cg.node_index(n).unwrap(), false))
-            .collect();
-        while let Some((func, children_done)) = stack.pop() {
-            let edges = cg
-                .graph()
-                .edges_directed(func, Direction::Outgoing)
-                .filter_map(|e| {
-                    assert_eq!(e.source(), func);
-                    let d = e.target();
-                    (!all_sccs.contains(&d)).then_some((g.edge_weight(e.id()).unwrap(), d))
-                });
-            if !children_done {
-                stack.push((func, true));
-                stack.extend(edges.map(|(_, tgt)| (tgt, false)));
-                // We know no tgt can push `func` because we have already filtered out cycles
-                continue
-            }
-            // Callees have all been processed.
-            if !seen.insert(func) { continue } // Caller has too!
-            for (edge, callee) in edges {
-                let tgt_func: H::Node = match g.node_weight(callee).unwrap() {
-                    CallGraphNode::FuncDecl(_) => continue,
-                    CallGraphNode::FuncDefn(n) => *n,
-                    CallGraphNode::NonFuncRoot => unreachable!("Call to non-func"),
-                };
-                if let CallGraphEdge::Call(n) = edge {
-                    // If we've processed `func` already, the calls will have become DFGs
-                    assert_eq!(h.static_source(*n), Some(tgt_func));
-                    if filt_func(*n, tgt_func) {
-                        h.apply_patch(InlineCall::new(tgt_func)).unwrap();
-                    }
+    let sccs = tarjan_scc(g)
+        .into_iter()
+        .map(|ns| {
+            ns.into_iter()
+                .map(|n| {
+                    let CallGraphNode::FuncDefn(fd) = g.node_weight(n).unwrap() else {
+                        panic!("Expected only FuncDefns in sccs")
+                    };
+                    *fd
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>();
+    let all_sccs: HashSet<_> = sccs.iter().cloned().flatten().collect();
+    let target_funcs = if target_funcs.is_empty() {
+        h.children(h.module_root())
+            .filter(|n| !all_sccs.contains(n))
+            .collect()
+    } else if let Some(n) = target_funcs.intersection(&all_sccs).next() {
+        let scc = sccs.iter().find(|ns| ns.as_slice().contains(n)).unwrap();
+        return Err(InlineAllError::FunctionOnCycle(*n, scc.clone()));
+    } else {
+        target_funcs
+    };
+    let mut q = VecDeque::from([h.entrypoint()]);
+    while let Some(n) = q.pop_front() {
+        if h.get_optype(n).is_call() {
+            if let Some(t) = h.static_source(n) {
+                if target_funcs.contains(&t) && filt_func(n, t) {
+                    h.apply_patch(InlineCall::new(n)).unwrap();
                 }
             }
-            
         }
-    } else {
-        todo!() // Can only modify inside entrypoint. But, can keep inlining.
+        // If that was a call, `n` is now a DFG containing the function body, so explore inside
+        q.extend(h.children(n));
     }
+    Ok(())
 }
