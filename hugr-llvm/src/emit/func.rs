84547355b36515346ee95a14eb9e6ea2e904deb0
Jenny Chen <jenny.chen@Quantinuum.com>
2025-07-29 14:14:44 -0600

    fix: added public func getter for EmitFuncContext (#2482)
    Closes: https://github.com/quantinuum-dev/eldarion/issues/360
    
    Enables:
    * https://github.com/CQCL/tket2/pull/997
    
    This PR adds a public func getter for EmitFuncContext to enable tket2
    qsystem to access the FunctionValue and sets linkage to Private for all
    the qsystem helper functions.
    
    Other Notes:
    * Commit
    [640629d](https://github.com/CQCL/hugr/pull/2482/commits/640629d394c033ca06a38e19387d891edde22f46)
    is based on the old hugr version
    ([hugr-v0.20.2](https://github.com/CQCL/hugr/tree/hugr-v0.20.2)) and is
    used by https://github.com/quantinuum-dev/eldarion/pull/373 as eldarion
    hasn't upgraded to latest hugr version.

--- hugr-llvm/src/emit/func.rs
+++ hugr-llvm/src/emit/func.rs
@@ -109,6 +109,11 @@ impl<'c, 'a, H: HugrView<Node = Node>> EmitFuncContext<'c, 'a, H> {
         self.todo.insert(node.node());
     }
 
+    /// Returns the current [FunctionValue] being emitted.
+    pub fn func(&self) -> FunctionValue<'c> {
+        self.func
+    }
+
     /// Returns the internal [Builder]. Callers must ensure that it is
     /// positioned at the end of a basic block. This invariant is not checked(it
     /// doesn't seem possible to check it).
@@ -351,4 +356,22 @@ pub fn build_ok_or_else<'c, H: HugrView<Node = Node>>(
     let right = either_ty.build_tag(builder, 1, vec![ok_value])?;
     let either = builder.build_select(is_ok, right, left, "")?;
     Ok(either)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_func_getter() {
+        // Use TestContext for consistent test setup
+        let test_ctx = crate::test::test_ctx(-1);
+        let emit_context = test_ctx.get_emit_module_context();
+        let func_type = emit_context.iw_context().void_type().fn_type(&[], false);
+        let function = emit_context
+            .module()
+            .add_function("test_func", func_type, None);
+        let func_context = super::EmitFuncContext::new(emit_context, function).unwrap();
+
+        // Assert the getter returns the correct function
+        assert_eq!(func_context.func(), function);
+    }
 }
