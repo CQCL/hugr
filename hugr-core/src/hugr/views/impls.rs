5c7d8a94cf1b7ef7e002d0b61a1dc09a03e1a133
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2025-08-12 15:45:40 +0100

    Optimize by turning HashMap into impl IntoIterator

--- hugr-core/src/hugr/views/impls.rs
+++ hugr-core/src/hugr/views/impls.rs
@@ -114,8 +114,8 @@ macro_rules! hugr_mut_methods {
                 fn connect(&mut self, src: Self::Node, src_port: impl Into<crate::OutgoingPort>, dst: Self::Node, dst_port: impl Into<crate::IncomingPort>);
                 fn disconnect(&mut self, node: Self::Node, port: impl Into<crate::Port>);
                 fn add_other_edge(&mut self, src: Self::Node, dst: Self::Node) -> (crate::OutgoingPort, crate::IncomingPort);
-                fn insert_forest(&mut self, other: crate::Hugr, roots: HashMap<crate::Node, Self::Node>) -> Result<HashMap<crate::Node, Self::Node>, InsertForestError>;
-                fn insert_view_forest<Other: crate::hugr::HugrView>(&mut self, other: &Other, nodes: impl IntoIterator<Item=Other::Node>, roots: HashMap<Other::Node, Self::Node>) -> Result<HashMap<Other::Node, Self::Node>, InsertForestError<Other::Node>>;
+                fn insert_forest(&mut self, other: crate::Hugr, roots: impl IntoIterator<Item=(crate::Node, Self::Node)>) -> Result<HashMap<crate::Node, Self::Node>, InsertForestError>;
+                fn insert_view_forest<Other: crate::hugr::HugrView>(&mut self, other: &Other, nodes: impl Iterator<Item=Other::Node> + Clone, roots: impl IntoIterator<Item=(Other::Node, Self::Node)>) -> Result<HashMap<Other::Node, Self::Node>, InsertForestError<Other::Node>>;
                 fn use_extension(&mut self, extension: impl Into<std::sync::Arc<crate::extension::Extension>>);
                 fn use_extensions<Reg>(&mut self, registry: impl IntoIterator<Item = Reg>) where crate::extension::ExtensionRegistry: Extend<Reg>;
             }
