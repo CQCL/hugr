c5cf1d7f6657caaf76b7b46e7210cd7cb19b532d
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2025-10-27 10:50:29 +0000

    minor fixes

--- hugr-py/src/hugr/hugr/pass.py
+++ hugr-py/src/hugr/hugr/pass.py
@@ -1,13 +1,19 @@
+"""A Protocol for a composable pass."""
+
 from typing import Protocol, Self
+
 from hugr.hugr.base import Hugr
+from hugr.node_port import Node
 
 
 class ComposablePass(Protocol):
+    """A Protocol which represents a composable Hugr transformation."""
+
     def __call__(self, hugr: Hugr) -> None: ...
 
     def then(self, other: Self) -> Self: ...
 
-    def with_entrypoint(self) -> Self: ...
+    def with_entrypoint(self, node: Node) -> Self: ...
 
     @property
     def is_global(self) -> bool: ...
