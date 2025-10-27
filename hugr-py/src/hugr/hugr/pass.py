8a3d6baa08085298d6aff32b3a16ff05b5d53af7
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2025-10-27 10:57:31 +0000

    more methods

--- hugr-py/src/hugr/hugr/pass.py
+++ hugr-py/src/hugr/hugr/pass.py
@@ -4,6 +4,7 @@ from typing import Protocol, Self
 
 from hugr.hugr.base import Hugr
 from hugr.node_port import Node
+from hugr.ops import Op
 
 
 class ComposablePass(Protocol):
@@ -14,6 +15,14 @@ class ComposablePass(Protocol):
     def then(self, other: Self) -> Self: ...
 
     def with_entrypoint(self, node: Node) -> Self: ...
+
+    @classmethod
+    def from_dict(cls, dictionary: dict) -> Self: ...
+
+    def to_dict(self) -> dict: ...
+
+    @property
+    def supported_ops(self) -> set[Op]: ...
 
     @property
     def is_global(self) -> bool: ...
