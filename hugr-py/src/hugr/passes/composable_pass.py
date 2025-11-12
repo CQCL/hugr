975ddbb03806c0198e3db9fc9b86e723d0656e18
Callum Macpherson <callum.macpherson@quantinuum.com>
2025-11-12 15:56:22 +0000

    fixes

--- hugr-py/src/hugr/passes/composable_pass.py
+++ hugr-py/src/hugr/passes/composable_pass.py
@@ -3,9 +3,10 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
-from typing import Protocol
+from typing import TYPE_CHECKING, Protocol
 
-from hugr.hugr.base import Hugr
+if TYPE_CHECKING:
+    from hugr.hugr.base import Hugr
 
 
 class ComposablePass(Protocol):
