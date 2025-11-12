45dc3fc20623ceb2734e7e3be2b80c47611e9ac6
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2025-11-12 17:41:30 +0000

    feat(py):  `ComposablePass` protocol and `ComposedPass` for hugr-py (#2636)
    closes #2617 
    
    Mostly follows Agustin's suggestion see
    [eeb4c50](https://github.com/CQCL/hugr/pull/2636/commits/eeb4c50880c599efb35e477b26123b2e635d4c4e)
    
    I'm also added some `to/from_dict` methods as well as a `supported_ops`
    property. EDIT: removed these additional methods for now.
    
    ---------
    
    Co-authored-by: Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>

---
+++ hugr-py/tests/test_passes.py
@@ -0,0 +1,26 @@
+from hugr.hugr.base import Hugr
+from hugr.passes.composable_pass import ComposablePass, ComposedPass
+
+
+def test_composable_pass() -> None:
+    class MyDummyPass(ComposablePass):
+        def __call__(self, hugr: Hugr) -> None:
+            return self(hugr)
+
+        def then(self, other: ComposablePass) -> ComposablePass:
+            return ComposedPass([self, other])
+
+        @property
+        def name(self) -> str:
+            return "Dummy"
+
+    dummy = MyDummyPass()
+
+    composed = dummy.then(dummy)
+
+    my_composed_pass = ComposedPass([dummy, dummy])
+
+    assert my_composed_pass.passes == [dummy, dummy]
+    assert isinstance(my_composed_pass, ComposablePass)
+    assert isinstance(composed, ComposablePass)
+    assert dummy.name == "Dummy"
