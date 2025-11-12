fcf00c0c44871a364c39c1f0dcdfda33406f9be2
Callum Macpherson <callum.macpherson@quantinuum.com>
2025-11-12 12:21:33 +0000

    docstrings

--- hugr-py/src/hugr/passes/composable_pass.py
+++ hugr-py/src/hugr/passes/composable_pass.py
@@ -11,17 +11,22 @@ from hugr.hugr.base import Hugr
 class ComposablePass(Protocol):
     """A Protocol which represents a composable Hugr transformation."""
 
-    def __call__(self, hugr: Hugr) -> None: ...
+    def __call__(self, hugr: Hugr) -> None:
+        """Call the pass to transform a HUGR."""
+        ...
 
-    def then(self, other: Self) -> Self: ...
+    def then(self, other: Self) -> Self:
+       """Perform another composable pass after this pass."""
+       ...
 
 
 @dataclass
 class ComposedPass(ComposablePass):
-   """A Sequence of composable passes."""
+   """A sequence of composable passes."""
    passes: list[ComposablePass]
 
    def __call__(self, hugr: Hugr):
+       """Call all of the passes in sequence."""
        for comp_pass in self.passes:
            comp_pass(hugr)
        
