17c3db13131a3dbd895944d98a319d0b2fcb66d5
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2025-11-24 10:26:46 +0000

    feat: Make `ComposablePass.__call__` return a Hugr (#2697)
    Implementing the revisions to `ComposablePass` and `ComposedPass` as
    discussed with Agustin.
    
    I still find the `_apply` and `_apply_inplace` rather unintuitive
    (although now I realise that it will work). Looking at the default
    implementations in the `ComposablePass` protocol these look circular but
    as you're always overrididing at least one of the `_apply` or
    `_apply_inplace` methods whenever you implement the protocol.
    
    
    Would be interested to know if there are suggestions to improve this.
    
    ---------
    
    Co-authored-by: Agustín Borgna <121866228+aborgna-q@users.noreply.github.com>
    Co-authored-by: Agustín Borgna <agustin.borgna@quantinuum.com>

--- hugr-py/src/hugr/passes/_composable_pass.py
+++ hugr-py/src/hugr/passes/_composable_pass.py
@@ -5,10 +5,13 @@ Currently unstable.
 
 from __future__ import annotations
 
+from copy import deepcopy
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
 if TYPE_CHECKING:
+    from collections.abc import Callable
+
     from hugr.hugr.base import Hugr
 
 
@@ -16,9 +19,11 @@ if TYPE_CHECKING:
 class ComposablePass(Protocol):
     """A Protocol which represents a composable Hugr transformation."""
 
-    def __call__(self, hugr: Hugr) -> None:
-        """Call the pass to transform a HUGR."""
-        ...
+    def __call__(self, hugr: Hugr, *, inplace: bool = True) -> Hugr:
+        """Call the pass to transform a HUGR.
+
+        See :func:`_impl_pass_call` for a helper function to implement this method.
+        """
 
     @property
     def name(self) -> str:
@@ -42,16 +47,65 @@ class ComposablePass(Protocol):
         return ComposedPass(pass_list)
 
 
+def impl_pass_call(
+    *,
+    hugr: Hugr,
+    inplace: bool,
+    inplace_call: Callable[[Hugr], None] | None = None,
+    copy_call: Callable[[Hugr], Hugr] | None = None,
+) -> Hugr:
+    """Helper function to implement a ComposablePass.__call__ method, given an
+    inplace or copy-returning pass methods.
+
+    At least one of the `inplace_call` or `copy_call` arguments must be provided.
+
+    :param hugr: The Hugr to apply the pass to.
+    :param inplace: Whether to apply the pass inplace.
+    :param inplace_call: The method to apply the pass inplace.
+    :param copy_call: The method to apply the pass by copying the Hugr.
+    :return: The transformed Hugr.
+    """
+    if inplace and inplace_call is not None:
+        inplace_call(hugr)
+        return hugr
+    elif inplace and copy_call is not None:
+        new_hugr = copy_call(hugr)
+        hugr._overwrite_hugr(new_hugr)
+        return hugr
+    elif not inplace and copy_call is not None:
+        return copy_call(hugr)
+    elif not inplace and inplace_call is not None:
+        new_hugr = deepcopy(hugr)
+        inplace_call(new_hugr)
+        return new_hugr
+    else:
+        msg = "Pass must implement at least an inplace or copy run method"
+        raise ValueError(msg)
+
+
 @dataclass
 class ComposedPass(ComposablePass):
     """A sequence of composable passes."""
 
     passes: list[ComposablePass]
 
-    def __call__(self, hugr: Hugr):
-        """Call all of the passes in sequence."""
-        for comp_pass in self.passes:
-            comp_pass(hugr)
+    def __call__(self, hugr: Hugr, *, inplace: bool = True) -> Hugr:
+        def apply(hugr: Hugr) -> Hugr:
+            result_hugr = hugr
+            for comp_pass in self.passes:
+                result_hugr = comp_pass(result_hugr, inplace=False)
+            return result_hugr
+
+        def apply_inplace(hugr: Hugr) -> None:
+            for comp_pass in self.passes:
+                comp_pass(hugr, inplace=True)
+
+        return impl_pass_call(
+            hugr=hugr,
+            inplace=inplace,
+            inplace_call=apply_inplace,
+            copy_call=apply,
+        )
 
     @property
     def name(self) -> str:
